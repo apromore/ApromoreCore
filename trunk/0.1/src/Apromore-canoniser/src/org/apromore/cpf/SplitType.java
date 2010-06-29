30c98057d7bcd7aabec329bd731d909b236336c6
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2010-06-29 08:08:54 +0000

    Unstable: fixing versions, sync between NPF and data

--- trunk/0.1/src/Apromore-canoniser/src/org/apromore/cpf/SplitType.java
+++ trunk/0.1/src/Apromore-canoniser/src/org/apromore/cpf/SplitType.java
@@ -31,7 +31,7 @@ import javax.xml.bind.annotation.XmlType;
     XORSplitType.class,
     ORSplitType.class
 })
-@Generated(value = "com.sun.tools.xjc.Driver", date = "2010-06-29T02:48:54+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
+@Generated(value = "com.sun.tools.xjc.Driver", date = "2010-06-29T04:37:55+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
 public class SplitType
     extends RoutingType
 {
