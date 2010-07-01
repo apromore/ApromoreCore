078ca35ddb4d4bc1c299936100101826d2e1009a
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2010-07-01 08:16:39 +0000

    Unstable: implementing update meta data

--- 0.1/src/Apromore-dataAccess/src/org/apromore/data_access/model_manager/UpdateProcessSummariesOutputMsgType.java
+++ 0.1/src/Apromore-dataAccess/src/org/apromore/data_access/model_manager/UpdateProcessSummariesOutputMsgType.java
@@ -4,24 +4,22 @@ package org.apromore.data_access.model_manager;
 import javax.annotation.Generated;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for WriteEditSessionOutputMsgType complex type.
+ * <p>Java class for UpdateProcessSummariesOutputMsgType complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="WriteEditSessionOutputMsgType">
+ * &lt;complexType name="UpdateProcessSummariesOutputMsgType">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element name="Result" type="{http://www.apromore.org/data_access/model_manager}ResultType"/>
  *       &lt;/sequence>
- *       &lt;attribute name="EditSessionCode" type="{http://www.w3.org/2001/XMLSchema}int" />
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -30,18 +28,15 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "WriteEditSessionOutputMsgType", propOrder = {
+@XmlType(name = "UpdateProcessSummariesOutputMsgType", propOrder = {
     "result"
 })
 @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-01T05:18:17+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
-public class WriteEditSessionOutputMsgType {
+public class UpdateProcessSummariesOutputMsgType {
 
     @XmlElement(name = "Result", required = true)
     @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-01T05:18:17+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
     protected ResultType result;
-    @XmlAttribute(name = "EditSessionCode")
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-01T05:18:17+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
-    protected Integer editSessionCode;
 
     /**
      * Gets the value of the result property.
@@ -67,32 +62,6 @@ public class WriteEditSessionOutputMsgType {
     @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-01T05:18:17+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
     public void setResult(ResultType value) {
         this.result = value;
-    }
-
-    /**
-     * Gets the value of the editSessionCode property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link Integer }
-     *     
-     */
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-01T05:18:17+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
-    public Integer getEditSessionCode() {
-        return editSessionCode;
-    }
-
-    /**
-     * Sets the value of the editSessionCode property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link Integer }
-     *     
-     */
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-01T05:18:17+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
-    public void setEditSessionCode(Integer value) {
-        this.editSessionCode = value;
     }
 
 }
