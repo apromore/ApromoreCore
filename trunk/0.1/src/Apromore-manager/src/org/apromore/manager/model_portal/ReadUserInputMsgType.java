38e6b4838d58622235d2b8120e1bd87bd4ca3d2a
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2010-07-02 05:33:33 +0000

    Stable. Edit process meta data implemented

---
+++ trunk/0.1/src/Apromore-manager/src/org/apromore/manager/model_portal/ReadUserInputMsgType.java
@@ -0,0 +1,63 @@
+
+package org.apromore.manager.model_portal;
+
+import javax.annotation.Generated;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for ReadUserInputMsgType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ReadUserInputMsgType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;attribute name="username" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ReadUserInputMsgType")
+@Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-02T11:00:01+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
+public class ReadUserInputMsgType {
+
+    @XmlAttribute
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-02T11:00:01+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
+    protected String username;
+
+    /**
+     * Gets the value of the username property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-02T11:00:01+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the value of the username property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-07-02T11:00:01+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-833")
+    public void setUsername(String value) {
+        this.username = value;
+    }
+
+}
