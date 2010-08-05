7d3216dd3d703c5166913685741d5c71e8513b9c
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2010-08-05 23:26:22 +0000

    Not to be checked out - onprogress -

---
+++ trunk/0.1/src/Apromore-toolbox/src/org/apromore/toolbox/model_da/ReadCanonicalsOutputMsgType.java
@@ -0,0 +1,111 @@
+
+package org.apromore.toolbox.model_da;
+
+import javax.annotation.Generated;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+import org.apache.cxf.jaxb.JAXBToStringBuilder;
+import org.apache.cxf.jaxb.JAXBToStringStyle;
+
+
+/**
+ * <p>Java class for ReadCanonicalsOutputMsgType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ReadCanonicalsOutputMsgType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="Result" type="{http://www.apromore.org/data_access/model_toolbox}ResultType"/>
+ *         &lt;element name="Canonicals" type="{http://www.apromore.org/data_access/model_toolbox}CanonicalsType"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ReadCanonicalsOutputMsgType", propOrder = {
+    "result",
+    "canonicals"
+})
+@Generated(value = "com.sun.tools.xjc.Driver", date = "2010-08-04T03:40:56+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+public class ReadCanonicalsOutputMsgType {
+
+    @XmlElement(name = "Result", required = true)
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-08-04T03:40:56+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    protected ResultType result;
+    @XmlElement(name = "Canonicals", required = true)
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-08-04T03:40:56+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    protected CanonicalsType canonicals;
+
+    /**
+     * Gets the value of the result property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link ResultType }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-08-04T03:40:56+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public ResultType getResult() {
+        return result;
+    }
+
+    /**
+     * Sets the value of the result property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link ResultType }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-08-04T03:40:56+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public void setResult(ResultType value) {
+        this.result = value;
+    }
+
+    /**
+     * Gets the value of the canonicals property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link CanonicalsType }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-08-04T03:40:56+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public CanonicalsType getCanonicals() {
+        return canonicals;
+    }
+
+    /**
+     * Sets the value of the canonicals property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link CanonicalsType }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-08-04T03:40:56+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public void setCanonicals(CanonicalsType value) {
+        this.canonicals = value;
+    }
+
+    /**
+     * Generates a String representation of the contents of this type.
+     * This is an extension method, produced by the 'ts' xjc plugin
+     * 
+     */
+    @Override
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-08-04T03:40:56+10:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public String toString() {
+        return JAXBToStringBuilder.valueOf(this, JAXBToStringStyle.DEFAULT_STYLE);
+    }
+
+}
