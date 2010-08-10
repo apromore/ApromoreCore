901946d93dedbe61b784b0549f81d634e4edb5df
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2010-08-10 00:27:53 +0000

    Stable - minor defects fixed

--- 0.1/src/Apromore-database/Scripts/apromore.sql
+++ 0.1/src/Apromore-database/Scripts/apromore.sql
@@ -4,7 +4,6 @@ drop table if exists derived_versions;
 drop table if exists annotations ;
 drop table if exists natives ;
 drop table if exists canonicals ;
-drop table if exists process_versions ;
 drop table if exists processes ;
 drop table if exists search_histories;
 drop table if exists users ;
@@ -58,30 +57,18 @@ create table processes (
 ) engine=InnoDB;
 show warnings ;
 
-
-create table process_versions (
+create table canonicals (
+    uri        varchar(40),
     processId int,
-    version_name varchar(40),
+    version_name  varchar(20),
+    author varchar(40),
     creation_date varchar(35),
     last_update varchar(35),
     ranking varchar(10),
     documentation text,
-    constraint pk_versions primary key (processId,version_name),
-    constraint fk_versions1 foreign key (processId) references processes(processId)
-    on delete cascade on update cascade
-) engine=InnoDB;
-show warnings ;
-
-create table canonicals (
-    uri        int     auto_increment,
-    processId int,
-    version_name varchar(40),
     content longtext,
     constraint pk_canonicals primary key (uri),
-    constraint un_canonicals unique (processId, version_name),
-    constraint fk_canonicals foreign key (processId, version_name) 
-    	references process_versions(processId, version_name)
-   		on update cascade on delete cascade
+    constraint un_canonicals unique (processId, version_name)
 ) engine=innoDB;
 show warnings ;
 
@@ -89,7 +76,7 @@ create table natives (
     uri        int     auto_increment,
     content longtext,
     nat_type varchar(20),
-    canonical int,
+    canonical varchar(40),
     constraint pk_natives primary key (uri),
     constraint un_natives unique (canonical,nat_type),
     constraint fk_natives foreign key (nat_type) references native_types(nat_type), 
@@ -101,7 +88,7 @@ show warnings ;
 create table annotations (
     uri         int     auto_increment,
     native      int,
-    canonical int,
+    canonical varchar(40),
     name 		varchar(40),
     content longtext,
     constraint pk_annotation primary key (uri),
@@ -113,14 +100,14 @@ show warnings ;
 
 create table derived_versions (
     processId int,
-    version varchar(40),
-    derived_version varchar(40),
+    version varchar(20),
+    derived_version varchar(20),
     constraint pk_derived_version primary key (processId,version,derived_version),
     constraint fk_derived_version1 foreign key (processId,version)
-    	references process_versions(processId,version_name)
+    	references canonicals(processId,version_name)
     	on delete cascade on update cascade,
     constraint fk_derived_version2 foreign key (processId,derived_version)
-    	references process_versions(processId,version_name)
+    	references canonicals(processId,version_name)
     	on delete cascade on update cascade
 ) engine=InnoDB;
 show warnings ;
@@ -133,11 +120,12 @@ create table edit_session_mappings (
 	version_name varchar(40),
 	nat_type  varchar(20),
 	annotation varchar(40),
+	remove_fake_events boolean,
 	constraint pk_edit_session_mappings primary key (code),
 	constraint fk__edit_session_mappings1 foreign key (username) references users(username)
 	on delete cascade on update cascade,
 	constraint fk__edit_session_mappings2 foreign key (processId,version_name)
-	references process_versions(processId,version_name)
+	references canonicals(processId,version_name)
 	on delete cascade on update cascade
 ) engine=InnoDB;
 show warnings ;
@@ -163,7 +151,7 @@ show warnings ;
 
 create view process_ranking (processId, ranking) as
     select processId, avg(ranking)
-    from process_versions
+    from canonicals
     group by processId;
     
 create view keywords (processId, word) as
@@ -180,7 +168,7 @@ create view keywords (processId, word) as
     from processes join users on (owner = username)
     union
     select processId, version_name
-    from process_versions
+    from canonicals
     ;
 
     create view head_versions0 (processid, version) as
@@ -193,6 +181,6 @@ create view keywords (processId, word) as
     create view head_versions (processId, version) as
     select processId, version from head_versions0
     union
-    select processId, version_name from process_versions
+    select processId, version_name from canonicals
     where processId not in (select processId from derived_versions);
     
