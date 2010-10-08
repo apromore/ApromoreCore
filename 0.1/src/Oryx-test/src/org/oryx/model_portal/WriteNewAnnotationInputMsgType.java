0b86d35651feafac464093b0df9dbc9fc585c0b7
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2010-10-08 16:38:23 +0000

    v0.2 - unstable - annotation not fully tested

---
+++ 0.1/src/Oryx-test/src/org/oryx/model_portal/WriteNewAnnotationInputMsgType.java
@@ -0,0 +1,144 @@
+
+package org.oryx.model_portal;
+
+import javax.activation.DataHandler;
+import javax.annotation.Generated;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlMimeType;
+import javax.xml.bind.annotation.XmlType;
+import org.apache.cxf.jaxb.JAXBToStringBuilder;
+import org.apache.cxf.jaxb.JAXBToStringStyle;
+
+
+/**
+ * <p>Java class for WriteNewAnnotationInputMsgType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="WriteNewAnnotationInputMsgType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="Native" type="{http://www.w3.org/2001/XMLSchema}base64Binary"/>
+ *       &lt;/sequence>
+ *       &lt;attribute name="EditSessionCode" type="{http://www.w3.org/2001/XMLSchema}int" />
+ *       &lt;attribute name="AnnotationName" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "WriteNewAnnotationInputMsgType", propOrder = {
+    "_native"
+})
+@Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+public class WriteNewAnnotationInputMsgType {
+
+    @XmlElement(name = "Native", required = true)
+    @XmlMimeType("application/octet-stream")
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    protected DataHandler _native;
+    @XmlAttribute(name = "EditSessionCode")
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    protected Integer editSessionCode;
+    @XmlAttribute(name = "AnnotationName")
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    protected String annotationName;
+
+    /**
+     * Gets the value of the native property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link DataHandler }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public DataHandler getNative() {
+        return _native;
+    }
+
+    /**
+     * Sets the value of the native property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link DataHandler }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public void setNative(DataHandler value) {
+        this._native = value;
+    }
+
+    /**
+     * Gets the value of the editSessionCode property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public Integer getEditSessionCode() {
+        return editSessionCode;
+    }
+
+    /**
+     * Sets the value of the editSessionCode property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public void setEditSessionCode(Integer value) {
+        this.editSessionCode = value;
+    }
+
+    /**
+     * Gets the value of the annotationName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public String getAnnotationName() {
+        return annotationName;
+    }
+
+    /**
+     * Sets the value of the annotationName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public void setAnnotationName(String value) {
+        this.annotationName = value;
+    }
+
+    /**
+     * Generates a String representation of the contents of this type.
+     * This is an extension method, produced by the 'ts' xjc plugin
+     * 
+     */
+    @Override
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-10-08T05:27:55+02:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public String toString() {
+        return JAXBToStringBuilder.valueOf(this, JAXBToStringStyle.DEFAULT_STYLE);
+    }
+
+}
