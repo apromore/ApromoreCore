2025d482809135d71a10afc518f619733be98aa5
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2010-11-17 17:23:03 +0000

    Unstable - ongoing work for Search and merge features

--- 0.1/src/Apromore-manager/src/org/apromore/manager/exception/ExceptionSearchForSimilar.java
+++ 0.1/src/Apromore-manager/src/org/apromore/manager/exception/ExceptionSearchForSimilar.java
@@ -1,22 +1,22 @@
 package org.apromore.manager.exception;
 
-public class ExceptionReadAllCanonicals extends Exception {
+public class ExceptionSearchForSimilar extends Exception {
 
-	public ExceptionReadAllCanonicals() {
+	public ExceptionSearchForSimilar() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ExceptionReadAllCanonicals(String message) {
+	public ExceptionSearchForSimilar(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
 
-	public ExceptionReadAllCanonicals(Throwable cause) {
+	public ExceptionSearchForSimilar(Throwable cause) {
 		super(cause);
 		// TODO Auto-generated constructor stub
 	}
 
-	public ExceptionReadAllCanonicals(String message, Throwable cause) {
+	public ExceptionSearchForSimilar(String message, Throwable cause) {
 		super(message, cause);
 		// TODO Auto-generated constructor stub
 	}
--- 0.1/src/Apromore-manager/src/org/apromore/manager/toolbox/RequestToToolbox.java
+++ 0.1/src/Apromore-manager/src/org/apromore/manager/toolbox/RequestToToolbox.java
@@ -1,40 +1,96 @@
 package org.apromore.manager.toolbox;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.xml.namespace.QName;
 
 import org.apromore.manager.commons.Constants;
-import org.apromore.manager.exception.ExceptionReadAllCanonicals;
-import org.apromore.manager.model_toolbox.CanonicalType;
+import org.apromore.manager.exception.ExceptionSearchForSimilar;
+import org.apromore.manager.model_portal.CanonicalType;
+import org.apromore.manager.model_portal.CanonicalsType;
+import org.apromore.manager.model_toolbox.ParameterType;
+import org.apromore.manager.model_toolbox.ParametersType;
 import org.apromore.manager.model_toolbox.ResultType;
 import org.apromore.manager.model_toolbox.SearchForSimilarProcessesInputMsgType;
 
 public class RequestToToolbox {
+
 	private static final QName SERVICE_NAME = new QName(Constants.TOOLBOX_MANAGER_URI, Constants.TOOLBOX_MANAGER_SERVICE);
 	private ToolboxManagerPortType port;
 
-	private RequestToToolbox() {
+	public RequestToToolbox() {
 		URL wsdlURL = ToolboxManagerService.WSDL_LOCATION;
 		ToolboxManagerService ss = new ToolboxManagerService(wsdlURL, SERVICE_NAME);
 		this.port = ss.getToolboxManager();
 	}
 
-	public List<CanonicalType> ReadAllCanonicals() throws ExceptionReadAllCanonicals {
+	
+	public org.apromore.manager.model_portal.CanonicalsType SearchForSimilarProcesses(
+			int selectedModelId, 
+			String method, 
+			double modelthreshold, 
+			double labelthreshold, 
+			double contextthreshold, 
+			double skipnweight, 
+			double subnweight, 
+			double skipeweight) throws ExceptionSearchForSimilar {
 		org.apromore.manager.model_toolbox.SearchForSimilarProcessesInputMsgType payload =
 			new SearchForSimilarProcessesInputMsgType();
+		payload.setAlgorithm(method);
+		payload.setProcessId(selectedModelId);
+		org.apromore.manager.model_toolbox.ParametersType params = new ParametersType();
+		// modelthreshold
+		org.apromore.manager.model_toolbox.ParameterType p = new ParameterType();
+		p.setName("modelthreshold");
+		p.setValue(modelthreshold);
+		params.getParameter().add(p);
+
+		// labelthreshold
+		p = new ParameterType();
+		p.setName("labelthreshold");
+		p.setValue(labelthreshold);
+		params.getParameter().add(p);
+
+		// contextthreshold
+		p = new ParameterType();
+		p.setName("contextthreshold");
+		p.setValue(contextthreshold);
+		params.getParameter().add(p);
+
+		if ("Greedy".equals(method)) {
+			// skipnweight
+			p = new ParameterType();
+			p.setName("skipnweight");
+			p.setValue(skipnweight);
+			params.getParameter().add(p);
+
+			// subnweight
+			p = new ParameterType();
+			p.setName("subnweight");
+			p.setValue(subnweight);
+			params.getParameter().add(p);
+
+			// skipeweight
+			p = new ParameterType();
+			p.setName("skipeweight");
+			p.setValue(skipeweight);
+			params.getParameter().add(p);
+		}
+		payload.setParameters(params);
 		org.apromore.manager.model_toolbox.SearchForSimilarProcessesOutputMsgType res =
 			this.port.searchForSimilarProcesses(payload);
 		ResultType result = res.getResult();
-		List<CanonicalType> canonicals = new ArrayList<CanonicalType>();
 		if (result.getCode()==-1) {
-			throw new ExceptionReadAllCanonicals (result.getMessage());
+			throw new ExceptionSearchForSimilar (result.getMessage());
 		} else {
-			org.apromore.manager.model_toolbox.CanonicalsType allCanonicalsDA = res.getCanonicals();
-			canonicals = allCanonicalsDA.getCanonicalType();    	
-			return canonicals;
+			org.apromore.manager.model_portal.CanonicalsType canonicalsP = new CanonicalsType();
+			for (org.apromore.manager.model_toolbox.CanonicalType canonicalT: res.getCanonicals().getCanonicalType()) {
+				org.apromore.manager.model_portal.CanonicalType canonicalP = new CanonicalType();
+				canonicalP.setCpf(canonicalT.getCpf());
+				canonicalP.setProcessId(canonicalT.getProcessId());
+				canonicalP.setVersionName(canonicalT.getVersionName());
+			}
+			return canonicalsP;
 		}
 	}
 }
