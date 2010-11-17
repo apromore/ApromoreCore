67fb7c327d8cb8a0d0cd0be4173d5a91bc6c88c8
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2010-11-17 17:23:03 +0000

    Unstable - ongoing work for Search and merge features

---
+++ 0.1/src/Apromore-manager/.cxftmp/src/org/apromore/manager/model_toolbox1/CpfIdType.java
@@ -0,0 +1,106 @@
+
+package org.apromore.manager.model_toolbox1;
+
+import javax.annotation.Generated;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import org.apache.cxf.jaxb.JAXBToStringBuilder;
+import org.apache.cxf.jaxb.JAXBToStringStyle;
+
+
+/**
+ * <p>Java class for Cpf_idType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="Cpf_idType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;attribute name="ProcessId" type="{http://www.w3.org/2001/XMLSchema}int" />
+ *       &lt;attribute name="VersionName" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Cpf_idType")
+@Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-16T05:22:29+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+public class CpfIdType {
+
+    @XmlAttribute(name = "ProcessId")
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-16T05:22:29+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    protected Integer processId;
+    @XmlAttribute(name = "VersionName")
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-16T05:22:29+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    protected String versionName;
+
+    /**
+     * Gets the value of the processId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-16T05:22:29+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public Integer getProcessId() {
+        return processId;
+    }
+
+    /**
+     * Sets the value of the processId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-16T05:22:29+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public void setProcessId(Integer value) {
+        this.processId = value;
+    }
+
+    /**
+     * Gets the value of the versionName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-16T05:22:29+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public String getVersionName() {
+        return versionName;
+    }
+
+    /**
+     * Sets the value of the versionName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-16T05:22:29+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public void setVersionName(String value) {
+        this.versionName = value;
+    }
+
+    /**
+     * Generates a String representation of the contents of this type.
+     * This is an extension method, produced by the 'ts' xjc plugin
+     * 
+     */
+    @Override
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-16T05:22:29+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public String toString() {
+        return JAXBToStringBuilder.valueOf(this, JAXBToStringStyle.DEFAULT_STYLE);
+    }
+
+}
---
+++ 0.1/src/Apromore-manager/.cxftmp/src/org/apromore/manager/model_toolbox1/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.apromore.org/toolbox/model_manager")
+package org.apromore.manager.model_toolbox1;
