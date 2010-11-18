51f9dce36a844caaad5e83a2a8e910af0eb9c3c0
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2010-11-18 15:46:07 +0000

    Unstable - ongoing work for Search and merge features

--- 0.1/src/Apromore-dataAccess/src/org/apromore/data_access/model_toolbox/ReadCanonicalsInputMsgType.java
+++ 0.1/src/Apromore-dataAccess/src/org/apromore/data_access/model_toolbox/ReadCanonicalsInputMsgType.java
@@ -1,10 +1,12 @@
 
 package org.apromore.data_access.model_toolbox;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.Generated;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 import org.apache.cxf.jaxb.JAXBToStringBuilder;
 import org.apache.cxf.jaxb.JAXBToStringStyle;
@@ -19,7 +21,9 @@ import org.apache.cxf.jaxb.JAXBToStringStyle;
  * &lt;complexType name="ReadCanonicalsInputMsgType">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;attribute name="Empty" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;sequence>
+ *         &lt;element name="ProcessVersion" type="{http://www.apromore.org/data_access/model_toolbox}ProcessVersionType" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -28,38 +32,44 @@ import org.apache.cxf.jaxb.JAXBToStringStyle;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "ReadCanonicalsInputMsgType")
-@Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-17T05:08:04+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+@XmlType(name = "ReadCanonicalsInputMsgType", propOrder = {
+    "processVersion"
+})
+@Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-18T04:27:53+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
 public class ReadCanonicalsInputMsgType {
 
-    @XmlAttribute(name = "Empty")
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-17T05:08:04+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
-    protected String empty;
+    @XmlElement(name = "ProcessVersion")
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-18T04:27:53+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    protected List<ProcessVersionType> processVersion;
 
     /**
-     * Gets the value of the empty property.
+     * Gets the value of the processVersion property.
      * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-17T05:08:04+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
-    public String getEmpty() {
-        return empty;
-    }
-
-    /**
-     * Sets the value of the empty property.
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the processVersion property.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getProcessVersion().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link ProcessVersionType }
+     * 
+     * 
      */
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-17T05:08:04+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
-    public void setEmpty(String value) {
-        this.empty = value;
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-18T04:27:53+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    public List<ProcessVersionType> getProcessVersion() {
+        if (processVersion == null) {
+            processVersion = new ArrayList<ProcessVersionType>();
+        }
+        return this.processVersion;
     }
 
     /**
@@ -68,7 +78,7 @@ public class ReadCanonicalsInputMsgType {
      * 
      */
     @Override
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-17T05:08:04+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2010-11-18T04:27:53+01:00", comments = "JAXB RI vhudson-jaxb-ri-2.1-2")
     public String toString() {
         return JAXBToStringBuilder.valueOf(this, JAXBToStringStyle.DEFAULT_STYLE);
     }
