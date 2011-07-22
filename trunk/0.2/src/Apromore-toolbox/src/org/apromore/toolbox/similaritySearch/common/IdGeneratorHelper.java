62f0df85eaffe40689b1988e5e3e55001ce90fa5
Marie-Christine.Fauvet@imag.fr <Marie-Christine.Fauvet@imag.fr@5faac2a4-3861-aa31-f262-64efd740a31a>
2011-07-22 04:48:00 +0000

    Change IdGeneratorHelper implementation to conform convention on ids taken in Apromore (see issue 106)

--- trunk/0.2/src/Apromore-toolbox/src/org/apromore/toolbox/similaritySearch/common/IdGeneratorHelper.java
+++ trunk/0.2/src/Apromore-toolbox/src/org/apromore/toolbox/similaritySearch/common/IdGeneratorHelper.java
@@ -1,10 +1,20 @@
 package org.apromore.toolbox.similaritySearch.common;
 
 import java.math.BigInteger;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class IdGeneratorHelper {
-	private BigInteger nextId = BigInteger.ONE;
-
+//	private BigInteger nextId = BigInteger.ONE;
+	private BigInteger nextId;
+	
+	public IdGeneratorHelper() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmsSSS");
+		Date date = new Date();
+		String time = dateFormat.format(date);
+		this.nextId = new BigInteger(time);
+	}
 	public BigInteger getNextId() {
 		BigInteger toreturn = new BigInteger(nextId.toString());
 		nextId = nextId.add(BigInteger.ONE);
