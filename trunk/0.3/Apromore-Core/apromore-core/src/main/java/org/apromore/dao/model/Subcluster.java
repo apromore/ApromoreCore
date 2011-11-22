2bccd9e5171199b7c081df2985be70f2432a97b6
cam.james@gmail.com <cam.james@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2011-11-22 04:11:24 +0000

    Comp:Version Control - Cameron James Added the models for the version control component. Getting to start integration.

---
+++ trunk/0.3/Apromore-Core/apromore-core/src/main/java/org/apromore/dao/model/Subcluster.java
@@ -0,0 +1,125 @@
+package org.apromore.dao.model;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Stores the Sub Clusters?????.
+ *
+ * @author Cameron James
+ */
+@Entity
+@Table(name = "subcluster")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@NamedQueries({
+        //@NamedQuery(name = TempVersion.FIND_USER, query = "SELECT usr FROM User usr WHERE usr.username = :username"),
+        //@NamedQuery(name = TempVersion.FIND_ALL_USERS, query = "SELECT usr FROM User usr")
+})
+@Configurable("subcluster")
+public class Subcluster implements Serializable {
+
+    /** Hard coded for interoperability. */
+    private static final long serialVersionUID = -2353318364761081118L;
+
+    private SubclusterId id;
+    private Integer fragmentSize;
+    private String subclusterId;
+    private FragmentVersion fragmentVersion;
+
+
+    /**
+     * Default Constructor.
+     */
+    public Subcluster() { }
+
+
+    /**
+     * Get the Primary Key for the Object.
+     * @return Returns the Id.
+     */
+    @EmbeddedId
+    @AttributeOverrides({
+            @AttributeOverride(name = "fragmentVersionId", column = @Column(name = "fragment_version_id", nullable = false, length = 40)),
+            @AttributeOverride(name = "parentClusterId", column = @Column(name = "parent_cluster_id", nullable = false, length = 80))})
+    public SubclusterId getId() {
+        return this.id;
+    }
+
+    /**
+     * Set the Primary Key for the Object.
+     * @param newId The id to set.
+     */
+    public void setId(final SubclusterId newId) {
+        this.id = newId;
+    }
+
+    /**
+     * Get the fragment version for the Object.
+     * @return Returns the fragment version.
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fragment_version_id", nullable = false, insertable = false, updatable = false)
+    public FragmentVersion getFragmentVersion() {
+        return this.fragmentVersion;
+    }
+
+    /**
+     * Set the fragment version for the Object.
+     * @param newFragmentVersion The fragment version to set.
+     */
+    public void setFragmentVersion(final FragmentVersion newFragmentVersion) {
+        this.fragmentVersion = newFragmentVersion;
+    }
+
+
+    /**
+     * Get the fragment size for the Object.
+     * @return Returns the fragment size.
+     */
+    @Column(name = "fragment_size")
+    public Integer getFragmentSize() {
+        return this.fragmentSize;
+    }
+
+    /**
+     * Set the fragment size for the Object.
+     * @param newFragmentSize The fragment size to set.
+     */
+    public void setFragmentSize(final Integer newFragmentSize) {
+        this.fragmentSize = newFragmentSize;
+    }
+
+
+    /**
+     * Get the sub cluster id for the Object.
+     * @return Returns the sub cluster id.
+     */
+    @Column(name = "subcluster_id", length = 80)
+    public String getSubclusterId() {
+        return this.subclusterId;
+    }
+
+    /**
+     * Set the sub cluster id for the Object.
+     * @param newSubclusterId The sub cluster id to set.
+     */
+    public void setSubclusterId(final String newSubclusterId) {
+        this.subclusterId = newSubclusterId;
+    }
+
+}
+
+
