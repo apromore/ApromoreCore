b33c05bec0ee04103ae19b3f2b1dba726a37fb06
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-08-05 23:08:47 +0000

    Added upload.sh, a unix-style alternative to upload.bat

---
+++ 0.3/Extras/upload.sh
@@ -0,0 +1,6 @@
+mvn install:install-file -Dfile=jbpt.jar -DgroupId=org.jbpt -DartifactId=jbpt -Dversion=0.2.197 -Dpackaging=jar -DgeneratePom=true 
+mvn install:install-file -Dfile=bpstruct.jar -DgroupId=ee.ut -DartifactId=bpstruct -Dversion=0.1.117 -Dpackaging=jar -DgeneratePom=true  
+mvn install:install-file -Dfile=rpsdag.jar -DgroupId=tu -DartifactId=rpsdag -Dversion=0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=taskmapping.jar -DgroupId=tu -DartifactId=taskmapping -Dversion=0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=jalopy-1.9.4-198.jar -DgroupId=triemax -DartifactId=jalopy -Dversion=1.9.4-198 -Dpackaging=jar 
+mvn install:install-file -Dfile=jalopy-maven-1.9.4-198.jar -DgroupId=triemax -DartifactId=jalopy-maven -Dversion=1.9.4-198 -Dpackaging=jar
