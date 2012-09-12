41f00a58fe16edac775ef3ce380aec97b73ea203
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-09-12 07:09:59 +0000

    Changed CanoniserResult class, propagated the API changes to the test suite.

--- Apromore-Core/apromore-service/src/main/java/org/apromore/canoniser/bpmn/CanoniserResult.java
+++ Apromore-Core/apromore-service/src/main/java/org/apromore/canoniser/bpmn/CanoniserResult.java
@@ -1,5 +1,59 @@
 package org.apromore.canoniser.bpmn;
 
+// Java 2 Standard classes
+import java.util.ArrayList;
+import java.util.List;
+
+// Local classes
+import org.apromore.anf.AnnotationsType;
+import org.apromore.cpf.CanonicalProcessType;
+
+/**
+ * Compound result type returned by the BPMN to CPF/ANF translation method {@link CanoniserDefinitions#canonise}.
+ *
+ * This is a collection of CPF/ANF pairs, each pair corresponding to a different process view occurring within the BPMN document.
+ *
+ * @author <a href="simon.raboczi@uqconnect.edu.au">Simon Raboczi</a>
+ * @since 0.4
+ */
+
 public class CanoniserResult {
-    // TODO
+
+    /**
+     * Processes in the order they appeared within the original BPMN document.
+     */
+    private List<CanonicalProcessType> cpfList = new ArrayList<>();
+
+    /**
+     * Each annotation applies to the process in the same position within the {@link #cpfList}.
+     */
+    private List<AnnotationsType> anfList = new ArrayList<>();
+
+    /**
+     * Add an associated pair of CPF and ANF structures to the result
+     */
+    public void put(CanonicalProcessType cpf, AnnotationsType anf) {
+        cpfList.add(cpf);
+        anfList.add(anf);
+    }
+
+    /**
+     * @param index
+     * @return the annotations for the <var>index</var>th process
+     */
+    public CanonicalProcessType getCpf(int i) { return cpfList.get(i); }
+
+    /**
+     * @param index
+     * @return the canonical form for the <var>index</var>th process
+     */
+    public AnnotationsType getAnf(int i) { return anfList.get(i); }
+
+    /**
+     * @return the number of CPF/ANF pairs in this result
+     */
+    public int size() {
+        assert anfList.size() == cpfList.size();
+        return cpfList.size();
+    }
 }
