daddd43691ca5ef53742bf2841a70994336a978f
felix.mannhardt@gmail.com <felix.mannhardt@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-09-20 07:36:32 +0000

    Added jbpt-0.2.314.jar to upload.sh

--- Extras/upload.sh
+++ Extras/upload.sh
@@ -1,4 +1,5 @@
 mvn install:install-file -Dfile=jbpt.jar -DgroupId=org.jbpt -DartifactId=jbpt -Dversion=0.2.197 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=jbpt-0.2.314.jar -DgroupId=org.jbpt -DartifactId=jbpt -Dversion=0.2.314 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=bpstruct.jar -DgroupId=ee.ut -DartifactId=bpstruct -Dversion=0.1.117 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=rpsdag.jar -DgroupId=tu -DartifactId=rpsdag -Dversion=0.1 -Dpackaging=jar
 mvn install:install-file -Dfile=taskmapping.jar -DgroupId=tu -DartifactId=taskmapping -Dversion=0.1 -Dpackaging=jar
