8c6fc31361dc6851b6ab49404d5f0035e0d83eb6
felix.mannhardt@gmail.com <felix.mannhardt@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-09-21 13:39:27 +0000

    Removed CanoniserException and CanonicalFormatNotFoundException from apromore-service as it is now in the Canoniser API, Fixed bpmn/pom.xml by using the same JAXB version as apromore-service, Added Spring Framework dependency to plugin-parent/pom.xml as all Plugins will be Spring OSGi components

--- Apromore-Plugins/plugin-canoniser/yawl/src/test/java/org/apromore/canoniser/yawl/BaseYAWL2CPFTest.java
+++ Apromore-Plugins/plugin-canoniser/yawl/src/test/java/org/apromore/canoniser/yawl/BaseYAWL2CPFTest.java
@@ -36,9 +36,9 @@ import org.xml.sax.SAXException;
 
 /**
  * Base class for all Pattern based Unit tests
- * 
+ *
  * @author <a href="felix.mannhardt@smail.wir.h-brs.de">Felix Mannhardt (Bonn-Rhein-Sieg University oAS)</a>
- * 
+ *
  */
 public abstract class BaseYAWL2CPFTest {
 
