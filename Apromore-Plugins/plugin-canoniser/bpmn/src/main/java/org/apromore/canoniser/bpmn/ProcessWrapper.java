80df693e55f28e7c197635915b1a9af8ca1f0020
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-10-08 06:03:39 +0000

    BPMN20Canoniser now extends DefaultAbstractCanoniser rather than implementing Canoniser directly. Implemented BPMN20Canoniser.createInitialNativeFormat. Added tests to cover more methods in BPMN20Canoniser.

---
+++ Apromore-Plugins/plugin-canoniser/bpmn/src/main/java/org/apromore/canoniser/bpmn/ProcessWrapper.java
@@ -0,0 +1,67 @@
+package org.apromore.canoniser.bpmn;
+
+// Java 2 Standard packages
+import java.util.List;
+import javax.xml.bind.JAXBElement;
+
+// Local packages
+import org.omg.spec.bpmn._20100524.model.TArtifact;
+import org.omg.spec.bpmn._20100524.model.TFlowElement;
+import org.omg.spec.bpmn._20100524.model.TLaneSet;
+import org.omg.spec.bpmn._20100524.model.TProcess;
+import org.omg.spec.bpmn._20100524.model.TSubProcess;
+
+/**
+ * Wrapper to provide a common interface to both {@link TProcess} and {@link TSubProcess}.
+ *
+ * @author <a href="mailto:simon.raboczi@uqconnect.edu.au">Simon Raboczi</a>
+ * @since 0.4
+ */
+public class ProcessWrapper {
+
+    private final String id;
+    private final List<JAXBElement<? extends TArtifact>> artifact;
+    private final List<JAXBElement<? extends TFlowElement>> flowElement;
+    private final List<TLaneSet> laneSet;
+
+    // Constructors
+
+    /**
+     * Wrap a {@link TProcess}.
+     *
+     * @param process  wrapped instance
+     */
+    public ProcessWrapper(final TProcess process) {
+        id          = process.getId();
+        artifact    = process.getArtifact();
+        flowElement = process.getFlowElement();
+        laneSet     = process.getLaneSet();
+    }
+
+    /**
+     * Wrap a {@link TSubProcess}.
+     *
+     * @param subprocess  wrapped instance
+     * @param id  identifier to be used for the implicit process within the subprocess
+     */
+    public ProcessWrapper(final TSubProcess subprocess, final String id) {
+        this.id     = id;
+        artifact    = subprocess.getArtifact();
+        flowElement = subprocess.getFlowElement();
+        laneSet     = subprocess.getLaneSet();
+    }
+
+    // Accessor methods
+
+    /** @return <code>id</code> property */
+    public String getId() { return id; }
+
+    /** @return <code>artifact</code> property */
+    public List<JAXBElement<? extends TArtifact>> getArtifact() { return artifact; }
+
+    /** @return <code>flowElement</code> property */
+    public List<JAXBElement<? extends TFlowElement>> getFlowElement() { return flowElement; }
+
+    /** @return <code>laneSet</code> property */
+    public List<TLaneSet> getLaneSet() { return laneSet; }
+}
