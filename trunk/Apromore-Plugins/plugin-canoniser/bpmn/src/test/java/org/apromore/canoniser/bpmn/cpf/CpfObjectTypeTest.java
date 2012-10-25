0a914fd9858e477edd40a3c07132aa89945b1af6
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-10-25 04:44:59 +0000

    Moved constants for the locations of the test directories into TestConstants. Rename CPF Objects if their BPMN equivalents within a single CPF Net have duplicate names.

---
+++ trunk/Apromore-Plugins/plugin-canoniser/bpmn/src/test/java/org/apromore/canoniser/bpmn/cpf/CpfObjectTypeTest.java
@@ -0,0 +1,56 @@
+package org.apromore.canoniser.bpmn.cpf;
+
+// Third party packages
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Test suite for {@link CpfObjectType}.
+ *
+ * @author <a href="mailto:simon.raboczi@uqconnect.edu.au">Simon Raboczi</a>
+ */
+public class CpfObjectTypeTest {
+
+    /** Test instance. */
+    CpfObjectType object;
+
+    /** Populate {@link #object} with a freshly-constructed {@link CpfObjectType}. */
+    @Before
+    public void initializeObject() {
+        object = (CpfObjectType) new ObjectFactory().createObjectType();
+    }
+
+    /**
+     * Test {@link CpfTaskType#isIsCollection) and {@link CpfTaskType#setIsCollection).
+     */
+    @Test
+    public void testIsCollection() throws Exception {
+        assertFalse(object.isIsCollection());
+
+        object.setIsCollection(true);
+        assertTrue(object.isIsCollection());
+
+        object.setIsCollection(false);
+        assertFalse(object.isIsCollection());
+    }
+     
+    /**
+     * Test {@link CpfTaskType#getOriginalName) and {@link CpfTaskType#setOriginalName).
+     */
+    @Test
+    public void testOriginalName() throws Exception {
+        assertNull(object.getOriginalName());
+
+        object.setOriginalName("test");
+        assertEquals("test", object.getOriginalName());
+
+        object.setOriginalName(null);
+        assertNull(object.getOriginalName());
+    }
+}
