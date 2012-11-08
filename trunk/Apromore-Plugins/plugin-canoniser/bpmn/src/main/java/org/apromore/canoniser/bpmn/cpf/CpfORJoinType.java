d753ace85da9c533045d820eaae63e6b385a53c6
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-11-08 09:47:27 +0000

    CpfRoutingType subclasses inherit implementation from CpfNodeTypeImpl.

--- trunk/Apromore-Plugins/plugin-canoniser/bpmn/src/main/java/org/apromore/canoniser/bpmn/cpf/CpfORJoinType.java
+++ trunk/Apromore-Plugins/plugin-canoniser/bpmn/src/main/java/org/apromore/canoniser/bpmn/cpf/CpfORJoinType.java
@@ -1,38 +1,35 @@
 package org.apromore.canoniser.bpmn.cpf;
 
 // Java 2 Standard packages
-import java.util.HashSet;
 import java.util.Set;
 
 // Local packages
-import org.apromore.cpf.EdgeType;
 import org.apromore.cpf.ORJoinType;
+import org.apromore.cpf.EdgeType;
 
 /**
- * CPF 0.6 task with convenience methods.
+ * CPF 1.0 OR join routing with convenience methods.
  *
  * @author <a href="mailto:simon.raboczi@uqconnect.edu.au">Simon Raboczi</a>
  * @since 0.4
  */
-public class CpfORJoinType extends ORJoinType implements CpfNodeType {
+public class CpfORJoinType extends ORJoinType implements CpfRoutingType {
 
-    /** Incoming edges. */
-    private Set<EdgeType> incomingEdges = new HashSet<EdgeType>();  // TODO - diamond operator
+    /** Secondary superclass. */
+    private final CpfNodeTypeImpl super2;
 
-    /** Outgoing edges. */
-    private Set<EdgeType> outgoingEdges = new HashSet<EdgeType>();  // TODO - diamond operator
-
-    /**
-     * @return every edge which has this node as its target
-     */
-    public Set<EdgeType> getIncomingEdges() {
-        return incomingEdges;
+    /** Constructor. */
+    public CpfORJoinType() {
+        super2 = new CpfNodeTypeImpl();
     }
 
-    /**
-     * @return every edge which has this node as its source
-     */
+    /** {@inheritDoc} */
+    public Set<EdgeType> getIncomingEdges() {
+        return super2.getIncomingEdges();
+    }
+
+    /** {@inheritDoc} */
     public Set<EdgeType> getOutgoingEdges() {
-        return outgoingEdges;
+        return super2.getOutgoingEdges();
     }
 }
