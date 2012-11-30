62f826abc876460bb67e10ba947f491127885f9b
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-11-30 06:32:13 +0000

    Partial rehabilitation of the EPML canoniser tests.

---
+++ Apromore-Plugins/plugin-canoniser/epml/src/test/java/org/apromore/canoniser/epml/RangeTest.java
@@ -0,0 +1,10 @@
+package org.apromore.canoniser.epml;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class RangeTest extends AbstractTest {
+
+    @Test public void testCanonise()   throws Exception { testCanonise("range"); }
+    @Ignore @Test public void testDeCanonise() throws Exception { testDeCanonise("range"); }
+}
