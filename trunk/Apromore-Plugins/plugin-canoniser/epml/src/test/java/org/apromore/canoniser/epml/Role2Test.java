8eb276b5616303eef1700455e7a6bfdf2f0261ea
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-11-30 06:32:13 +0000

    Partial rehabilitation of the EPML canoniser tests.

---
+++ trunk/Apromore-Plugins/plugin-canoniser/epml/src/test/java/org/apromore/canoniser/epml/Role2Test.java
@@ -0,0 +1,10 @@
+package org.apromore.canoniser.epml;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class Role2Test extends AbstractTest {
+
+    @Ignore @Test public void testCanonise()   throws Exception { testCanonise("role_2"); }
+    @Ignore @Test public void testDeCanonise() throws Exception { testDeCanonise("role_2"); }
+}
