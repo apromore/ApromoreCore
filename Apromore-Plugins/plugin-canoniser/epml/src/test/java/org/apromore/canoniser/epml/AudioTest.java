5d2a11c7b882ff3dcecc87cc269b9788b6e0348b
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2012-12-07 02:57:31 +0000

    EPML canonizer generates required CPF/ANF root uri attribute. When an an EPML range relation has no mapped id, a log message warning is produced rather than a NullPointerException. Added Audio.epml test file.

---
+++ Apromore-Plugins/plugin-canoniser/epml/src/test/java/org/apromore/canoniser/epml/AudioTest.java
@@ -0,0 +1,8 @@
+package org.apromore.canoniser.epml;
+
+import org.junit.Test;
+
+public class AudioTest extends AbstractTest {
+
+    @Test public void testCanonise() throws Exception { testCanonise("Audio"); }
+}
