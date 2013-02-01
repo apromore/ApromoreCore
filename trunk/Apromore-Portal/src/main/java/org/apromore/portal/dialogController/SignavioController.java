4d87dba8be767683441ff848d48231b7832a1320
felix.mannhardt@gmail.com <felix.mannhardt@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-02-01 04:07:10 +0000

    [Apromore-Portal]. Auto Layout only, if editSession.isAnnotationAvailable

--- trunk/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/SignavioController.java
+++ trunk/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/SignavioController.java
@@ -64,7 +64,7 @@ public class SignavioController extends BaseController {
             param.put(JSON_DATA, data.replace("\n", "").trim());
             param.put("url", getURL(editSession.getNativeType()));
             param.put("importPath", getImportPath(editSession.getNativeType()));
-			param.put("doAutoLayout", "false");
+			param.put("doAutoLayout", editSession.isWithAnnotation());
             Executions.getCurrent().pushArg(param);
         } catch (Exception e) {
             e.printStackTrace();
