669699db92c522397d655322eda28145ecf3de5a
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-02-11 06:09:52 +0000

    Implemented command line process upload.

--- trunk/Extras/cpfImporter/src/main/java/org/apromore/tools/cpfimporter/Import.java
+++ trunk/Extras/cpfImporter/src/main/java/org/apromore/tools/cpfimporter/Import.java
@@ -1,7 +1,14 @@
 package org.apromore.tools.cpfimporter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Collections;
+import java.util.Set;
+
 import org.apromore.manager.client.ManagerService;
+import org.apromore.model.ImportProcessResultType;
 import org.apromore.model.UsernamesType;
+import org.apromore.plugin.property.RequestParameterType;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,19 +22,21 @@ public final class Import {
     private AutowireCapableBeanFactory fac;
 
     /* The Canonical Process Importer Starting point. */
-    public static void main(String[] args) {
-        new Import();
+    public static void main(String[] args) throws Exception {
+        new Import(args[0]);
     }
 
 
     /**
      * Default Constructor.
      */
-    public Import() {
+    public Import(final String arg0) throws Exception {
         ctx = new ClassPathXmlApplicationContext("classpath*:spring/applicationContext-managerClient.xml");
         fac = ctx.getAutowireCapableBeanFactory();
 
-        ShowAllUsers();
+        //ShowAllUsers();
+        //CreateFolder(arg0);
+        UploadProcess(new File(arg0));
     }
 
     /**
@@ -41,6 +50,7 @@ public final class Import {
 
 
     /* Test method to show we connect to the server. */
+    /*
     private void ShowAllUsers() {
         ManagerService manager = (ManagerService) getBean("managerClient");
 
@@ -49,5 +59,31 @@ public final class Import {
             System.out.println("User Found: " + user);
         }
     }
+    */
 
+    private void CreateFolder(final String name) throws Exception {
+        final ManagerService manager = (ManagerService) getBean("managerClient");
+        final String user = "raboczi";
+
+        manager.createFolder(user, name, 0);
+    }
+
+    private void UploadProcess(final File file) throws Exception {
+        final ManagerService manager = (ManagerService) getBean("managerClient");
+        final String user = "raboczi";
+        final Set<RequestParameterType<?>> noCanoniserParameters = Collections.<RequestParameterType<?>>emptySet();
+
+        ImportProcessResultType result = manager.importProcess(
+            user,
+            "AML fragment",
+            file.getName(),
+            Double.valueOf("1.0"),  // versionNumber,
+            new FileInputStream(file),
+            "domain",
+            "documentation",
+            "created",
+            "lastUpdate",
+            noCanoniserParameters
+        );
+    }
 }
