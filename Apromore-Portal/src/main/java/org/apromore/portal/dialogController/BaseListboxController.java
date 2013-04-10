bb11b737a06ae23f388a06a50e96a3ff8a088e42
cam.james@gmail.com <cam.james@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-04-10 23:30:41 +0000

    Bug fixes - Cameron James Fixed UI buttons for add folders and security so they actually work.

--- Apromore-Portal/src/main/java/org/apromore/portal/dialogController/BaseListboxController.java
+++ Apromore-Portal/src/main/java/org/apromore/portal/dialogController/BaseListboxController.java
@@ -1,15 +1,25 @@
 package org.apromore.portal.dialogController;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.apromore.model.FolderType;
+import org.apromore.portal.common.UserSessionManager;
+import org.apromore.portal.dialogController.workspaceOptions.AddFolderController;
+import org.apromore.portal.exception.DialogException;
+import org.apromore.portal.exception.ExceptionAllUsers;
+import org.apromore.portal.exception.ExceptionDomains;
+import org.apromore.portal.exception.ExceptionFormats;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.SuspendNotAllowedException;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.ListitemRenderer;
+import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Paging;
 
 public abstract class BaseListboxController extends BaseController {
@@ -19,6 +29,9 @@ public abstract class BaseListboxController extends BaseController {
     private final Listbox listBox;
 
     private final MainController mainController; // the main controller
+    private AddFolderController addFolderController;
+    private CreateProcessController createController;
+    private SecuritySetupController securitySetupController;
 
     private final Paging pg;
 
@@ -26,9 +39,14 @@ public abstract class BaseListboxController extends BaseController {
     private final Button selectAllB;
     private final Button unselectAllB;
     private final Button refreshB;
+    private final Button btnAddFolder;
+    private final Button btnAddProcess;
+    private final Button btnRenameFolder;
+    private final Button btnRemoveFolder;
+    private final Button btnSecurity;
 
-    public BaseListboxController(MainController mainController,
-                                 String componentId, ListitemRenderer itemRenderer) {
+
+    public BaseListboxController(MainController mainController, String componentId, ListitemRenderer itemRenderer) {
         super();
         setHflex("true");
         setVflex("true");
@@ -44,6 +62,11 @@ public abstract class BaseListboxController extends BaseController {
         this.unselectAllB = (Button) mainController.getFellow("unselectAllB");
         this.selectAllB = (Button) mainController.getFellow("selectAllB");
         this.refreshB = (Button) mainController.getFellow("refreshB");
+        this.btnAddFolder = (Button) mainController.getFellow("btnAddFolder");
+        this.btnAddProcess = (Button) mainController.getFellow("btnAddProcess");
+        this.btnRenameFolder = (Button) mainController.getFellow("btnRenameFolder");
+        this.btnRemoveFolder = (Button) mainController.getFellow("btnRemoveFolder");
+        this.btnSecurity = (Button) mainController.getFellow("btnSecurity");
 
         getListBox().setItemRenderer(itemRenderer);
         getListBox().setModel(new ListModelList());
@@ -79,6 +102,36 @@ public abstract class BaseListboxController extends BaseController {
             @Override
             public void onEvent(Event event) throws Exception {
                 refreshContent();
+            }
+        });
+
+        this.btnAddFolder.addEventListener("onClick", new EventListener() {
+            public void onEvent(Event event) throws Exception {
+                addFolder();
+            }
+        });
+
+        this.btnAddProcess.addEventListener("onClick", new EventListener() {
+            public void onEvent(Event event) throws Exception {
+                addProcess();
+            }
+        });
+
+        this.btnRenameFolder.addEventListener("onClick", new EventListener() {
+            public void onEvent(Event event) throws Exception {
+                renameFolder();
+            }
+        });
+
+        this.btnRemoveFolder.addEventListener("onClick", new EventListener() {
+            public void onEvent(Event event) throws Exception {
+                removeFolder();
+            }
+        });
+
+        this.btnSecurity.addEventListener("onClick", new EventListener() {
+            public void onEvent(Event event) throws Exception {
+                security();
             }
         });
     }
@@ -119,6 +172,106 @@ public abstract class BaseListboxController extends BaseController {
         }
         getListBox().clearSelection();
         getListBox().setSelectedItems(reveredSet);
+    }
+
+    protected void addFolder() throws InterruptedException {
+        this.mainController.eraseMessage();
+        try {
+            this.addFolderController = new AddFolderController(mainController, 0, "");
+        } catch (DialogException e) {
+            Messagebox.show(e.getMessage(), "Attention", Messagebox.OK, Messagebox.ERROR);
+        }
+    }
+
+    protected void addProcess() throws InterruptedException {
+        this.mainController.eraseMessage();
+        try {
+            this.createController = new CreateProcessController(this.mainController, this.mainController.getNativeTypes());
+        } catch (SuspendNotAllowedException e) {
+            Messagebox.show(e.getMessage(), "Attention", Messagebox.OK, Messagebox.ERROR);
+        } catch (InterruptedException e) {
+            Messagebox.show(e.getMessage(), "Attention", Messagebox.OK, Messagebox.ERROR);
+        } catch (ExceptionDomains e) {
+            String message;
+            if (e.getMessage() == null) {
+                message = "Couldn't retrieve domains reference list.";
+            } else {
+                message = e.getMessage();
+            }
+            Messagebox.show(message, "Attention", Messagebox.OK, Messagebox.ERROR);
+        } catch (ExceptionFormats e) {
+            String message;
+            if (e.getMessage() == null) {
+                message = "Couldn't retrieve formats reference list.";
+            } else {
+                message = e.getMessage();
+            }
+            Messagebox.show(message, "Attention", Messagebox.OK, Messagebox.ERROR);
+        } catch (ExceptionAllUsers e) {
+            String message;
+            if (e.getMessage() == null) {
+                message = "Couldn't retrieve users reference list.";
+            } else {
+                message = e.getMessage();
+            }
+            Messagebox.show(message, "Attention", Messagebox.OK, Messagebox.ERROR);
+        }
+    }
+
+    protected void renameFolder() throws InterruptedException {
+        this.mainController.eraseMessage();
+        try {
+            List<Integer> folderIds = UserSessionManager.getSelectedFolderIds();
+
+            if (folderIds.size() == 1) {
+                int selectedFolderId = folderIds.get(0);
+                String selectedFolderName = "";
+                List<FolderType> availableFolders = UserSessionManager.getCurrentFolder() == null || UserSessionManager.getCurrentFolder().getId() == 0 ? UserSessionManager.getTree() : UserSessionManager.getCurrentFolder().getFolders();
+                for (FolderType folder : availableFolders) {
+                    if (folder.getId() == selectedFolderId) {
+                        selectedFolderName = folder.getFolderName();
+                        break;
+                    }
+                }
+
+                this.addFolderController = new AddFolderController(mainController, folderIds.get(0), selectedFolderName);
+            } else if (folderIds.size() > 1) {
+                Messagebox.show("Only one item can be renamed at the time.", "Attention", Messagebox.OK, Messagebox.ERROR);
+            } else {
+                Messagebox.show("Please select item to rename", "Attention", Messagebox.OK, Messagebox.ERROR);
+            }
+        } catch (DialogException e) {
+            Messagebox.show(e.getMessage(), "Attention", Messagebox.OK, Messagebox.ERROR);
+        }
+    }
+
+    protected void removeFolder() throws Exception {
+        Messagebox.show("Are you sure you want to delete selected item(s)?", "Prompt", Messagebox.YES | Messagebox.NO, Messagebox.QUESTION, new EventListener() {
+            public void onEvent(Event evt) throws Exception {
+                switch (((Integer) evt.getData())) {
+                    case Messagebox.YES:
+                        MainController mainController = UserSessionManager.getMainController();
+                        List<Integer> folderIds = UserSessionManager.getSelectedFolderIds();
+                        for (int folderId : folderIds) {
+                            mainController.getService().deleteFolder(folderId);
+                        }
+                        mainController.getMenu().deleteSelectedProcessVersions();
+                        mainController.loadWorkspace();
+                        break;
+                    case Messagebox.NO:
+                        break;
+                }
+            }
+        });
+    }
+
+    protected void security() throws InterruptedException {
+        this.mainController.eraseMessage();
+        try {
+            this.securitySetupController = new SecuritySetupController(this.mainController);
+        } catch (DialogException e) {
+            Messagebox.show(e.getMessage(), "Attention", Messagebox.OK, Messagebox.ERROR);
+        }
     }
 
     public MainController getMainController() {
