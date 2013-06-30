ab2896f22d61acd0821163e71d2d422872aaf0f4
cam.james@gmail.com <cam.james@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-06-30 03:20:11 +0000

    General - Cameron James updated some tests so they get executed.

--- Apromore-Plugins/plugin-annotation/core/api/src/test/java/org/apromore/annotation/DefaultAnnotationProcessorUnitTest.java
+++ Apromore-Plugins/plugin-annotation/core/api/src/test/java/org/apromore/annotation/DefaultAnnotationProcessorUnitTest.java
@@ -10,7 +10,7 @@ import org.apromore.cpf.CanonicalProcessType;
 import org.junit.Before;
 import org.junit.Test;
 
-public class DefaultAbstractAnnotationProcessorTest {
+public class DefaultAnnotationProcessorUnitTest {
 
     private DefaultAbstractAnnotationProcessor defaultAbstractAnnotationProcessor;
 
