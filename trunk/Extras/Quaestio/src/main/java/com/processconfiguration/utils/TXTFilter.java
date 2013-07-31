3651019b70ef96582a00f7827a2d01d79d6235c1
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-07-31 06:14:27 +0000

    First commit for the Quaestio questionnaire application.  This is version 0.9, slightly reorganized to match the Maven directory conventions, with the Java sources converted to UTF8, and with a POM file added.  The executable JAR needs to be run from the Quaestio directory because the BDDC executables are sought on the local filesystem.

---
+++ trunk/Extras/Quaestio/src/main/java/com/processconfiguration/utils/TXTFilter.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright © 2006-2011, www.processconfiguration.com
+ *   
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *   
+ * Contributors:
+ *      Marcello La Rosa - initial API and implementation, subsequent revisions
+ *      Florian Gottschalk - individualizer for YAWL
+ *      Possakorn Pitayarojanakul - integration with Configurator and Individualizer
+ ******************************************************************************/
+/**
+ * Copyright © 2006-2009, Marcello La Rosa (marcello.larosa@gmail.com)
+ *   
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *      Marcello La Rosa - initial API and implementation
+ */
+package com.processconfiguration.utils;
+
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+
+
+public class TXTFilter extends FileFilter {
+
+	@Override
+	public boolean accept(File f) {
+		if (f.isDirectory()) {
+			return true;//enable directories
+		}
+
+		String extension = Utils.getExtension(f);
+		if (extension != null) {
+			if (extension.equals(Utils.txt))
+				return true;
+			else {
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+
+	@Override
+	public String getDescription() {
+		return "Text document (*.txt)";
+	}
+
+}
