420f803b00f498df0e394d30d73c9e25948032fe
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-08-13 06:02:49 +0000

    Modifications to Apromore-Editor to display Configurable BPMN

--- Apromore-Editor/platform extensions/bpmn20xmlbasic/src/de/hpi/bpmn2_0/factory/node/ChoreographyParticipantFactory.java
+++ Apromore-Editor/platform extensions/bpmn20xmlbasic/src/de/hpi/bpmn2_0/factory/node/ChoreographyParticipantFactory.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (c) 2009
  * Philipp Giese, Sven Wagner-Boysen
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,45 +23,49 @@
 
 package de.hpi.bpmn2_0.factory.node;
 
+import java.util.Map;
+
+import org.oryxeditor.server.diagram.generic.GenericShape;
+
 import de.hpi.bpmn2_0.exceptions.BpmnConverterException;
 import de.hpi.bpmn2_0.factory.AbstractBpmnFactory;
 import de.hpi.bpmn2_0.factory.BPMNElement;
 import de.hpi.bpmn2_0.model.BaseElement;
 import de.hpi.bpmn2_0.model.bpmndi.di.DiagramElement;
-import org.oryxeditor.server.diagram.generic.GenericShape;
 
 /**
  * @author Sven
+ *
  */
 public class ChoreographyParticipantFactory extends AbstractBpmnFactory {
 
-    /* (non-Javadoc)
-      * @see de.hpi.bpmn2_0.factory.AbstractBpmnFactory#createBpmnElement(org.oryxeditor.server.diagram.Shape, de.hpi.bpmn2_0.factory.BPMNElement)
-      */
-    // @Override
-    public BPMNElement createBpmnElement(GenericShape shape, BPMNElement parent)
-            throws BpmnConverterException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	/* (non-Javadoc)
+	 * @see de.hpi.bpmn2_0.factory.AbstractBpmnFactory#createBpmnElement(org.oryxeditor.server.diagram.Shape, de.hpi.bpmn2_0.factory.BPMNElement)
+	 */
+	// @Override
+	public BPMNElement createBpmnElement(GenericShape shape, BPMNElement parent, State state)
+			throws BpmnConverterException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    /* (non-Javadoc)
-      * @see de.hpi.bpmn2_0.factory.AbstractBpmnFactory#createDiagramElement(org.oryxeditor.server.diagram.Shape)
-      */
-    // @Override
-    protected DiagramElement createDiagramElement(GenericShape shape) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	/* (non-Javadoc)
+	 * @see de.hpi.bpmn2_0.factory.AbstractBpmnFactory#createDiagramElement(org.oryxeditor.server.diagram.Shape)
+	 */
+	// @Override
+	protected DiagramElement createDiagramElement(GenericShape shape) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    /* (non-Javadoc)
-      * @see de.hpi.bpmn2_0.factory.AbstractBpmnFactory#createProcessElement(org.oryxeditor.server.diagram.Shape)
-      */
-    // @Override
-    protected BaseElement createProcessElement(GenericShape shape)
-            throws BpmnConverterException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	/* (non-Javadoc)
+	 * @see de.hpi.bpmn2_0.factory.AbstractBpmnFactory#createProcessElement(org.oryxeditor.server.diagram.Shape)
+	 */
+	// @Override
+	protected BaseElement createProcessElement(GenericShape shape)
+			throws BpmnConverterException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
