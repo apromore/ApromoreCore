ae40831143dd71ac137ecadca5ca71b2b93bf7da
cam.james@gmail.com <cam.james@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-11-12 06:06:25 +0000

    EDT-12 #time 2d Total work logged #comment #resolve BPStruct integration is complete, more testing needs to be done but have to move on to other tasks so resolving this one to open a new issue soon for making sure it works correctly.

--- Apromore-Editor/platform extensions/bpstruct/src/org/apromore/common/converters/bpstruct/ProcessModelToBasicDiagram.java
+++ Apromore-Editor/platform extensions/bpstruct/src/org/apromore/common/converters/bpstruct/ProcessModelToBasicDiagram.java
@@ -1,14 +1,8 @@
 package org.apromore.common.converters.bpstruct;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import de.hpi.bpt.process.ControlFlow;
-import de.hpi.bpt.process.Gateway;
-import de.hpi.bpt.process.Node;
+import de.hpi.bpt.process.*;
 import de.hpi.bpt.process.Process;
-import de.hpi.bpt.process.Task;
+import org.apache.log4j.Logger;
 import org.oryxeditor.server.diagram.Bounds;
 import org.oryxeditor.server.diagram.Point;
 import org.oryxeditor.server.diagram.StencilSetReference;
@@ -17,12 +11,16 @@ import org.oryxeditor.server.diagram.basic.BasicEdge;
 import org.oryxeditor.server.diagram.basic.BasicNode;
 import org.oryxeditor.server.diagram.basic.BasicShape;
 
+import java.util.*;
+
 /**
  * Converts the JBPT Process Model into a Editor Basic Diagram.
  *
  * @author Cameron James
  */
 public class ProcessModelToBasicDiagram {
+
+    private static final Logger LOGGER = Logger.getLogger(ProcessModelToBasicDiagram.class);
 
     private static final String AND = "AND";
     private static final String AND_CONNECTOR = "ParallelGateway";
@@ -32,6 +30,10 @@ public class ProcessModelToBasicDiagram {
     private static final String OR_CONNECTOR = "Inclusive_Databased_Gateway";
     private static final String NAME = "name";
     private static final String GATEWAY_TYPE = "gatewaytype";
+    private static final String SEQUENCE_FLOW = "SequenceFlow";
+
+    private Map<String, BasicShape> shapes = new HashMap<>();
+
 
     /**
      * Does the conversion from Process Model (JBPT) into a Basic Diagram (sIgnavio).
@@ -44,8 +46,7 @@ public class ProcessModelToBasicDiagram {
 
         configureDiagram(diagram, process);
 
-        addTasksToDiagram(diagram, process);
-        addGatewaysToDiagram(diagram, process);
+        addNodesToDiagram(diagram, process);
         addEdgesToDiagram(diagram, process);
 
         return diagram;
@@ -53,39 +54,39 @@ public class ProcessModelToBasicDiagram {
 
 
     /* Add all the tasks to diagram. */
-    private void addTasksToDiagram(BasicDiagram diagram, Process process) {
-        for (Task task : process.getTasks()) {
-            diagram.addChildShape(configureBasicNode(task, process));
-        }
-    }
-
-    /* Add all the gateways to the diagram. */
-    private void addGatewaysToDiagram(BasicDiagram diagram, Process process) {
-        for (Gateway gateway : process.getGateways()) {
-            diagram.addChildShape(configureBasicGateway(gateway, process));
+    private void addNodesToDiagram(BasicDiagram diagram, Process process) {
+        BasicShape shape;
+        for (Node node : process.getNodes()) {
+            shape = addNode(node, process);
+            shapes.put(node.getId(), shape);
+            diagram.addChildShape(shape);
         }
     }
 
     /* Add all the edges to the diagram. */
     private void addEdgesToDiagram(BasicDiagram diagram, Process process) {
+        BasicShape shape;
         for (ControlFlow edge : process.getControlFlow()) {
-            diagram.addChildShape(configureBasicEdge(edge, process));
+            shape = configureBasicEdge(edge, shapes);
+            shapes.put(edge.getId(), shape);
+            diagram.addChildShape(shape);
         }
     }
 
 
     /* Configures the basic details of the Diagram. */
     private void configureDiagram(BasicDiagram diagram, Process process) {
-        diagram.setResourceId("oryx-canvas123");
+        diagram.setResourceId("canvas");
         diagram.setStencilId("BPMNDiagram");
 
         // Properties
         diagram.setProperty(NAME, process.getName());
+        diagram.setProperty("documentation", "");
         diagram.setProperty("targetnamespace", "http://www.signavio.com/bpmn20");
         diagram.setProperty("expressionlanguage", "http://www.w3.org/1999/XPath");
         diagram.setProperty("typelanguage", "http://www.w3.org/2001/XMLSchema");
         diagram.setProperty("orientation", "vertical");
-        diagram.setStencilsetRef(new StencilSetReference("http://b3mn.org/stencilset/bpmn2.0#", "/editor/editor/stencilsets//bpmn2.0/bpmn2.0.json"));
+        diagram.setStencilsetRef(new StencilSetReference("http://b3mn.org/stencilset/bpmn2.0#", "/signaviocore/editor/stencilsets//bpmn2.0/bpmn2.0.json"));
 
         List<String> ssExtensions = new ArrayList<>();
         ssExtensions.add("http://oryx-editor.org/stencilsets/extensions/bpmn2.0basicsubset#");
@@ -94,117 +95,123 @@ public class ProcessModelToBasicDiagram {
     }
 
 
-    /* Configures the basic details of the Node. */
-    private BasicShape configureBasicNode(Node node, Process process) {
-        BasicNode node1 = new BasicNode(node.getId());
-
-        if (isStartEvent(node, process.getControlFlow())) {
-            node1.setStencilId("StartNoneEvent");
-            node1.setProperty("bgcolor", "#ffffff");
-            node1.setProperty("bordercolor", "#000000");
-            node1.setProperty("trigger", "None");
-        } else if (isEndEvent(node, process.getControlFlow())) {
-            node1.setStencilId("EndNoneEvent");
-            node1.setProperty("bgcolor", "#ffffff");
-            node1.setProperty("bordercolor", "#000000");
-            node1.setProperty("trigger", "None");
+    /* Work out what type it is and send it on it's way to get added. */
+    private BasicShape addNode(Node node, Process process) {
+        if (node instanceof Gateway) {
+            return addGateway((Gateway) node);
+        } else if (node instanceof Task) {
+            return addTask((Task) node, process);
         } else {
-            node1.setStencilId("Task");
-            node1.setProperty(NAME, node.getName());
-            node1.setProperty("startquantity", 1);
-            node1.setProperty("completionquantity", 1);
-            node1.setProperty("isforcompensation", false);
-            node1.setProperty("tasktype", "None");
-            node1.setProperty("bgcolor", "#ffffcc");
-            node1.setProperty("bordercolor", "#000000");
-            node1.setProperty("looptype", "None");
-            node1.setProperty("behavior", "all");
-            node1.setProperty("onebehavioreventref:", "signal");
-            node1.setProperty("nonebehavioreventref", "signal");
-            node1.setProperty("outmsgitemkind", "Information");
-            node1.setProperty("inmsgitemkind", "Information");
-            node1.setProperty("implementation", "webService");
+            System.out.println("Unable to determine Node type: " + node.getId() + " (" + node.toString() + ")");
         }
-
-        node1.setBounds(new Bounds(new Point(0,0), new Point(100,70)));
-
-        return node1;
+        return null;
     }
 
 
+    /* Configures the basic details of the task. */
+    private BasicShape addTask(Task task, Process process) {
+        BasicNode node = new BasicNode(task.getId());
+
+        if (isStartEvent(task, process.getControlFlow())) {
+            node.setStencilId("StartNoneEvent");
+            node.setProperty("bgcolor", "#ffffff");
+            node.setProperty("bordercolor", "#000000");
+            node.setProperty("trigger", "None");
+        } else if (isEndEvent(task, process.getControlFlow())) {
+            node.setStencilId("EndNoneEvent");
+            node.setProperty("bgcolor", "#ffffff");
+            node.setProperty("bordercolor", "#000000");
+            node.setProperty("trigger", "None");
+        } else {
+            node.setStencilId("Task");
+            node.setProperty(NAME, task.getName());
+            node.setProperty("startquantity", 1);
+            node.setProperty("completionquantity", 1);
+            node.setProperty("isforcompensation", false);
+            node.setProperty("tasktype", "None");
+            node.setProperty("bgcolor", "#ffffcc");
+            node.setProperty("bordercolor", "#000000");
+            node.setProperty("looptype", "None");
+            node.setProperty("behavior", "all");
+            node.setProperty("onebehavioreventref:", "signal");
+            node.setProperty("nonebehavioreventref", "signal");
+            node.setProperty("outmsgitemkind", "Information");
+            node.setProperty("inmsgitemkind", "Information");
+            node.setProperty("implementation", "webService");
+        }
+
+        node.setBounds(new Bounds(new Point(0,0), new Point(100,70)));
+
+        return node;
+    }
+
     /* Configures the basic details of the Gateway. */
-    private BasicShape configureBasicGateway(Gateway gateway, Process process) {
+    private BasicShape addGateway(Gateway gateway) {
         BasicNode node = new BasicNode(gateway.getId());
         node.setProperty(NAME, gateway.getName());
 
         if (gateway.isAND()) {
             node.setStencilId(AND_CONNECTOR);
             node.setProperty(GATEWAY_TYPE, AND);
-            node.setProperty("bordercolor", "#000000");
-            node.setProperty("bgcolor", "#ffffff");
-            node.setProperty("configurable", "false");
 
         } else if (gateway.isXOR()) {
             node.setStencilId(XOR_CONNECTOR);
             node.setProperty(GATEWAY_TYPE, XOR);
-            node.setProperty("bordercolor", "#000000");
-            node.setProperty("bgcolor", "#ffffff");
-            node.setProperty("configurable", "false");
             node.setProperty("xortype", "Data");
             node.setProperty("markervisible", true);
 
         } else if (gateway.isOR()) {
             node.setStencilId(OR_CONNECTOR);
             node.setProperty(GATEWAY_TYPE, OR);
-            node.setProperty("bordercolor", "#000000");
-            node.setProperty("bgcolor", "#ffffff");
-            node.setProperty("configurable", "false");
         }
 
+        node.setProperty("bordercolor", "#000000");
+        node.setProperty("bgcolor", "#ffffff");
+        node.setProperty("configurable", "false");
         node.setBounds(new Bounds(new Point(0,0), new Point(40,40)));
-        addNodesToBasicGateway(node, gateway, process);
 
         return node;
     }
 
 
     /* Configures the basic details of the Edge. */
-    private BasicShape configureBasicEdge(ControlFlow flow, Process process) {
+    private BasicShape configureBasicEdge(ControlFlow flow, Map<String, BasicShape> shapes) {
         BasicEdge edge = new BasicEdge(flow.getId());
 
-        edge.setStencilId("SequenceFlow");
+        edge.setStencilId(SEQUENCE_FLOW);
         edge.setProperty("conditiontype", "None");
         edge.setProperty("absentinconfiguration", "false");
         edge.setProperty("isimmediate", true);
         edge.setProperty("bordercolor", "#000000");
 
-        List<Point> points = new ArrayList<>();
-        points.add(new Point(0, 0));
-        points.add(new Point(0, 0));
-        edge.setDockers(points);
+        edge.connectToASource(shapes.get(flow.getSource().getId()));
+        edge.addDocker(determineDockerPoint(shapes.get(flow.getSource().getId()), true));
 
-        edge.connectToASource(configureBasicNode(flow.getSource(), process));
-        edge.connectToATarget(configureBasicNode(flow.getTarget(), process));
+        edge.connectToATarget(shapes.get(flow.getTarget().getId()));
+        edge.addDocker(determineDockerPoint(shapes.get(flow.getSource().getId()), false));
+
+        System.out.println("Edge: " + flow.getId() + " (" + flow.getSource().getId() + ", " + flow.getTarget().getId() + ")");
 
         return edge;
     }
 
+    /* Determine the docking point for this Node */
+    private Point determineDockerPoint(BasicShape shape, boolean source) {
+        double x = shape.getBounds().getHeight() / 2;
+        double y;
 
-
-    /* Gateway nodes has inbound or outbound nodes which must be specified. */
-    private void addNodesToBasicGateway(BasicNode node, Gateway gateway, Process process) {
-        for (ControlFlow flow : process.getControlFlow()) {
-            if (gateway.getId().equals(flow.getSource().getId())) {
-                // We have an outbound gateway node to specify
-                node.addOutgoingAndUpdateItsIncomings(configureBasicNode(flow.getTarget(), process));
-
-            } else if (gateway.getId().equals(flow.getTarget().getId())) {
-                // We have an inbound gateway node to specify
-                node.addIncomingAndUpdateItsOutgoings(configureBasicNode(flow.getSource(), process));
-            }
+        if (source) {
+            y = shape.getBounds().getLowerRight().getY();
+        } else {
+            y = shape.getBounds().getLowerLeft().getY();
         }
+
+        return new Point(x, y);
     }
 
+
+
+    /* Determine if this is a starting Node */
     private boolean isStartEvent(Node node, Collection<ControlFlow> flows) {
         boolean result = true;
         for (ControlFlow flow : flows) {
@@ -216,6 +223,7 @@ public class ProcessModelToBasicDiagram {
         return result;
     }
 
+    /* Determine if this is a ending Node */
     private boolean isEndEvent(Node node, Collection<ControlFlow> flows) {
         boolean result = true;
         for (ControlFlow flow : flows) {
--- Apromore-Portal/src/main/java/org/apromore/portal/dialogController/SecuritySetupController.java
+++ Apromore-Portal/src/main/java/org/apromore/portal/dialogController/SecuritySetupController.java
@@ -1,20 +1,20 @@
 package org.apromore.portal.dialogController;
 
+import org.apromore.portal.common.UserSessionManager;
 import org.apromore.portal.exception.DialogException;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zul.Window;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Igor
- * Date: 2/07/12
- * Time: 5:08 PM
- * To change this template use File | Settings | File Templates.
+ * Controller used to setup security for folders and processes.
+ *
+ * @author Igor
  */
 public class SecuritySetupController extends BaseController {
 
     private MainController mainController;
-    private FolderTreeController folderTreeController;
     private FindUsersController findUsersController;
     private PermissionsController permissionsController;
 
@@ -24,18 +24,17 @@ public class SecuritySetupController extends BaseController {
         try {
             final Window win = (Window) Executions.createComponents("/macros/securitySetup.zul", null, null);
 
+            FolderTreeController folderTreeController = new FolderTreeController(this, win);
             this.permissionsController = new PermissionsController(this, win);
-            this.folderTreeController = new FolderTreeController(this, win);
             this.findUsersController = new FindUsersController(this, win);
 
             win.doModal();
 
-//            win.addEventListener("onClose",
-//                    new EventListener() {
-//                        public void onEvent(Event event) throws Exception {
-//                            UserSessionManager.getMainController().loadWorkspace();
-//                        }
-//                    });
+            win.addEventListener("onClose", new EventListener<Event>() {
+                public void onEvent(Event event) throws Exception {
+                    UserSessionManager.getMainController().loadWorkspace();
+                }
+            });
 
         } catch (Exception e) {
             throw new DialogException("Error in controller: " + e.getMessage());
