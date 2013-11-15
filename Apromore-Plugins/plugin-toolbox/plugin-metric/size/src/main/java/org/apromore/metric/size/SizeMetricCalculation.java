7abdfc5cfacf9b447ce78100c7ed09a196e5f214
cam.james@gmail.com <cam.james@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-11-15 01:32:28 +0000

    PLUG-4 #time 4h #resolved #comment The metric for size has been implemented

---
+++ Apromore-Plugins/plugin-toolbox/plugin-metric/size/src/main/java/org/apromore/metric/size/SizeMetricCalculation.java
@@ -0,0 +1,23 @@
+package org.apromore.metric.size;
+
+import org.apromore.graph.canonical.Canonical;
+import org.apromore.plugin.PluginRequest;
+import org.apromore.plugin.metric.DefaultAbstractMetricProcessor;
+import org.apromore.plugin.metric.result.MetricPluginResult;
+import org.springframework.stereotype.Component;
+
+/**
+ * Size Metric Calculation.
+ *
+ * @author <a href="mailto:cam.james@gmail.com">Cameron James</a>
+ */
+@Component("sizeMetricCalculation")
+public class SizeMetricCalculation extends DefaultAbstractMetricProcessor {
+
+    @Override
+    public MetricPluginResult calculate(Canonical model, PluginRequest request) {
+        MetricPluginResult result = new MetricPluginResult();
+        result.setMetricResults((double) model.getNodes().size());
+        return null;
+    }
+}
