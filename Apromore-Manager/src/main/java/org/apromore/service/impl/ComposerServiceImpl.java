9839b5b82a73ca6191039a610edce0a666ef4439
cam.james@gmail.com <cam.james@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-11-27 05:35:06 +0000

    PLUG-5 #time 1h #comment Plugin Graph has had all unused code removed and some other code tidied up.

--- Apromore-Manager/src/main/java/org/apromore/service/impl/ComposerServiceImpl.java
+++ Apromore-Manager/src/main/java/org/apromore/service/impl/ComposerServiceImpl.java
@@ -1,10 +1,5 @@
 package org.apromore.service.impl;
 
-import javax.inject.Inject;
-import java.util.concurrent.TimeUnit;
-
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import org.apromore.dao.FragmentVersionRepository;
 import org.apromore.dao.model.FragmentVersion;
 import org.apromore.exception.ExceptionDao;
@@ -16,6 +11,8 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.inject.Inject;
+
 /**
  * @author Chathura Ekanayake
  */
@@ -23,31 +20,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, readOnly = true, rollbackFor = Exception.class)
 public class ComposerServiceImpl implements ComposerService {
 
-    private static final int CONCURRENCY_LEVEL = 4;
-    private static final int CACHE_SIZE = 100000;
-    private static final int EXPIRATION_TIME = 180;
-
+    @Inject
     private FragmentVersionRepository fragmentVersionRepository;
+    @Inject
     private GraphService gService;
 
-    private Cache<Integer, Canonical> cache;
-
-    /**
-     * Default Constructor allowing Spring to Autowire for testing and normal use.
-     * @param graphService Graphing Services.
-     */
-    @Inject
-    public ComposerServiceImpl(final FragmentVersionRepository fvRepository, final GraphService graphService) {
-        fragmentVersionRepository = fvRepository;
-        gService = graphService;
-
-        cache = CacheBuilder.newBuilder().concurrencyLevel(CONCURRENCY_LEVEL).maximumSize(CACHE_SIZE).softValues()
-                .expireAfterWrite(EXPIRATION_TIME, TimeUnit.SECONDS).build();
-    }
 
 
     /**
      * Compose a process Model graph from the DB.
+     *
      * @param rootFragment the root Fragment we are going to build this model from.
      * @return the process model graph
      * @throws ExceptionDao if something fails.
@@ -60,6 +42,7 @@ public class ComposerServiceImpl implements ComposerService {
 
     /**
      * Compose a process Model graph from the DB.
+     *
      * @param rootFragmentId the root Fragment Id.
      * @return the process model graph
      * @throws ExceptionDao if something fails.
@@ -71,16 +54,11 @@ public class ComposerServiceImpl implements ComposerService {
     }
 
 
-
     /* Compose a Fragment. */
     private Canonical composeFragment(FragmentVersion fv) {
-        Canonical canonical = cache.getIfPresent(fv.getId());
-        if (canonical == null) {
-            canonical = new Canonical();
-            canonical = gService.fillNodesByFragment(canonical, fv.getUri());
-            canonical = gService.fillEdgesByFragmentURI(canonical, fv.getUri());
-            cache.put(fv.getId(), canonical);
-        }
+        Canonical canonical = new Canonical();
+        canonical = gService.fillNodesByFragment(canonical, fv.getUri());
+        canonical = gService.fillEdgesByFragmentURI(canonical, fv.getUri());
         return canonical;
     }
 
