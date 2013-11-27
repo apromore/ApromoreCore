33905386e1250c7e43641497b4291a8e8d749ac6
cam.james@gmail.com <cam.james@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2013-11-27 07:53:40 +0000

    Code simplification

--- Apromore-Plugins/plugin-core/plugin-graph/src/main/java/org/apromore/graph/util/GraphConstants.java
+++ Apromore-Plugins/plugin-core/plugin-graph/src/main/java/org/apromore/graph/util/GraphConstants.java
@@ -7,5 +7,7 @@ public final class GraphConstants {
     public static final String FUNCTION = "Function";
     public static final String EVENT = "Event";
     public static final String POCKET = "Pocket";
-    
+
+    /* Only here so people can't instantiate this class */
+    private GraphConstants() { }
 }
