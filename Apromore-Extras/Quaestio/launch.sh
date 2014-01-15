7f4c87bd0fc4484c915d0b225fab7a164bc2b1dd
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2014-01-15 07:14:29 +0000

    Updated version of Quaestio uber-jar wasn't reflected in the launch script.

--- Apromore-Extras/Quaestio/launch.sh
+++ Apromore-Extras/Quaestio/launch.sh
@@ -1,4 +1,4 @@
-java -jar target/quaestio-0.9-jar-with-dependencies.jar \
+java -jar target/quaestio-1.0.jar \
 	-qml ../bpmncmap/src/test/resources/Airport.qml \
 	-model ../bpmncmap/src/test/resources/International\ departure.bpmn \
 	-cmap ../bpmncmap/src/test/resources/International\ departure.cmap \
