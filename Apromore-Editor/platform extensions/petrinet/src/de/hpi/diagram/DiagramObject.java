0ce9206770c2923aba69443b2c6e95d9412126ed
cam.james@gmail.com <cam.james@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2014-02-04 04:58:01 +0000

    Added more PNML code into the Editor, Updated the delete to not throw an error on certain conditions, and fix the bpmn to epml and vice vera work as expected.

---
+++ Apromore-Editor/platform extensions/petrinet/src/de/hpi/diagram/DiagramObject.java
@@ -0,0 +1,47 @@
+package de.hpi.diagram;
+
+import java.util.Map;
+
+
+public abstract class DiagramObject implements Comparable<DiagramObject> {
+
+	protected String resourceId;
+	protected String type;
+	protected Map<String, String> properties;
+	
+	public String getResourceId() {
+		return resourceId;
+	}
+
+	public void setResourceId(String resourceId) {
+		this.resourceId = resourceId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public int compareTo(DiagramObject o){
+		return this.getResourceId().compareTo(((DiagramObject)o).getResourceId());
+	}
+	
+	public void setProperties(Map<String, String> properties) {
+		this.properties = properties;
+	}
+	
+	public String getPropertyValue(String key) {
+		if (properties != null) {
+			return properties.get(key);
+		} else {
+			return null;
+		}
+	}
+	
+	public boolean hasPropertyValue(String key) {
+		return (properties != null) && properties.containsKey(key);
+	}
+}
