d7a2fda7dc0a60f275b718c83246e5d2c48c9e21
cam.james@gmail.com <cam.james@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2014-02-07 01:36:48 +0000

    Update to the Editor.

--- trunk/Apromore-Editor/editor/src/scripts/Plugins/pdf.js
+++ trunk/Apromore-Editor/editor/src/scripts/Plugins/pdf.js
@@ -30,16 +30,16 @@ ORYX.Plugins.File = ORYX.Plugins.AbstractPlugin.extend({
     construct: function(facade){
         this.facade = facade;
 
-//        this.facade.offer({
-//            'name': ORYX.I18N.File.print,
-//            'functionality': this.print.bind(this),
-//            'group': ORYX.I18N.File.group,
-//            'icon': ORYX.PATH + "images/printer.png",
-//            'description': ORYX.I18N.File.printDesc,
-//            'index': 3,
-//            'minShape': 0,
-//            'maxShape': 0
-//        });
+        this.facade.offer({
+            'name': ORYX.I18N.File.print,
+            'functionality': this.print.bind(this),
+            'group': ORYX.I18N.File.group,
+            'icon': ORYX.PATH + "images/printer.png",
+            'description': ORYX.I18N.File.printDesc,
+            'index': 3,
+            'minShape': 0,
+            'maxShape': 0
+        });
 
         this.facade.offer({
             'name': ORYX.I18N.File.pdf,
@@ -83,70 +83,71 @@ ORYX.Plugins.File = ORYX.Plugins.AbstractPlugin.extend({
                 Ext.Msg.alert(ORYX.I18N.Oryx.title, ORYX.I18N.File.genPDFFailed);
             }).bind(this)
         });
+    },
+
+    print: function(){
+        Ext.Msg.show({
+            title		: ORYX.I18N.File.printTitle,
+            msg			: ORYX.I18N.File.printMsg,
+            buttons		: Ext.Msg.YESNO,
+            icon		: Ext.MessageBox.QUESTION,
+            fn			: function(btn) {
+                if (btn == "yes") {
+                    // Set all options for the new window
+                    var option = $H({
+                        width: 300,
+                        height: 400,
+                        toolbar: "no",
+                        status: "no",
+                        menubar: "yes",
+                        dependent: "yes",
+                        resizable: "yes",
+                        scrollbars: "yes"
+                    });
+
+                    // Create the new window with all the settings
+                    var newWindow = window.open("", "PrintWindow", option.invoke('join', '=').join(','));
+
+                    // Add a style tag to the head and hide all controls
+                    var head = newWindow.document.getElementsByTagName('head')[0];
+                    var style = document.createElement("style");
+                    style.innerHTML = " body {padding:0px; margin:0px} .svgcontainer { display:none; }";
+                    head.appendChild(style);
+
+                    // Clone the svg-node and append this to the new body
+                    newWindow.document.getElementsByTagName('body')[0].appendChild(this.facade.getCanvas().getSVGRepresentation());
+                    var svg = newWindow.document.getElementsByTagName('body')[0].getElementsByTagName('svg')[0];
+
+                    // Set the width and height
+                    svg.setAttributeNS(null, 'width', 1100);
+                    svg.setAttributeNS(null, 'height', 1400);
+
+                    // Set the correct size and rotation
+                    svg.lastChild.setAttributeNS(null, 'transform', 'scale(0.47, 0.47) rotate(270, 1510, 1470)');
+
+                    var markers = ['marker-start', 'marker-mid', 'marker-end'];
+                    var path = $A(newWindow.document.getElementsByTagName('path'));
+                    path.each(function(pathNode){
+                        markers.each(function(marker){
+                            // Get the marker value
+                            var url = pathNode.getAttributeNS(null, marker);
+                            if (!url) {
+                                return
+                            }
+
+                            // Replace the URL and set them new
+                            url = "url(about:blank#" + url.slice(5);
+                            pathNode.setAttributeNS(null, marker, url);
+                        });
+                    });
+
+                    // Get the print dialog
+                    newWindow.print();
+                    return true;
+                }
+            }.bind(this)
+        });
     }
 
-//    print: function(){
-//        Ext.Msg.show({
-//            title		: ORYX.I18N.File.printTitle,
-//            msg			: ORYX.I18N.File.printMsg,
-//            buttons		: Ext.Msg.YESNO,
-//            icon		: Ext.MessageBox.QUESTION,
-//            fn			: function(btn) {
-//                if (btn == "yes") {
-//                    // Set all options for the new window
-//                    var option = $H({
-//                        width: 300,
-//                        height: 400,
-//                        toolbar: "no",
-//                        status: "no",
-//                        menubar: "yes",
-//                        dependent: "yes",
-//                        resizable: "yes",
-//                        scrollbars: "yes"
-//                    });
-//
-//                    // Create the new window with all the settings
-//                    var newWindow = window.open("", "PrintWindow", option.invoke('join', '=').join(','));
-//
-//                    // Add a style tag to the head and hide all controls
-//                    var head = newWindow.document.getElementsByTagName('head')[0];
-//                    var style = document.createElement("style");
-//                    style.innerHTML = " body {padding:0px; margin:0px} .svgcontainer { display:none; }";
-//                    head.appendChild(style);
-//
-//                    // Clone the svg-node and append this to the new body
-//                    newWindow.document.getElementsByTagName('body')[0].appendChild(this.facade.getCanvas().getSVGRepresentation());
-//                    var svg = newWindow.document.getElementsByTagName('body')[0].getElementsByTagName('svg')[0];
-//
-//                    // Set the width and height
-//                    svg.setAttributeNS(null, 'width', 1100);
-//                    svg.setAttributeNS(null, 'height', 1400);
-//
-//                    // Set the correct size and rotation
-//                    svg.lastChild.setAttributeNS(null, 'transform', 'scale(0.47, 0.47) rotate(270, 1510, 1470)');
-//
-//                    var markers = ['marker-start', 'marker-mid', 'marker-end'];
-//                    var path = $A(newWindow.document.getElementsByTagName('path'));
-//                    path.each(function(pathNode){
-//                        markers.each(function(marker){
-//                            // Get the marker value
-//                            var url = pathNode.getAttributeNS(null, marker);
-//                            if (!url) {
-//                                return
-//                            }
-//
-//                            // Replace the URL and set them new
-//                            url = "url(about:blank#" + url.slice(5);
-//                            pathNode.setAttributeNS(null, marker, url);
-//                        });
-//                    });
-//
-//                    // Get the print dialog
-//                    newWindow.print();
-//                    return true;
-//                }
-//            }.bind(this)
-//        });
-//    }
 });
 
