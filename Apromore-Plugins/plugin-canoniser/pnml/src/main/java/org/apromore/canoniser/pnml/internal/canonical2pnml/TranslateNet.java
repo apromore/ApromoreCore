ced9d8047a4236ef7d96e7cc763c8e3b3a3aa012
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2014-03-17 02:28:05 +0000

    Added Case 12 to PNML canoniser tests (CPF derived from a BPMN task with a boundary event). PNML elements synthesized during decanonization have graphics elements so that they can be re-layout'ed.

--- Apromore-Plugins/plugin-canoniser/pnml/src/main/java/org/apromore/canoniser/pnml/internal/canonical2pnml/TranslateNet.java
+++ Apromore-Plugins/plugin-canoniser/pnml/src/main/java/org/apromore/canoniser/pnml/internal/canonical2pnml/TranslateNet.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.apromore.anf.AnnotationsType;
 import org.apromore.cpf.ANDJoinType;
 import org.apromore.cpf.ANDSplitType;
+import org.apromore.cpf.CancellationRefType;
 import org.apromore.cpf.EdgeType;
 import org.apromore.cpf.EventType;
 import org.apromore.cpf.NetType;
@@ -17,6 +18,10 @@ import org.apromore.cpf.TaskType;
 import org.apromore.cpf.WorkType;
 import org.apromore.cpf.XORJoinType;
 import org.apromore.cpf.XORSplitType;
+import org.apromore.pnml.ArcType;
+import org.apromore.pnml.ArcTypeType;
+import org.apromore.pnml.PlaceType;
+import org.apromore.pnml.TransitionType;
 
 public class TranslateNet {
 
@@ -91,6 +96,42 @@ public class TranslateNet {
         ids = tt.getIds();
         moids.setValues(data, ids);
         moids.mapIDS();
+
+        // Second pass through the nodes, to translate CPF cancellation sets into PNML reset arcs
+        for (NodeType node : net.getNode()) {
+            if (node instanceof WorkType) {
+                WorkType work = (WorkType) node;
+                for (CancellationRefType cancellationRef: work.getCancelNodeId()) {
+                    //LOGGER.info("Work node " + work.getId() + " cancels node " + cancellationRef.getRefId());
+                    NodeType cpfNode = data.nodeRefMap.get(cancellationRef.getRefId());
+                    assert cpfNode != null: "Unable to find CPF node for id " + cancellationRef.getRefId();
+                    PlaceType cancelled = data.getRunningPlaceMap().get(cpfNode);
+                    if (cancelled != null) {
+                        //String cancellingId = data.get_id_map_value(work.getId());
+                        //LOGGER.info("Transition " + cancellingId + " cancelled place " + cancelled.getId());
+                        assert data.getEndNodeMap().containsKey(work):
+                            "CPF Work node " + work.getId() + " doesn't have an ending PNML element";
+                        assert data.getEndNodeMap().get(work) instanceof TransitionType:
+                            "CPF Work node " + work.getId() + " ended by non-transition PNML element " + data.getEndNodeMap().get(work).getId();
+                        TransitionType cancelling = (TransitionType) data.getEndNodeMap().get(work);
+
+                        ArcType resetArc = new ArcType();
+                        resetArc.setId(String.valueOf(ids++));
+                        resetArc.setSource(cancelled);
+                        resetArc.setTarget(cancelling);
+
+                        ArcTypeType resetArcType = new ArcTypeType();
+                        resetArcType.setText("reset");
+                        resetArc.setType(resetArcType);
+
+                        data.getNet().getArc().add(resetArc);
+                    }
+                }
+                for (CancellationRefType cancellationRef: work.getCancelEdgeId()) {
+                    // not yet implemented
+                }
+            }
+        }
     }
 
     public long getIds() {
