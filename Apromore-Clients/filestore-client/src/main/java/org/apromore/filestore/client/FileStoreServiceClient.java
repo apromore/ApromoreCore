eff6f6fcfdb31ec38ce0a231ac04b65f31f67206
conforti.raffaele@gmail.com <conforti.raffaele@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2014-04-29 04:00:58 +0000

    Introduced Disclaimer on login page for portal and filestore, Introduced forgot password and register new user on filestore page, Fixed link to Apromore Initiative in portal index page Updated year copyright

--- Apromore-Clients/filestore-client/src/main/java/org/apromore/filestore/client/FileStoreServiceClient.java
+++ Apromore-Clients/filestore-client/src/main/java/org/apromore/filestore/client/FileStoreServiceClient.java
@@ -6,6 +6,13 @@ import java.util.List;
 import com.github.sardine.DavResource;
 import com.github.sardine.Sardine;
 import com.github.sardine.SardineFactory;
+import org.apromore.model.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+import javax.xml.bind.JAXBElement;
 
 /**
  * Implementation of the Apromore File Store Client.
@@ -14,9 +21,22 @@ import com.github.sardine.SardineFactory;
  */
 public class FileStoreServiceClient implements FileStoreService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileStoreServiceClient.class);
+    private static final ObjectFactory WS_CLIENT_FACTORY = new ObjectFactory();
+
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "password";
 
+    private WebServiceTemplate webServiceTemplate;
+
+    /**
+     * Default Constructor.
+     *
+     * @param webServiceTemplate the webservice template
+     */
+    public FileStoreServiceClient(WebServiceTemplate webServiceTemplate) {
+        this.webServiceTemplate = webServiceTemplate;
+    }
 
     /**
      * @see FileStoreService#list(String)
@@ -84,4 +104,66 @@ public class FileStoreServiceClient implements FileStoreService {
         SardineFactory.begin(USERNAME, PASSWORD).delete(url);
     }
 
+    /**
+     * @see org.apromore.filestore.client.FileStoreService#readUserByEmail(String)
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public UserType readUserByEmail(String email) throws Exception {
+        LOGGER.debug("Preparing ResetUserRequest.....");
+
+        ReadUserByEmailInputMsgType msg = new ReadUserByEmailInputMsgType();
+        msg.setEmail(email);
+
+        JAXBElement<ReadUserByEmailInputMsgType> request = WS_CLIENT_FACTORY.createReadUserByEmailRequest(msg);
+
+        JAXBElement<ReadUserByEmailOutputMsgType> response = (JAXBElement<ReadUserByEmailOutputMsgType>) webServiceTemplate.marshalSendAndReceive(request);
+        if (response.getValue().getResult().getCode() == -1) {
+            throw new Exception(response.getValue().getResult().getMessage());
+        } else {
+            return response.getValue().getUser();
+        }
+    }
+
+    /**
+     * @see org.apromore.filestore.client.FileStoreService#resetUserPassword(String, String)
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean resetUserPassword(String username, String password) {
+        LOGGER.debug("Preparing Reset the Users Password.....");
+
+        ResetUserPasswordInputMsgType msg = new ResetUserPasswordInputMsgType();
+        msg.setUsername(username);
+        msg.setPassword(password);
+
+        JAXBElement<ResetUserPasswordInputMsgType> request = WS_CLIENT_FACTORY.createResetUserPasswordRequest(msg);
+
+        JAXBElement<ResetUserPasswordOutputMsgType> response = (JAXBElement<ResetUserPasswordOutputMsgType>) webServiceTemplate.marshalSendAndReceive(request);
+        return response.getValue().isSuccess();
+    }
+
+    /**
+     * @see org.apromore.filestore.client.FileStoreService#writeUser(org.apromore.model.UserType)
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public UserType writeUser(UserType user) throws Exception {
+        LOGGER.debug("Preparing WriteUserRequest.....");
+
+        WriteUserInputMsgType msg = new WriteUserInputMsgType();
+        msg.setUser(user);
+
+        JAXBElement<WriteUserInputMsgType> request = WS_CLIENT_FACTORY.createWriteUserRequest(msg);
+
+        JAXBElement<WriteUserOutputMsgType> response = (JAXBElement<WriteUserOutputMsgType>) webServiceTemplate.marshalSendAndReceive(request);
+        if (response.getValue().getResult().getCode() == -1) {
+            throw new Exception(response.getValue().getResult().getMessage());
+        }
+
+        return response.getValue().getUser();
+    }
 }
