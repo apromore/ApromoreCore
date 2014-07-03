1685ec0817e2af7a2ab38001dcef8d1abdf7f87f
raboczi <raboczi@5faac2a4-3861-aa31-f262-64efd740a31a>
2014-07-03 05:38:07 +0000

    Relocated DavFileSystemView to the filestore-client module.

--- trunk/Apromore-Extras/Cmapper/src/main/java/com/processconfiguration/cmapper/Applet.java
+++ trunk/Apromore-Extras/Cmapper/src/main/java/com/processconfiguration/cmapper/Applet.java
@@ -33,6 +33,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 // Local classes
 import com.processconfiguration.quaestio.ApromoreProcessModel;
+import org.apromore.filestore.client.DavFileSystemView;
 import org.apromore.filestore.client.FileStoreService;
 
 /**
