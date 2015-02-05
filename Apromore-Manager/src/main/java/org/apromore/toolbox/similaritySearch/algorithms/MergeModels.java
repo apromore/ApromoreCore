c093c89ab24b51aaedba98efb0ec1beddaac8af6
conforti.raffaele@gmail.com <conforti.raffaele@gmail.com@5faac2a4-3861-aa31-f262-64efd740a31a>
2015-02-05 05:26:01 +0000

    Updated Copyright Statement

--- Apromore-Manager/src/main/java/org/apromore/toolbox/similaritySearch/algorithms/MergeModels.java
+++ Apromore-Manager/src/main/java/org/apromore/toolbox/similaritySearch/algorithms/MergeModels.java
@@ -16,9 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.
  * If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
- */
-
-package org.apromore.toolbox.similaritySearch.algorithms;
+ */org.apromore.toolbox.similaritySearch.algorithms;
 
 import org.apromore.toolbox.similaritySearch.common.IdGeneratorHelper;
 import org.apromore.toolbox.similaritySearch.common.VertexPair;
