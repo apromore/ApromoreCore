42a51d7c51c9c68f7e5a2c11ab9e901a7b0a1f65
raboczi <raboczi@gmail.com>
2016-02-29 13:03:33 +1000

    Removed non-OSGi copy of the editor.

--- Apromore-Editor/platform extensions/jpdl/src/com/signavio/warehouse/business/util/jpdl4/WireDouble.java
+++
@@ -1,9 +0,0 @@
-package com.signavio.warehouse.business.util.jpdl4;
-
-public class WireDouble extends WireObjectGroup {
-
-    public WireDouble(String name, String value) {
-        super(name, value, "double");
-    }
-
-}
--- Apromore-Editor/platform extensions/jpdl/src/com/signavio/warehouse/business/util/jpdl4/WireFalse.java
+++
@@ -1,9 +0,0 @@
-package com.signavio.warehouse.business.util.jpdl4;
-
-public class WireFalse extends WireObjectGroup {
-
-    public WireFalse(String name, String value) {
-        super(name, value, "false");
-    }
-
-}
