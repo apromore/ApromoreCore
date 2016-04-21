51e341e56e780f0d9d0c22a4c6108f96c40c8627
nemo-91 <adrn.agst@gmail.com>
2016-04-21 14:50:51 +1000

    added metrics plugin

---
+++ Metrics-Portal-Plugin/src/main/java/org/apromore/plugin/portal/metrics/MetricsPlugin.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright © 2009-2016 The Apromore Initiative.
+ *
+ * This file is part of "Apromore".
+ *
+ * "Apromore" is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * "Apromore" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ */
+
+package org.apromore.plugin.portal.metrics;
+
+// Java 2 Standard Edition packages
+
+import org.apromore.model.ProcessSummaryType;
+import org.apromore.model.VersionSummaryType;
+import org.apromore.plugin.portal.PortalContext;
+import org.apromore.portal.custom.gui.plugin.PluginCustomGui;
+import org.apromore.portal.custom.gui.tab.impl.TabHeader;
+import org.apromore.portal.custom.gui.tab.impl.TabRowValue;
+import org.apromore.service.BPMNDiagramImporter;
+import org.apromore.service.ProcessService;
+import org.apromore.service.metrics.MetricsService;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.apromore.helper.Version;
+import org.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zkoss.zk.ui.event.*;
+import org.zkoss.zul.*;
+
+import java.io.IOException;
+import java.util.*;
+
+// Java 2 Enterprise Edition packages
+// Third party packages
+// Local packages
+
+/**
+ * Metrics service. Created by Adriano Augusto 18/04/2016
+ */
+@Component("plugin")
+public class MetricsPlugin extends PluginCustomGui {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MetricsPlugin.class);
+
+
+    private PortalContext portalContext;
+    private final MetricsService metricsService;
+    private final ProcessService processService;
+    private final BPMNDiagramImporter importerService;
+    private Map<ProcessSummaryType, List<VersionSummaryType>> processVersions;
+
+    /* zk gui variables */
+    private Window settings;
+    private Radiogroup size;
+    private Radiogroup cfc;
+    private Radiogroup acd;
+    private Radiogroup mcd;
+    private Radiogroup cnc;
+    private Radiogroup density;
+    private Radiogroup structuredness;
+    private Radiogroup separability;
+    private Radiogroup duplicates;
+
+    private Button okButton;
+    private Button cancelButton;
+
+    @Inject
+    public MetricsPlugin(  final MetricsService    metricsService,
+                           final ProcessService processService,
+                           final BPMNDiagramImporter importerService) {
+
+        this.metricsService      = metricsService;
+        this.processService      = processService;
+        this.importerService     = importerService;
+    }
+
+    @Override
+    public String getLabel(Locale locale) {
+        return "BPMN Metrics";
+    }
+
+    @Override
+    public void execute(PortalContext context) {
+        this.portalContext = context;
+        portalContext.getMessageHandler().displayInfo("Executing Metrics service!");
+
+        try {
+            this.settings = (Window) portalContext.getUI().createComponent(getClass().getClassLoader(), "zul/metrics.zul", null, null);
+
+            this.size = (Radiogroup) this.settings.getFellow("size");
+            this.cfc = (Radiogroup) this.settings.getFellow("cfc");
+            this.acd = (Radiogroup) this.settings.getFellow("acd");
+            this.mcd = (Radiogroup) this.settings.getFellow("mcd");
+            this.cnc = (Radiogroup) this.settings.getFellow("cnc");
+            this.density = (Radiogroup) this.settings.getFellow("density");
+            this.structuredness = (Radiogroup) this.settings.getFellow("structuredness");
+            this.separability = (Radiogroup) this.settings.getFellow("separability");
+            this.duplicates = (Radiogroup) this.settings.getFellow("duplicates");
+
+            this.cancelButton = (Button) this.settings.getFellow("CancelButton");
+            this.okButton = (Button) this.settings.getFellow("OKButton");
+
+            this.cancelButton.addEventListener("onClick", new org.zkoss.zk.ui.event.EventListener<Event>() {
+                public void onEvent(Event event) throws Exception {
+                    cancel();
+                }
+            });
+            this.okButton.addEventListener("onClick", new org.zkoss.zk.ui.event.EventListener<Event>() {
+                public void onEvent(Event event) throws Exception {
+                    runComputation();
+                }
+            });
+            this.settings.doModal();
+        } catch (IOException e) {
+            Messagebox.show("Something went wrong (" + e.getMessage() + ")", "Attention", Messagebox.OK, Messagebox.ERROR);
+        }
+    }
+
+
+
+    protected void cancel() {
+        this.settings.detach();
+    }
+
+    protected void runComputation() {
+        Map<String, String> metrics;
+        this.settings.detach();
+
+        boolean size = this.size.getSelectedIndex() == 0 ? true : false;
+        boolean cfc = this.cfc.getSelectedIndex() == 0 ? true : false;
+        boolean acd  = this.acd.getSelectedIndex() == 0 ? true : false;
+        boolean mcd = this.mcd.getSelectedIndex() == 0 ? true : false;
+        boolean cnc = this.cnc.getSelectedIndex() == 0 ? true : false;
+        boolean density  = this.density.getSelectedIndex() == 0 ? true : false;
+        boolean structuredness = this.structuredness.getSelectedIndex() == 0 ? true : false;
+        boolean separability = this.separability.getSelectedIndex() == 0 ? true : false;
+        boolean duplicates  = this.duplicates.getSelectedIndex() == 0 ? true : false;
+
+        try {
+            processVersions = portalContext.getSelection().getSelectedProcessModelVersions();
+            for (ProcessSummaryType process : processVersions.keySet()) {
+                for (VersionSummaryType vst : processVersions.get(process)) {
+                    int procID = process.getId();
+                    String procName = process.getName();
+                    String branch = vst.getName();
+                    Version version = new Version(vst.getVersionNumber());
+
+                    String model = processService.getBPMNRepresentation(procName, procID, branch, version);
+                    BPMNDiagram diagram = importerService.importBPMNDiagram(model);
+
+                    metrics = metricsService.computeMetrics(  diagram, size, cfc, acd, mcd, cnc, density,
+                                                    structuredness, separability, duplicates);
+
+                    if(metrics == null) LOGGER.info("GOT NULL!");
+                    else {
+                        List<TabRowValue> rows = new ArrayList<>();
+                        for (String key : metrics.keySet()) {
+                            LOGGER.info(key + " : " + metrics.get(key));
+                            rows.add(createTabRowValue(key, metrics.get(key)));
+                        }
+                        TabHeader tabHeader = createTabHeader("Metric", "Value");
+                        addTab("Metrics", "", rows, tabHeader, null, portalContext);
+                    }
+
+                    LOGGER.info("Metrics - done!");
+                    this.portalContext.refreshContent();
+                }
+            }
+        } catch(Exception e) {
+            StringBuilder sb = new StringBuilder();
+            e.printStackTrace();
+            for(StackTraceElement element : e.getStackTrace()) {
+                sb.append(element.toString() + "\n");
+            }
+            String message = "Search failed (" + sb.toString() + ")";
+            Messagebox.show(message, "Attention", Messagebox.OK, Messagebox.ERROR);
+        }
+    }
+
+}
