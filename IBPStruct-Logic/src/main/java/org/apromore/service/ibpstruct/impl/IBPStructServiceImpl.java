4bf53480b8305d41bfc16884dbdc4935a42d0212
nemo-91 <adrn.agst@gmail.com>
2016-05-04 15:26:25 +1000

    added iBPStruct as plugin, plus bpmntk-osgi bundle

--- IBPStruct-Logic/src/main/java/org/apromore/service/ibpstruct/impl/IBPStructServiceImpl.java
+++ IBPStruct-Logic/src/main/java/org/apromore/service/ibpstruct/impl/IBPStructServiceImpl.java
@@ -20,7 +20,10 @@
 
 package org.apromore.service.ibpstruct.impl;
 
+import org.processmining.contexts.uitopia.UIContext;
+import org.processmining.contexts.uitopia.UIPluginContext;
 import org.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
+import org.processmining.plugins.bpmn.BpmnDefinitions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -35,8 +38,25 @@ import au.edu.qut.structuring.StructuringService;
 public class IBPStructServiceImpl implements IBPStructService {
     private static final Logger LOGGER = LoggerFactory.getLogger(IBPStructServiceImpl.class);
 
+//    @Override
+//    public BPMNDiagram structureProcess( BPMNDiagram model,
+//                                         String  policy,
+//                                         int     maxDepth,
+//                                         int     maxSolutions,
+//                                         int     maxChildren,
+//                                         int     maxStates,
+//                                         int     maxMinutes,
+//                                         boolean timeBounded,
+//                                         boolean keepBisimulation,
+//                                         boolean forceStructuring )
+//    {
+//        StructuringService ss = new StructuringService();
+//        return ss.structureDiagram( model, policy, maxDepth, maxSolutions, maxChildren,
+//                                    maxStates, maxMinutes, timeBounded, keepBisimulation, forceStructuring);
+//    }
+
     @Override
-    public BPMNDiagram structureProcess( BPMNDiagram model,
+    public String structureProcess( BPMNDiagram model,
                                          String  policy,
                                          int     maxDepth,
                                          int     maxSolutions,
@@ -48,7 +68,33 @@ public class IBPStructServiceImpl implements IBPStructService {
                                          boolean forceStructuring )
     {
         StructuringService ss = new StructuringService();
-        return ss.structureDiagram( model, policy, maxDepth, maxSolutions, maxChildren,
-                                    maxStates, maxMinutes, timeBounded, keepBisimulation, forceStructuring);
+
+        BPMNDiagram structuredDiagram = ss.structureDiagram( model, policy, maxDepth, maxSolutions, maxChildren, maxStates, maxMinutes, timeBounded, keepBisimulation, forceStructuring);
+        String structuredModel;
+
+        UIContext context = new UIContext();
+        UIPluginContext uiPluginContext = context.getMainPluginContext();
+        BpmnDefinitions.BpmnDefinitionsBuilder definitionsBuilder = new BpmnDefinitions.BpmnDefinitionsBuilder(uiPluginContext, structuredDiagram);
+        BpmnDefinitions definitions = new BpmnDefinitions("definitions", definitionsBuilder);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<definitions xmlns=\"http://www.omg.org/spec/BPMN/20100524/MODEL\"\n " +
+                "xmlns:dc=\"http://www.omg.org/spec/DD/20100524/DC\"\n " +
+                "xmlns:bpmndi=\"http://www.omg.org/spec/BPMN/20100524/DI\"\n " +
+                "xmlns:di=\"http://www.omg.org/spec/DD/20100524/DI\"\n " +
+                "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n " +
+                "targetNamespace=\"http://www.omg.org/bpmn20\"\n " +
+                "xsi:schemaLocation=\"http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd\">");
+        sb.append(definitions.exportElements());
+        sb.append("</definitions>");
+
+        structuredModel = sb.toString();
+
+        structuredModel = structuredModel.replaceAll("\n", "&#10;");
+        structuredModel = structuredModel.replaceAll(">&#10;", ">\n");
+        structuredModel = structuredModel.replaceAll("\"&#10;", "\"\n");
+
+        return structuredModel;
     }
 }
