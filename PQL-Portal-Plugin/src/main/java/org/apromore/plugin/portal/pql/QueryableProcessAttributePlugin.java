482a65bff17f8b40e08cbcd7c5aad914243bed18
raboczi <raboczi@gmail.com>
2016-05-16 12:17:17 +1000

    The PQL column in the Portal's process summary list shows icons for PQL index status.

--- PQL-Portal-Plugin/src/main/java/org/apromore/plugin/portal/pql/QueryableProcessAttributePlugin.java
+++ PQL-Portal-Plugin/src/main/java/org/apromore/plugin/portal/pql/QueryableProcessAttributePlugin.java
@@ -20,23 +20,34 @@
 package org.apromore.plugin.portal.pql;
 
 // Java 2 Standard
+import java.util.List;
 import java.util.Locale;
 
+// Java 2 Enterprise
+import javax.inject.Inject;
+
 // Third party
+import org.pql.index.IndexStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.zkoss.zul.Image;
 import org.zkoss.zul.Listcell;
 
 // First party
 import org.apromore.model.ProcessSummaryType;
+import org.apromore.model.VersionSummaryType;
 import org.apromore.plugin.DefaultParameterAwarePlugin;
 import org.apromore.plugin.portal.PortalProcessAttributePlugin;
+import org.apromore.service.pql.PQLService;
 
 /**
  * Adds the "queryable?" attribute to the Portal's process summary list.
  */
 @Component("queryableProcessAttributePlugin")
 public class QueryableProcessAttributePlugin extends DefaultParameterAwarePlugin implements PortalProcessAttributePlugin {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueryableProcessAttributePlugin.class);
 
     // Icons displayed in the "Queryable?" column of the process summary list
     public static final String PQL_UNINDEXED_ICON   = "/img/add.png";
@@ -46,6 +57,8 @@ public class QueryableProcessAttributePlugin extends DefaultParameterAwarePlugin
     public static final String PQL_ERROR_ICON       = "/img/alert.png";
 
     private static final String CENTRE_ALIGN = "vertical-align: middle; text-align:center";
+
+    @Inject private PQLService pqlService;
 
     /** {@inheritDoc} */
     public String getLabel(Locale locale) {
@@ -59,22 +72,25 @@ public class QueryableProcessAttributePlugin extends DefaultParameterAwarePlugin
      */
     public Listcell getListcell(ProcessSummaryType process) {
 
+        List<VersionSummaryType> list = process.getVersionSummaries();
+        VersionSummaryType lastVersion = list.get(list.size() - 1);  // get the last version
+        String externalId = process.getId() + "/MAIN/" + lastVersion.getVersionNumber();
+
         // Associate an icon with the indexing status
         String iconPath;
-        iconPath = PQL_ERROR_ICON;
-        /*
-        if (process.getPqlIndexerStatus() == null) {
-            iconPath = Constants.PQL_ERROR_ICON;
-        } else {
-            switch (process.getPqlIndexerStatus()) {
+        try {
+            IndexStatus status = pqlService.getIndexStatus(externalId);
+            switch (status) {
             case UNINDEXED:   iconPath = PQL_UNINDEXED_ICON;    break;
             case INDEXING:    iconPath = PQL_INDEXING_ICON;     break;
             case INDEXED:     iconPath = PQL_INDEXED_ICON;      break;
             case CANNOTINDEX: iconPath = PQL_CANNOTINDEX_ICON;  break;
             default:          iconPath = PQL_ERROR_ICON;
             }
+        } catch (Exception e) {
+            LOGGER.warn("Unable to get index status for process " + externalId, e);
+            iconPath = PQL_ERROR_ICON;
         }
-        */
         assert iconPath != null;
 
         // Return a list cell containing the indexing status icon
@@ -84,4 +100,3 @@ public class QueryableProcessAttributePlugin extends DefaultParameterAwarePlugin
         return lc;
     }
 }
-
