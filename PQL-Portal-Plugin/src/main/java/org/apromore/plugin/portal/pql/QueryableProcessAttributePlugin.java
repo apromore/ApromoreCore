761a260444cbfac75326519286f77a8b7f2ec8ea
raboczi <raboczi@gmail.com>
2016-06-16 15:47:10 +1000

    Added tooltips to the icons in the PQL portal plugin.

--- PQL-Portal-Plugin/src/main/java/org/apromore/plugin/portal/pql/QueryableProcessAttributePlugin.java
+++ PQL-Portal-Plugin/src/main/java/org/apromore/plugin/portal/pql/QueryableProcessAttributePlugin.java
@@ -32,8 +32,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.zkoss.zul.Image;
+import org.zkoss.zul.Label;
 import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listheader;
+import org.zkoss.zul.Popup;
 
 // First party
 import org.apromore.helper.Version;
@@ -53,11 +55,11 @@ public class QueryableProcessAttributePlugin extends DefaultParameterAwarePlugin
     private static final Logger LOGGER = LoggerFactory.getLogger(QueryableProcessAttributePlugin.class);
 
     // Icons displayed in the "Queryable?" column of the process summary list
-    public static final String PQL_UNINDEXED_ICON   = "/img/add.png";
+    public static final String PQL_UNINDEXED_ICON   = "/img/unselect.png";
     public static final String PQL_INDEXING_ICON    = "/img/arrow_refresh.png";
     public static final String PQL_INDEXED_ICON     = "/img/select.png";
     public static final String PQL_CANNOTINDEX_ICON = "/img/cross.png";
-    public static final String PQL_ERROR_ICON       = "/img/alert.png";
+    public static final String PQL_ERROR_ICON       = "/img/error.png";
 
     private static final String CENTRE_ALIGN = "vertical-align: middle; text-align:center";
 
@@ -84,18 +86,34 @@ public class QueryableProcessAttributePlugin extends DefaultParameterAwarePlugin
 
         // Associate an icon with the indexing status
         String iconPath;
+        String tooltip;
         try {
             IndexStatus status = pqlService.getIndexStatus(externalId);
             switch (status) {
-            case UNINDEXED:   iconPath = PQL_UNINDEXED_ICON;    break;
-            case INDEXING:    iconPath = PQL_INDEXING_ICON;     break;
-            case INDEXED:     iconPath = PQL_INDEXED_ICON;      break;
-            case CANNOTINDEX: iconPath = PQL_CANNOTINDEX_ICON;  break;
-            default:          iconPath = PQL_ERROR_ICON;
+            case UNINDEXED:
+                iconPath = PQL_UNINDEXED_ICON;
+                tooltip = "Unindexed";
+                break;
+            case INDEXING:
+                iconPath = PQL_INDEXING_ICON;
+                tooltip = "Indexing";
+                break;
+            case INDEXED:
+                iconPath = PQL_INDEXED_ICON;
+                tooltip = "Indexed";
+                break;
+            case CANNOTINDEX:
+                iconPath = PQL_CANNOTINDEX_ICON;
+                tooltip = "Cannot index";
+                break;
+            default:
+                iconPath = PQL_ERROR_ICON;
+                tooltip  = "Unknown index status code: " + status;
             }
         } catch (Exception e) {
             LOGGER.warn("Unable to get index status for process " + externalId, e);
             iconPath = PQL_ERROR_ICON;
+            tooltip = e.toString();
         }
         assert iconPath != null;
 
@@ -103,6 +121,14 @@ public class QueryableProcessAttributePlugin extends DefaultParameterAwarePlugin
         Listcell lc = new Listcell();
         lc.appendChild(new Image(iconPath));
         lc.setStyle(CENTRE_ALIGN);
+        if (tooltip != null) {
+            Label label = new Label(tooltip);
+            label.setMultiline(true);
+            Popup popup = new Popup();
+            popup.appendChild(label);
+            lc.appendChild(popup);
+            lc.setTooltip(popup);
+        }
         return lc;
     }
 }
