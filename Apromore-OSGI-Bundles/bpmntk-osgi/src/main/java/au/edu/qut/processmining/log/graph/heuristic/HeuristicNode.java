7b9222e9fae3b4a3c77b140b4571d9fdc54517eb
nem0-91 <adrn.agst@gmail.com>
2016-10-24 15:44:09 +0300

    progressing with heuristic miner plus

--- Apromore-OSGI-Bundles/bpmntk-osgi/src/main/java/au/edu/qut/processmining/log/graph/heuristic/HeuristicNode.java
+++ Apromore-OSGI-Bundles/bpmntk-osgi/src/main/java/au/edu/qut/processmining/log/graph/heuristic/HeuristicNode.java
@@ -6,4 +6,13 @@ import au.edu.qut.processmining.log.graph.EventNode;
  * Created by Adriano on 24/10/2016.
  */
 public class HeuristicNode extends EventNode {
+    //nothing new w.r.t. EventNode
+
+    public HeuristicNode() { super(); }
+
+    public HeuristicNode(String label) { super(label); }
+
+    public HeuristicNode(int frequency) { super(frequency); }
+
+    public HeuristicNode(String label, int frequency) { super(label, frequency); }
 }
