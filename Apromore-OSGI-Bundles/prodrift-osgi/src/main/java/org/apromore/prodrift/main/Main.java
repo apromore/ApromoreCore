881ae8695feca92fb6fa1577fb648bb582138169
Ali <alirezaostovar@gmail.com>
2017-03-03 16:00:00 +1000

    ProDrift source code, first release.

---
+++ Apromore-OSGI-Bundles/prodrift-osgi/src/main/java/org/apromore/prodrift/main/Main.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2009-2017 The Apromore Initiative.
+ *
+ * This file is part of "Apromore".
+ *
+ * "Apromore" is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * "Apromore" is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ */
+package org.apromore.prodrift.main;
+
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apromore.prodrift.driftcharacterization.CharacterizationAccuracyResult;
+
+
+public class Main {
+	
+	public static List<String> logNameList = new ArrayList<>();
+	
+	public static Map<String, CharacterizationAccuracyResult> CharacterizationAccuracyMap = new LinkedHashMap<>();
+	
+	public static boolean completeCharacterizationExperiment = false;
+	public static boolean completeGoodnessOfKSPTExperiment = false;
+	
+	public static boolean isLogGenerationForNick = false;	
+	public static String startActivity1 = "START";
+	public static String startActivity2 = "START";
+	public static String endActivity1 = "END";
+	
+	public static boolean isStandAlone = true;
+
+	public static void main(final String[] args) throws Exception 
+	{
+		
+	}
+
+}
+	 
