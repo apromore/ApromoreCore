c013a0b4faeded9de33f3d26a289f581e7f60556
raboczi <raboczi@gmail.com>
2017-05-02 15:10:35 +1000

    Moved all the remaining log animation machinery from the editor's BPMN plugin to Log-Animation-Logic.

--- Log-Animation-Portal-Plugin/src/main/java/org/apromore/plugin/editor/loganimation/BPMNAnimationServlet.java
+++ Log-Animation-Portal-Plugin/src/main/java/org/apromore/plugin/editor/loganimation/BPMNAnimationServlet.java
@@ -20,23 +20,7 @@
 
 package org.apromore.plugin.editor.loganimation;
 
-import de.hpi.bpmn2_0.animation.AnimationJSONBuilder;
-import org.json.JSONException;
-import org.json.JSONObject;
-import de.hpi.bpmn2_0.exceptions.BpmnConverterException;
-import de.hpi.bpmn2_0.factory.AbstractBpmnFactory;
-import de.hpi.bpmn2_0.model.Definitions;
-import de.hpi.bpmn2_0.replay.AnimationLog;
-import de.hpi.bpmn2_0.replay.Optimizer;
-import de.hpi.bpmn2_0.replay.ReplayParams;
-import de.hpi.bpmn2_0.replay.Replayer;
-import de.hpi.bpmn2_0.transformation.Diagram2BpmnConverter;
-import org.oryxeditor.server.diagram.basic.BasicDiagram;
-import org.oryxeditor.server.diagram.basic.BasicDiagramBuilder;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+// Java 2 Standard Edition
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -46,11 +30,37 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+
+// Java 2 Enterprise Edition
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// Third party
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.deckfour.xes.model.XLog;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.processmining.plugins.signaturediscovery.encoding.EncodeTraces;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+// Local
+import de.hpi.bpmn2_0.exceptions.BpmnConverterException;
+import de.hpi.bpmn2_0.factory.AbstractBpmnFactory;
+import de.hpi.bpmn2_0.model.Definitions;
+import de.hpi.bpmn2_0.replay.AnimationLog;
+import de.hpi.bpmn2_0.replay.Optimizer;
+import de.hpi.bpmn2_0.replay.ReplayParams;
+import de.hpi.bpmn2_0.replay.Replayer;
+import de.hpi.bpmn2_0.transformation.Diagram2BpmnConverter;
+import org.apromore.service.loganimation.LogAnimationService;
+import org.oryxeditor.server.diagram.basic.BasicDiagram;
+import org.oryxeditor.server.diagram.basic.BasicDiagramBuilder;
 
 /**
  * Create animation for a process diagram from event log
@@ -67,13 +77,13 @@ public class BPMNAnimationServlet extends HttpServlet {
     private static final int MAX_FILE_SIZE = 1024 * 1024 * 500; // 500MB
     private static final int REQUEST_SIZE = 1024 * 1024 * 500; // 500MB
 
-    /**
-     * Log record.
-     */
-    private class Log {
-        String fileName;
-        XLog   xlog;
-        String color;
+    @Inject
+    private LogAnimationService logAnimationService;
+
+    @Override
+    public void init() throws ServletException { 
+        super.init();
+        SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, getServletContext());
     }
 
     /* (non-Javadoc)
@@ -81,8 +91,7 @@ public class BPMNAnimationServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        PrintWriter out=null;
-        List<Log> logs = new ArrayList<>();
+        List<LogAnimationService.Log> logs = new ArrayList<>();
         Set<XLog> xlogs = new HashSet<>();
         //Set<XLog> optimizedLogs = new HashSet<>();
         String jsonData = "";
@@ -120,7 +129,6 @@ public class BPMNAnimationServlet extends HttpServlet {
         } 
 
         try {
-            
             // parses the request's content to extract file data
             List<FileItem> formItems = upload.parseRequest(req);
             Iterator iter = formItems.iterator();
@@ -152,7 +160,7 @@ public class BPMNAnimationServlet extends HttpServlet {
                             String color = item.getString();
 
                             // Record the log
-                            Log log = new Log();
+                            LogAnimationService.Log log = new LogAnimationService.Log();
                             log.fileName = fileName;
                             log.xlog     = xlog;
                             log.color    = item.getString();
@@ -165,135 +173,24 @@ public class BPMNAnimationServlet extends HttpServlet {
                     }
             }            
             
-            /*
-            * ------------------------------------------
-            * Convert JSON map to BPMN objects
-            * ------------------------------------------
-            */         
-            LOGGER.info("Then, convert JSON to BPMN map objects");
-            if (!jsonData.equals("")) {
-                bpmnDefinition = this.getBPMNfromJson(jsonData);
-                LOGGER.info("BPMN Diagram Definition" + bpmnDefinition.toString());
-            } else {
-                LOGGER.info("JSON data sent to server is empty");
-            }
-            
-            
-            /*
-            * ------------------------------------------
-            * Optimize logs and process model
-            * ------------------------------------------
-            */
-            Optimizer optimizer = new Optimizer();
-            for (Log log : logs) {
-                //optimizedLogs.add(optimizer.optimizeLog(log.xlog));
-                log.xlog = optimizer.optimizeLog(log.xlog);
-            }
-            bpmnDefinition = optimizer.optimizeProcessModel(bpmnDefinition);
+            // Convert the model from Signavio JSON format to a BPMN-formatted string
+            StringWriter stringWriter = new StringWriter();
+            JAXBContext.newInstance("de.hpi.bpmn2_0.model:de.hpi.bpmn2_0.model.extension.synergia", getClass().getClassLoader())
+                       .createMarshaller()
+                       .marshal(getBPMNfromJson(jsonData), stringWriter);
 
+            // Use the log animation service to obtain the animation data
+            String jsonAnimationData = logAnimationService.createAnimation(stringWriter.toString(), logs);
             
-            
-            /*
-            * ------------------------------------------
-            * Check BPMN diagram validity and replay log
-            * ------------------------------------------
-            */              
-            
-            //Reading backtracking properties for testing
-            String propertyFile = "/properties.xml";
-            InputStream is = getServletContext().getResourceAsStream(propertyFile);            
-            Properties props = new Properties();            
-            props.loadFromXML(is);
-            ReplayParams params = new ReplayParams();
-            params.setMaxCost(Double.valueOf(props.getProperty("MaxCost")).doubleValue());
-            params.setMaxDepth(Integer.valueOf(props.getProperty("MaxDepth")).intValue());
-            params.setMinMatchPercent(Double.valueOf(props.getProperty("MinMatchPercent")).doubleValue());
-            params.setMaxMatchPercent(Double.valueOf(props.getProperty("MaxMatchPercent")).doubleValue());
-            params.setMaxConsecutiveUnmatch(Integer.valueOf(props.getProperty("MaxConsecutiveUnmatch")).intValue());
-            params.setActivityMatchCost(Double.valueOf(props.getProperty("ActivityMatchCost")).doubleValue());
-            params.setActivitySkipCost(Double.valueOf(props.getProperty("ActivitySkipCost")).doubleValue());
-            params.setEventSkipCost(Double.valueOf(props.getProperty("EventSkipCost")).doubleValue());
-            params.setNonActivityMoveCost(Double.valueOf(props.getProperty("NonActivityMoveCost")).doubleValue());
-            params.setTraceChunkSize(Integer.valueOf(props.getProperty("TraceChunkSize")).intValue());
-            params.setMaxNumberOfNodesVisited(Integer.valueOf(props.getProperty("MaxNumberOfNodesVisited")).intValue());
-            params.setMaxActivitySkipPercent(Double.valueOf(props.getProperty("MaxActivitySkipPercent")).doubleValue());
-            params.setMaxNodeDistance(Integer.valueOf(props.getProperty("MaxNodeDistance")).intValue());
-            params.setTimelineSlots(Integer.valueOf(props.getProperty("TimelineSlots")).intValue());
-            params.setTotalEngineSeconds(Integer.valueOf(props.getProperty("TotalEngineSeconds")).intValue());
-            params.setProgressCircleBarRadius(Integer.valueOf(props.getProperty("ProgressCircleBarRadius")).intValue());
-            params.setSequenceTokenDiffThreshold(Integer.valueOf(props.getProperty("SequenceTokenDiffThreshold")).intValue());
-            params.setMaxTimePerTrace(Long.valueOf(props.getProperty("MaxTimePerTrace")).longValue());
-            params.setMaxTimeShortestPathExploration(Long.valueOf(props.getProperty("MaxTimeShortestPathExploration")).longValue());
-            params.setExactTraceFitnessCalculation(props.getProperty("ExactTraceFitnessCalculation"));
-            params.setBacktrackingDebug(props.getProperty("BacktrackingDebug"));
-            params.setExploreShortestPathDebug(props.getProperty("ExploreShortestPathDebug"));     
-            params.setCheckViciousCycle(props.getProperty("CheckViciousCycle"));
-            params.setStartEventToFirstEventDuration(Integer.valueOf(props.getProperty("StartEventToFirstEventDuration")).intValue());
-            params.setLastEventToEndEventDuration(Integer.valueOf(props.getProperty("LastEventToEndEventDuration")).intValue());            
-
-            Replayer replayer = new Replayer(bpmnDefinition, params);
-            ArrayList<AnimationLog> replayedLogs = new ArrayList();
-            if (replayer.isValidProcess()) {
-                LOGGER.info("Process " + bpmnDefinition.getId() + " is valid");
-                EncodeTraces.getEncodeTraces().read(xlogs); //build a mapping from traceId to charstream
-                for (Log log: logs) {
-
-                    AnimationLog animationLog = replayer.replay(log.xlog, log.color);
-                    //AnimationLog animationLog = replayer.replayWithMultiThreading(log.xlog, log.color);
-                    if (animationLog !=null && !animationLog.isEmpty()) {
-                        replayedLogs.add(animationLog);
-                    }
-                }
-
-            } else {
-                LOGGER.info(replayer.getProcessCheckingMsg());
-            }
-            
-            /*
-            * ------------------------------------------
-            * Return Json animation
-            * ------------------------------------------
-            */
-            LOGGER.info("Start sending back JSON animation script to browser");
-            if (replayedLogs.size() > 0) {
-                out = res.getWriter();
-                res.setContentType("text/html");  // Ext2JS's file upload requires this rather than "application/json"
-                res.setStatus(200);
-                
-                //To be replaced
-                AnimationJSONBuilder jsonBuilder = new AnimationJSONBuilder(replayedLogs, replayer, params);
-                JSONObject json = jsonBuilder.parseLogCollection();
-                json.put("success", true);  // Ext2JS's file upload requires this flag
-                String string = json.toString();
-                //LOGGER.info(string);
-                jsonBuilder.clear();
-                
-                out.write(string);
-            }
-            else {
-                /*
-                out = res.getWriter();
-                res.setContentType("text/html");
-                res.setStatus(204);
-                out.write("");
-                */
-                String json = "{success:false, errors: {errormsg: '" + "No logs can be played." + "'}}"; 
-                res.setContentType("text/html; charset=UTF-8");
-                res.getWriter().print(json);                
-            }
+            // Write out the animation data as the servlet response
+            PrintWriter out = res.getWriter();
+            res.setContentType("text/html");  // Ext2JS's file upload requires this rather than "application/json"
+            res.setStatus(200);
+            out.write(jsonAnimationData);
             
         } catch (Exception e) {
             try {
                 LOGGER.log(Level.SEVERE, "Failed to POST logs", e);
-                /*
-                res.setStatus(500);
-                res.setContentType("text/plain");
-                PrintWriter writer = new PrintWriter(out);
-                writer.println("Failed to generate animation JSON script " + e);
-                e.printStackTrace(writer);
-                e.printStackTrace();
-                res.getWriter().write(e.toString());
-                */
                 String json = "{success:false, errors: {errormsg: '" +e.getMessage() + "'}}"; 
                 res.setContentType("text/html; charset=UTF-8");
                 res.getWriter().print(json);
@@ -313,41 +210,5 @@ public class BPMNAnimationServlet extends HttpServlet {
         Definitions definitions = converter.getDefinitionsFromDiagram();
 
         return definitions;
-    }
-
-    // Returns the contents of the file in a byte array.
-    private static byte[] getBytesFromFile(File file) throws IOException {
-        InputStream is = new FileInputStream(file);
-
-        // Get the size of the file
-        long length = file.length();
-
-        // You cannot create an array using a long type.
-        // It needs to be an int type.
-        // Before converting to an int type, check
-        // to ensure that file is not larger than Integer.MAX_VALUE.
-        if (length > Integer.MAX_VALUE) {
-            // File is too large
-        }
-
-        // Create the byte array to hold the data
-        byte[] bytes = new byte[(int)length];
-
-        // Read in the bytes
-        int offset = 0;
-        int numRead = 0;
-        while (offset < bytes.length
-                && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
-            offset += numRead;
-        }
-
-        // Ensure all the bytes have been read in
-        if (offset < bytes.length) {
-            throw new IOException("Could not completely read file "+file.getName());
-        }
-
-        // Close the input stream and return bytes
-        is.close();
-        return bytes;
     }
 }
