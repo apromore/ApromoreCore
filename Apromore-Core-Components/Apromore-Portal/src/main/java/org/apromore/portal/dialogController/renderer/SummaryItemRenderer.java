94c8fab3509e30fe309f54179449be3a6a7f4743
hoangnguyen7x <hoangnguyen7x@yahoo.com>
2019-02-26 13:57:50 +1000

    Double-click will open the model in BPMN.io instead of Signavio.

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/renderer/SummaryItemRenderer.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/renderer/SummaryItemRenderer.java
@@ -99,13 +99,11 @@ public class SummaryItemRenderer implements ListitemRenderer {
                 VersionSummaryType version = getLatestVersion(process.getVersionSummaries());
                 AnnotationsType annotation = getLastestAnnotation(version.getAnnotations());
                 if (annotation != null) {
-                    mainController.editProcess(process, version, getNativeType(annotation.getNativeType()), annotation.getAnnotationName().get(0),
-                        "false", new HashSet<RequestParameterType<?>>());
-//                    mainController.visualizeLog();
+                    mainController.editProcess2(process, version, getNativeType(annotation.getNativeType()), annotation.getAnnotationName().get(0),
+                        "false", new HashSet<RequestParameterType<?>>(), false);
                 } else {
-                    mainController.editProcess(process, version, getNativeType(process.getOriginalNativeType()), null, "false",
-                            new HashSet<RequestParameterType<?>>());
-//                    mainController.visualizeLog();
+                    mainController.editProcess2(process, version, getNativeType(process.getOriginalNativeType()), null, "false",
+                            new HashSet<RequestParameterType<?>>(), false);
                 }
             }
 
