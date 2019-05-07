898dfac322897c13600fa92fb427c645e79fc042
hoangnguyen7x <hoangnguyen7x@yahoo.com>
2019-05-07 16:27:44 +1000

    Restructure and document Log Filter

---
+++ Apromore-Custom-Plugins/Process-Discoverer-Plugin/src/main/java/org/apromore/plugin/processdiscoverer/impl/filter/Type.java
@@ -0,0 +1,7 @@
+package org.apromore.plugin.processdiscoverer.impl.filter;
+
+public enum Type {
+    CONCEPT_NAME, DIRECT_FOLLOW, EVENTUAL_FOLLOW, LIFECYCLE_TRANSITION, 
+    ORG_GROUP, ORG_RESOURCE, ORG_ROLE, TIME_DURATION, TIME_TIMESTAMP,
+    UNKNOWN
+}
