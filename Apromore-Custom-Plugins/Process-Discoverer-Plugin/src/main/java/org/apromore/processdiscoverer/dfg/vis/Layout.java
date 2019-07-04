65fc26b306ece4ae384df95a841a13ccebe2a70f
Bruce Nguyen <hoangnguyen7x@yahoo.com>
2019-07-04 14:39:56 +1000

    Add getDistWeight() method to convert from (x,y) waypoint to (dist,weight) value.

--- Apromore-Custom-Plugins/Process-Discoverer-Plugin/src/main/java/org/apromore/processdiscoverer/dfg/vis/Layout.java
+++ Apromore-Custom-Plugins/Process-Discoverer-Plugin/src/main/java/org/apromore/processdiscoverer/dfg/vis/Layout.java
@@ -11,6 +11,11 @@ import org.processmining.models.graphbased.directed.bpmn.BPMNNode;
 
 import com.google.gwt.dev.util.collect.HashMap;
 
+/**
+ * Represent the layout for BPMNDiagram 
+ * @author Bruce Nguyen
+ *
+ */
 public class Layout {
 	private Map<String,LayoutElement> elementIDMap = new HashMap<>();
 	private double nodeWidth = 10;
@@ -63,5 +68,41 @@ public class Layout {
 	
 	public double getVerticalLength(BPMNEdge<? extends BPMNNode, ? extends BPMNNode> edge) {
 		return this.getVerticalLength(edge.getSource(), edge.getTarget());
+	}
+	
+	/**
+	 * Convert from a point coordinate (x,y) to (distance,weight) used in cytoscape segment point
+	 * From: stackoverflow.com/questions/53622515/cytoscape-js-for-edge-segment-transforming-coordinates-onto-segment-distances/54551842
+	 * @param sX: source node X
+	 * @param sY: source node Y
+	 * @param tX: target node X
+	 * @param tY: target node Y
+	 * @param PointX: point X
+	 * @param PointY: point Y
+	 * @return: 2-element array {distance, weight}
+	 */
+	public Double[] getDistWeight(double sX, double sY, double tX, double tY, double PointX, double PointY) {
+	    double W, D;
+
+	    D = ( PointY - sY + (sX-PointX) * (sY-tY) / (sX-tX) ) /  Math.sqrt( 1 + Math.pow((sY-tY) / (sX-tX), 2) );
+	    W = Math.sqrt(  Math.pow(PointY-sY,2) + Math.pow(PointX-sX,2) - Math.pow(D,2)  );
+
+	    double distAB = Math.sqrt(Math.pow(tX-sX, 2) + Math.pow(tY-sY, 2));
+	    W = W / distAB;
+
+	    //Check whether the point (PointX, PointY) is on right or left of the line src to tgt. 
+	    //For instance : a point C(X, Y) and line (AB).  d=(xB-xA)(yC-yA)-(yB-yA)(xC-xA). 
+	    //If d>0, then C is on left of the line. if d<0, it is on right. if d=0, it is on the line.
+	    double delta1 = (tX-sX)*(PointY-sY)-(tY-sY)*(PointX-sX);
+	    delta1 = (delta1 >= 0) ? 1 : -1;
+	        
+	    //check whether the point (PointX, PointY) is "behind" the line src to tgt
+	    double delta2 = (tX-sX)*(PointX-sX)+(tY-sY)*(PointY-sY);
+	    delta2 = (delta2 >= 0) ? 1 : -1;
+
+	    D = Math.abs(D) * delta1;   //ensure that sign of D is same as sign of delta1. Hence we need to take absolute value of D and multiply by delta1
+	    W = W * delta2;
+
+	    return new Double[] {D,W};
 	}
 }
