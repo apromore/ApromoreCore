1334109ca67d974ec6221f5185a10050cf9bc64c
Bruce Nguyen <hoangnguyen7x@yahoo.com>
2019-08-07 00:16:53 +1000

    Add new Log-Filter-Portal-Plugin

---
+++ Apromore-Custom-Plugins/Log-Filter-Portal-Plugin/src/main/java/org/apromore/plugin/portal/logfilter/LogFilterInterface.java
@@ -0,0 +1,28 @@
+package org.apromore.plugin.portal.logfilter;
+
+import java.io.IOException;
+import java.util.List;
+import org.apromore.logfilter.criteria.LogFilterCriterion;
+import org.apromore.plugin.portal.PortalContext;
+import org.deckfour.xes.model.XLog;
+
+/**
+ * This interface is used to register this plugin as an OSGi service for
+ * other plugins to call to 
+ * @author Bruce Nguyen
+ *
+ */
+public interface LogFilterInterface {
+	/**
+	 * Open the log filter UI
+	 * @param portalContext
+	 * @param log
+	 * @param label: the name assigned to this list of criteria
+	 * @param originalCriteria: the input criteria which can be edited in the log filter UI 
+	 * @param logStats: log statistics
+	 * @return: an array, 1st element: filtered XLog, 2nd element: List<LogFilterCriterion>
+	 * @throws IOException
+	 */
+	public void execute(PortalContext portalContext, XLog log, String label, 
+			List<LogFilterCriterion> originalCriteria, LogStatistics logStats) throws IOException;
+}
