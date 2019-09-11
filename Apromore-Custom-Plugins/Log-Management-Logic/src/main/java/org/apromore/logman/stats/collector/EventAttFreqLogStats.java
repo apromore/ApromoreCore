65751fea7f96c9f08bef50b3151eb8924b3b18de
Bruce Nguyen <hoangnguyen7x@yahoo.com>
2019-09-11 12:10:58 +1000

    Update.

--- Apromore-Custom-Plugins/Log-Management-Logic/src/main/java/org/apromore/logman/stats/collector/EventAttFreqLogStats.java
+++ Apromore-Custom-Plugins/Log-Management-Logic/src/main/java/org/apromore/logman/stats/collector/EventAttFreqLogStats.java
@@ -3,16 +3,8 @@ package org.apromore.logman.stats.collector;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-
-import org.apromore.logman.log.Constants;
-import org.apromore.logman.log.LogVisitor;
-import org.apromore.logman.log.activityaware.AXLog;
-import org.apromore.logman.log.activityaware.AXTrace;
-import org.apromore.logman.log.activityaware.Activity;
-import org.apromore.logman.log.event.LogFilterListener;
 import org.apromore.logman.log.event.LogFilteredEvent;
 import org.deckfour.xes.model.XAttribute;
-import org.deckfour.xes.model.XAttributeMap;
 import org.deckfour.xes.model.XAttributeTimestamp;
 import org.deckfour.xes.model.XEvent;
 
