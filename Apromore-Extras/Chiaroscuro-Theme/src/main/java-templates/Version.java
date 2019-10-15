653ef50f32d8113d33bdfc912a9edb2de5885731
raboczi <raboczi@icloud.com>
2019-10-15 16:38:33 +1100

    Initial commit of the Chiaroscuro ZK theme.
    Chiaroscuro uses CSS variables from a central CSS file rather than hardcoding colors and requiring the entire theme to be changed -- it's "the last ZK theme you'll ever need".
    Because compiling it requires lessc, none of this is built by default, and there isn't a profile to activate it yet, either.
    The portal and process discoverer are correctly linked to the new CSS the Chiaroscuro requires.
    The zk-osgi bundle DOESN'T include the Chiaroscuro theme yet.

---
+++ Apromore-Extras/Chiaroscuro-Theme/src/main/java-templates/Version.java
@@ -0,0 +1,30 @@
+/* Version.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Oct 6, 2010 12:30:37 PM , Created by jimmy
+}}IS_NOTE
+
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under LGPL Version 3.0 in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package org.zkoss.theme.${theme.name};
+
+/**
+ *  The version of the ${theme.name} theme.
+ * {@link #UID} must be the same as the version specified in lang-addon.xml.
+ * @author Jimmy
+ */
+public class Version {
+	/** Returns the version UID.
+	 */
+	public static final String UID = "${project.version}";
+}
