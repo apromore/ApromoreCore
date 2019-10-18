97a46b76c1956daad9a372df34488c9115d982df
Chii Chang <chiichang1@gmail.com>
2019-10-18 20:25:43 +1100

    update

--- Apromore-Custom-Plugins/Log-Filter-Logic/src/main/java/org/apromore/logfilter/criteria/impl/LogFilterCriterionDirectFollow.java
+++ Apromore-Custom-Plugins/Log-Filter-Logic/src/main/java/org/apromore/logfilter/criteria/impl/LogFilterCriterionDirectFollow.java
@@ -23,31 +23,95 @@ package org.apromore.logfilter.criteria.impl;
 import org.apromore.logfilter.criteria.model.Action;
 import org.apromore.logfilter.criteria.model.Containment;
 import org.apromore.logfilter.criteria.model.Level;
+import org.deckfour.xes.extension.std.XTimeExtension;
 import org.deckfour.xes.model.XEvent;
 import org.deckfour.xes.model.XTrace;
+import org.eclipse.collections.impl.set.mutable.UnifiedSet;
 
+import java.util.Calendar;
 import java.util.Set;
 
 public class LogFilterCriterionDirectFollow extends AbstractLogFilterCriterion {
 
+    String attributeOption = "";
+    UnifiedSet<String> followSet = new UnifiedSet<>();
+    String requiredAttributeString = "";
+    String intervalString = "";
+
     public LogFilterCriterionDirectFollow(Action action, Containment containment, Level level, String label, String attribute, Set<String> value) {
         super(action, containment, level, label, attribute, value);
+
+        for (String s : value) {
+            if(s.contains("=>")) {
+                followSet.put(s);
+            }else if(s.contains("@&")) {
+                requiredAttributeString = "have the same \"" + s.substring(2) + "\"";
+            }else if(s.contains("@!")) {
+                requiredAttributeString = "have the different \"" + s.substring(2) + "\"";
+            }else if(s.contains("@$")) {
+                attributeOption = s.substring(2);
+            }else if(s.contains("@<|")) {
+                intervalString = " time interval is less than " + s.substring(3);
+            }else if(s.contains("@<=")) {
+                intervalString = " time interval is up to " + s.substring(3);
+            }else if(s.contains("@>|")) {
+                intervalString = " time interval is greater than " + s.substring(3);
+            }else if(s.contains("@>=")) {
+                intervalString = " time interval is at least " + s.substring(2);
+            }
+        }
     }
 
     @Override
     public boolean matchesCriterion(XTrace trace) {
         if(level == Level.TRACE) {
-            String s = trace.get(0).getAttributes().get("concept:name").toString();
-//            if (value.contains("|> => " + s)) return true;
-            if (value.contains("[Start] => " + s)) return true;
-            for (int i = 0; i < trace.size() - 1; i++) {
-                String event1 = trace.get(i).getAttributes().get("concept:name").toString();
-                String event2 = trace.get(i + 1).getAttributes().get("concept:name").toString();
-                if (value.contains(event1 + " => " + event2)) return true;
+            String attributeOption = "concept:name";
+            String sameAttribute = "";
+            String differentAttribute = "";
+            String lessTimeString = "";
+            String lessEqualTimeString = "";
+            String moreTimeString = "";
+            String moreEqualTimeString = "";
+            for (String s : value) {
+                if(s.contains("@&")) sameAttribute = s.substring(2);
+                if(s.contains("@!")) differentAttribute = s.substring(2);
+                if(s.contains("@$")) attributeOption = s.substring(2);
+                if(s.contains("@<|")) lessTimeString = s.substring(3);
+                if(s.contains("@<=")) lessEqualTimeString = s.substring(3);
+                if(s.contains("@>|")) moreTimeString = s.substring(3);
+                if(s.contains("@>=")) moreEqualTimeString = s.substring(3);
             }
-            String e = trace.get(trace.size() - 1).getAttributes().get("concept:name").toString();
-//            return (value.contains(e + " => []"));
-            return (value.contains(e + " => [End]"));
+            String s = trace.get(0).getAttributes().get(attributeOption).toString();
+            if (value.contains("[Start] => " + s)) return true;
+            String e = trace.get(trace.size() - 1).getAttributes().get(attributeOption).toString();
+            if ((value.contains(e + " => [End]"))) return true;
+            for(int i=0; i<(trace.size() -1); i++) {
+                XEvent event1 = trace.get(i);
+                XEvent event2 = trace.get(i + 1);
+                String event1V = event1.getAttributes().get(attributeOption).toString();
+                String event2V = event2.getAttributes().get(attributeOption).toString();
+                if (value.contains(event1V + " => " + event2V)) {
+                    if(!sameAttribute.equals("")) {
+                        if (haveSameAttributeValue(event1, event2, sameAttribute)) return false;
+                    }
+                    if(!differentAttribute.equals("")) {
+                        if (haveSameAttributeValue(event1, event2, differentAttribute)) return false;
+                    }
+                    if(!lessTimeString.equals("")) {
+                        if (!haveIntervalShoterThan(event1, event2, lessTimeString)) return false;
+                    }
+                    if(!lessEqualTimeString.equals("")) {
+                        if (!haveIntervalShoterEqualThan(event1, event2, lessEqualTimeString)) return false;
+                    }
+                    if(!moreTimeString.equals("")) {
+                        if (!haveIntervalLongerThan(event1, event2, moreTimeString)) return false;
+                    }
+                    if(!moreEqualTimeString.equals("")) {
+                        if (!haveIntervalLongerEqualThan(event1, event2, moreEqualTimeString)) return false;
+                    }
+                    return true;
+                }
+            }
         }
         return false;
     }
@@ -57,4 +121,115 @@ public class LogFilterCriterionDirectFollow extends AbstractLogFilterCriterion {
         return false;
     }
 
+    @Override
+    public String toString() {
+        String displayString = super.getAction() +
+                " all traces where their events contain the Directly-follows relation of the \"" +
+                attributeOption + "\" equal to " + followSet.toString();
+        if(!requiredAttributeString.equals("")) displayString += " and " + requiredAttributeString;
+        if(!intervalString.equals("")) displayString += " and " + intervalString;
+        return displayString;
+    }
+
+
+
+    private boolean haveSameAttributeValue(XEvent event1, XEvent event2, String attributeKey) {
+        if(event1.getAttributes().get(attributeKey) == null || event2.getAttributes().get(attributeKey) == null) return false;
+        String value1 = event1.getAttributes().get(attributeKey).toString();
+        String value2 = event2.getAttributes().get(attributeKey).toString();
+        return value1.equals(value2);
+    }
+
+    private boolean haveIntervalShoterThan(XEvent event1, XEvent event2, String intervalString) {
+        long intervalValue = millisecondsOfString(intervalString);
+        long e1Time = epochMilliOf(event1);
+        long e2Time = epochMilliOf(event2);
+        long e1e2Interval = e2Time - e1Time;
+        if(e1e2Interval < intervalValue) return true;
+        else return false;
+    }
+
+    private boolean haveIntervalShoterEqualThan(XEvent event1, XEvent event2, String intervalString) {
+        long intervalValue = millisecondsOfString(intervalString);
+        long e1Time = epochMilliOf(event1);
+        long e2Time = epochMilliOf(event2);
+        long e1e2Interval = e2Time - e1Time;
+        if(e1e2Interval <= intervalValue) return true;
+        else return false;
+    }
+
+    private boolean haveIntervalLongerThan(XEvent event1, XEvent event2, String intervalString) {
+        long intervalValue = millisecondsOfString(intervalString);
+        long e1Time = epochMilliOf(event1);
+        long e2Time = epochMilliOf(event2);
+        long e1e2Interval = e2Time - e1Time;
+        if(e1e2Interval > intervalValue) return true;
+        else return false;
+    }
+
+    private boolean haveIntervalLongerEqualThan(XEvent event1, XEvent event2, String intervalString) {
+        long intervalValue = millisecondsOfString(intervalString);
+        long e1Time = epochMilliOf(event1);
+        long e2Time = epochMilliOf(event2);
+        long e1e2Interval = e2Time - e1Time;
+        if(e1e2Interval >= intervalValue) return true;
+        else return false;
+    }
+
+    private long millisecondsOfString(String intervalString) {
+        long input = Long.valueOf(intervalString.substring(0, intervalString.indexOf(" ")));
+        String unitString = intervalString.substring(intervalString.indexOf(" ") + 1);
+        long unitValue = unitStringToLong(unitString);
+        long intervalValue = input * unitValue;
+        return intervalValue;
+    }
+
+    private long unitStringToLong(String s) { //2019-10-18
+        if(s.toLowerCase().equals("years")) return new Long("31536000000");
+        if(s.toLowerCase().equals("months")) return new Long("2678400000");
+        if(s.toLowerCase().equals("weeks")) return new Long("604800000");
+        if(s.toLowerCase().equals("days")) return new Long("86400000");
+        if(s.toLowerCase().equals("hours")) return new Long("3600000");
+        if(s.toLowerCase().equals("minutes")) return new Long("60000");
+        if(s.toLowerCase().equals("seconds")) return new Long("1000");
+        return new Long(0);
+    }
+
+    private static long epochMilliOf(XEvent xEvent) {
+        String timestampString = xEvent.getAttributes().get(XTimeExtension.KEY_TIMESTAMP).toString();
+        Calendar calendar = javax.xml.bind.DatatypeConverter.parseDateTime(timestampString);
+        return calendar.getTimeInMillis();
+    }
 }
+
+
+//public class LogFilterCriterionDirectFollow extends AbstractLogFilterCriterion {
+//
+//    public LogFilterCriterionDirectFollow(Action action, Containment containment, Level level, String label, String attribute, Set<String> value) {
+//        super(action, containment, level, label, attribute, value);
+//    }
+//
+//    @Override
+//    public boolean matchesCriterion(XTrace trace) {
+//        if(level == Level.TRACE) {
+//            String s = trace.get(0).getAttributes().get("concept:name").toString();
+////            if (value.contains("|> => " + s)) return true;
+//            if (value.contains("[Start] => " + s)) return true;
+//            for (int i = 0; i < trace.size() - 1; i++) {
+//                String event1 = trace.get(i).getAttributes().get("concept:name").toString();
+//                String event2 = trace.get(i + 1).getAttributes().get("concept:name").toString();
+//                if (value.contains(event1 + " => " + event2)) return true;
+//            }
+//            String e = trace.get(trace.size() - 1).getAttributes().get("concept:name").toString();
+////            return (value.contains(e + " => []"));
+//            return (value.contains(e + " => [End]"));
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean matchesCriterion(XEvent event) {
+//        return false;
+//    }
+//
+//}
