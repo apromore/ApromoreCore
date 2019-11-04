d5a7dc44d2d70c430f32e25646653b85f6042ac1
raboczi <raboczi@icloud.com>
2019-11-04 15:56:20 +1000

    Tidied the formatting of the CSVImporterLogic interface.

--- Apromore-Custom-Plugins/CSVImporter-Logic/src/main/java/org/apromore/service/csvimporter/CSVImporterLogic.java
+++ Apromore-Custom-Plugins/CSVImporter-Logic/src/main/java/org/apromore/service/csvimporter/CSVImporterLogic.java
@@ -17,40 +17,28 @@ import java.util.List;
  */
 public interface CSVImporterLogic {
 
-    /**
-     * Sample service API method.
-     *
-     * @param n  parameter the service requires
-     * @return some result
-     * @throws SampleException if something goes wrong
-     */
     List<LogModel> prepareXesModel(CSVReader r);
     void setHeads(List<String> line);
     HashMap<String, Integer> getHeads();
     void setLine(List<String>  line);
     void automaticFormat(ListModelList<String[]> result, List<String> myHeader);
-     void resetLine();
-     void resetHead();
-     void resetList() ;
-     void setOtherTimestamps(ListModelList<String[]> result);
+    void resetLine();
+    void resetHead();
+    void resetList() ;
+    void setOtherTimestamps(ListModelList<String[]> result);
     void setOtherAll(Window window);
     void setIgnoreAll(Window window);
     void setLists(int cols, HashMap<String, Integer> heads, String boxwidth);
-     List<Listbox> getLists();
+    List<Listbox> getLists();
     void tryParsing(String format, int colPos);
     void openPopUp();
-     void setPopUPBox(Div popUPBox);
+    void setPopUPBox(Div popUPBox);
 
-     void setPopupID(String popupID);
+    void setPopupID(String popupID);
 
-     void setTextboxID(String textboxID);
+    void setTextboxID(String textboxID);
 
-     void setLabelID(String labelID);
+    void setLabelID(String labelID);
     XLog createXLog(List<LogModel> traces);
     void toXESfile(XLog xLog, String FileName) throws FileNotFoundException, IOException;
-    /**
-     * Something that might go wrong.
-     */
-    public static class SampleException extends Exception {
-    }
 }
