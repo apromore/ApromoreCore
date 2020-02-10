5060176573ddf858398a088e1969b81aa5eefbb5
raboczi <raboczi@icloud.com>
2020-02-10 14:29:50 +1000

    Added license header to Signavio Core Components code.

--- Apromore-Custom-Plugins/Apromore-Editor/bpmn20xmlbasic/src/main/java/de/hpi/bpmn2_0/model/extension/synergia/ConfigurationMapping.java
+++ Apromore-Custom-Plugins/Apromore-Editor/bpmn20xmlbasic/src/main/java/de/hpi/bpmn2_0/model/extension/synergia/ConfigurationMapping.java
@@ -1,24 +1,44 @@
-/*
- * Copyright © 2009-2018 The Apromore Initiative.
- *
- * This file is part of "Apromore".
- *
- * "Apromore" is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
- *
- * "Apromore" is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program.
- * If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
- */
 
 package de.hpi.bpmn2_0.model.extension.synergia;
+
+/*-
+ * #%L
+ * Signavio Core Components
+ * %%
+ * Copyright (C) 2006 - 2020 Philipp Berger, Martin Czuchra, Gero Decker,
+ * Ole Eckermann, Lutz Gericke,
+ * Alexander Hold, Alexander Koglin, Oliver Kopp, Stefan Krumnow,
+ * Matthias Kunze, Philipp Maschke, Falko Menge, Christoph Neijenhuis,
+ * Hagen Overdick, Zhen Peng, Nicolas Peters, Kerstin Pfitzner, Daniel Polak,
+ * Steffen Ryll, Kai Schlichting, Jan-Felix Schwarz, Daniel Taschik,
+ * Willi Tscheschner, Björn Wagner, Sven Wagner-Boysen, Matthias Weidlich
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ * 
+ * 
+ * Ext JS (http://extjs.com/) is used under the terms of the Open Source LGPL 3.0
+ * license.
+ * The license and the source files can be found in our SVN repository at:
+ * http://oryx-editor.googlecode.com/.
+ * #L%
+ */
 
 import java.util.ArrayList;
 import java.util.List;
