cc2aa865ff059d83a9f0816bb57eba9222d3f19a
raboczi <raboczi@icloud.com>
2020-02-27 15:33:06 +1000

    Fix for AP-1161.

--- Supplements/database/db-mysql.sql
+++ Supplements/database/db-mysql.sql
@@ -884,12 +884,12 @@ CREATE INDEX `idx_fragment_distance` ON `fragment_distance` (`ged`) USING BTREE;
 
 LOCK TABLES `native_type` WRITE;
 /*!40000 ALTER TABLE `native_type` DISABLE KEYS */;
-INSERT INTO `native_type` VALUES (1,'EPML 2.0','epml');
-INSERT INTO `native_type` VALUES (2,'XPDL 2.2','xpdl');
-INSERT INTO `native_type` VALUES (3,'PNML 1.3.2', 'pnml');
-INSERT INTO `native_type` VALUES (4,'YAWL 2.2', 'yawl');
+/*INSERT INTO `native_type` VALUES (1,'EPML 2.0','epml');*/
+/*INSERT INTO `native_type` VALUES (2,'XPDL 2.2','xpdl');*/
+/*INSERT INTO `native_type` VALUES (3,'PNML 1.3.2', 'pnml');*/
+/*INSERT INTO `native_type` VALUES (4,'YAWL 2.2', 'yawl');*/
 INSERT INTO `native_type` VALUES (5,'BPMN 2.0', 'bpmn');
-INSERT INTO `native_type` VALUES (6,'AML fragment', 'aml');
+/*INSERT INTO `native_type` VALUES (6,'AML fragment', 'aml');*/
 /*!40000 ALTER TABLE `native_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
