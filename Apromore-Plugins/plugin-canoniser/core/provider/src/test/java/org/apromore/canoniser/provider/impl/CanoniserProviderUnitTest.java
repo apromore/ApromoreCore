8bf433d5d7d3a7c83d4408f1aedd5406a6705ddd
frank-ma-leonardo <f.ma@leonardo.com.au>
2020-02-28 16:41:34 +1100

    Update copyright header in ApromoreCore (Exclude Apromore-Editor, OpenXES)

--- Apromore-Plugins/plugin-canoniser/core/provider/src/test/java/org/apromore/canoniser/provider/impl/CanoniserProviderUnitTest.java
+++ Apromore-Plugins/plugin-canoniser/core/provider/src/test/java/org/apromore/canoniser/provider/impl/CanoniserProviderUnitTest.java
@@ -1,18 +1,21 @@
-/**
- *  Copyright 2012, Felix Mannhardt
+/*
+ * This file is part of "Apromore".
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Copyright (C) 2019 - 2020 The University of Melbourne.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * "Apromore" is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * "Apromore" is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
  */
 package org.apromore.canoniser.provider.impl;
 
