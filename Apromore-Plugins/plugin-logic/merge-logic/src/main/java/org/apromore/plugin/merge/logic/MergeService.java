b6d57519c17f5184c6c9f09918b876fb217f1683
frank-ma-leonardo <f.ma@leonardo.com.au>
2020-03-26 14:13:51 +1100

    Fix preambles in merge-logic and merge-portal

--- Apromore-Plugins/plugin-logic/merge-logic/src/main/java/org/apromore/plugin/merge/logic/MergeService.java
+++ Apromore-Plugins/plugin-logic/merge-logic/src/main/java/org/apromore/plugin/merge/logic/MergeService.java
@@ -1,6 +1,7 @@
 /*
  * This file is part of "Apromore".
  *
+ * Copyright (C) 2013 - 2016 Reina Uba.
  * Copyright (C) 2016 - 2017 Queensland University of Technology.
  * Copyright (C) 2018, 2020 The University of Melbourne.
  *
