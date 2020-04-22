135237aaf17d1c17fb491b91493bcedf0a966a25
Ibrahim <i.mahdy93@gmail.com>
2020-04-22 20:26:53 +0300

    AP-1427 - Use first non empty value for case attributes

---
+++ Apromore-Custom-Plugins/CSVImporter-Logic/src/main/java/org/apromore/service/csvimporter/impl/CreateXLog.java
@@ -0,0 +1,181 @@
+package org.apromore.service.csvimporter.impl;
+
+import org.deckfour.xes.extension.std.XConceptExtension;
+import org.deckfour.xes.extension.std.XLifecycleExtension;
+import org.deckfour.xes.extension.std.XOrganizationalExtension;
+import org.deckfour.xes.extension.std.XTimeExtension;
+import org.deckfour.xes.factory.XFactory;
+import org.deckfour.xes.factory.XFactoryNaiveImpl;
+import org.deckfour.xes.model.*;
+import org.deckfour.xes.model.impl.XAttributeLiteralImpl;
+import org.deckfour.xes.model.impl.XAttributeTimestampImpl;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+class CreateXLog {
+
+    XLog generateXLog(List<LogEventModel> events) {
+        if (events == null) return null;
+
+        XFactory xFactory = new XFactoryNaiveImpl();
+
+        XConceptExtension concept = XConceptExtension.instance();
+        XLifecycleExtension lifecycle = XLifecycleExtension.instance();
+        XTimeExtension timestamp = XTimeExtension.instance();
+        XOrganizationalExtension resource = XOrganizationalExtension.instance();
+
+        XLog xLog = xFactory.createLog();
+        xLog.getExtensions().add(concept);
+        xLog.getExtensions().add(lifecycle);
+        xLog.getExtensions().add(timestamp);
+        xLog.getExtensions().add(resource);
+
+        lifecycle.assignModel(xLog, XLifecycleExtension.VALUE_MODEL_STANDARD);
+
+        XTrace xTrace = null;
+        XEvent xEvent;
+        List<XEvent> allEvents = new ArrayList<XEvent>();
+        String newTraceID = null;    // to keep track of events, when a new trace is created we assign its value and add the respective events for the trace.
+        Map<String, String> caseAttributes = new HashMap<>();
+
+        for (LogEventModel myEvent : events) {
+            String caseID = myEvent.getCaseID();
+
+            if (newTraceID == null || !newTraceID.equals(caseID)) {    // This could be new Trace
+
+                assignEventsToTrace(allEvents, xTrace);
+                assignMyCaseAttributes(caseAttributes, xTrace);
+                allEvents = new ArrayList<>();
+                caseAttributes = new HashMap<>();
+
+                xTrace = xFactory.createTrace();
+                concept.assignName(xTrace, caseID);
+                xLog.add(xTrace);
+                newTraceID = caseID;
+            }
+
+
+            setMyCaseAttributes(caseAttributes, myEvent.getCaseAttributes());
+
+            if (myEvent.getStartTimestamp() != null) {
+                xEvent = createEvent(myEvent, false);
+                allEvents.add(xEvent);
+            }
+
+            xEvent = createEvent(myEvent, true);
+            allEvents.add(xEvent);
+        }
+
+        // for last trace
+        assignEventsToTrace(allEvents, xTrace);
+        assignMyCaseAttributes(caseAttributes, xTrace);
+        return xLog;
+    }
+
+    private void assignEventsToTrace(List<XEvent> allEvents, XTrace xTrace){
+        if (allEvents != null && !allEvents.isEmpty()) {
+            Comparator<XEvent> compareTimestamp = (XEvent o1, XEvent o2) -> {
+                Date o1Date;
+                Date o2Date;
+                if (o1.getAttributes().get("time:timestamp") != null) {
+                    XAttribute o1da = o1.getAttributes().get("time:timestamp");
+                    if (((XAttributeTimestamp) o1da).getValue() != null) {
+                        o1Date = ((XAttributeTimestamp) o1da).getValue();
+                    } else {
+                        return -1;
+                    }
+                } else {
+                    return -1;
+                }
+
+                if (o2.getAttributes().get("time:timestamp") != null) {
+                    XAttribute o2da = o2.getAttributes().get("time:timestamp");
+                    if (((XAttributeTimestamp) o2da).getValue() != null) {
+                        o2Date = ((XAttributeTimestamp) o2da).getValue();
+                    } else {
+                        return 1;
+                    }
+                } else {
+                    return 1;
+                }
+
+                if (o1Date == null || o1Date.toString().isEmpty()) {
+                    return 1;
+                } else if (o2Date == null || o2Date.toString().isEmpty()) {
+                    return -1;
+                } else {
+                    return o1Date.compareTo(o2Date);
+                }
+            };
+
+            allEvents.sort(compareTimestamp);
+            xTrace.addAll(allEvents);
+        }
+    }
+
+    private void assignMyCaseAttributes(Map<String, String> caseAttributes, XTrace xTrace){
+        if(caseAttributes!= null && !caseAttributes.isEmpty()){
+            XAttribute attribute;
+            for (Map.Entry<String, String> entry : caseAttributes.entrySet()) {
+                if (entry.getValue() != null && entry.getValue().trim().length() != 0) {
+                    attribute = new XAttributeLiteralImpl(entry.getKey(), entry.getValue());
+                    xTrace.getAttributes().put(entry.getKey(), attribute);
+                }
+            }
+        }
+    }
+
+    private void setMyCaseAttributes(Map<String, String> caseAttributes, Map<String, String> eventCaseAttributes){
+        for (Map.Entry<String, String> entry : eventCaseAttributes.entrySet()) {
+            if (entry.getValue() != null && entry.getValue().trim().length() != 0 && !caseAttributes.containsKey(entry.getKey())) {
+                caseAttributes.put(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    private XEvent createEvent(LogEventModel myEvent, Boolean isEndTimestamp) {
+
+        XFactory xFactory = new XFactoryNaiveImpl();
+        XEvent xEvent = xFactory.createEvent();
+
+        XConceptExtension concept = XConceptExtension.instance();
+        concept.assignName(xEvent, myEvent.getActivity());
+
+        if (myEvent.getResource() != null) {
+            XOrganizationalExtension resource = XOrganizationalExtension.instance();
+            resource.assignResource(xEvent, myEvent.getResource());
+        }
+
+
+        XLifecycleExtension lifecycle = XLifecycleExtension.instance();
+        XTimeExtension timestamp = XTimeExtension.instance();
+        if (!isEndTimestamp) {
+            lifecycle.assignStandardTransition(xEvent, XLifecycleExtension.StandardModel.COMPLETE);
+            timestamp.assignTimestamp(xEvent, myEvent.getEndTimestamp());
+        } else {
+            lifecycle.assignStandardTransition(xEvent, XLifecycleExtension.StandardModel.START);
+            timestamp.assignTimestamp(xEvent, myEvent.getStartTimestamp());
+        }
+
+
+        XAttribute attribute;
+        if (myEvent.getOtherTimestamps() != null) {
+            Map<String, Timestamp> otherTimestamps = myEvent.getOtherTimestamps();
+            for (Map.Entry<String, Timestamp> entry : otherTimestamps.entrySet()) {
+                attribute = new XAttributeTimestampImpl(entry.getKey(), entry.getValue());
+                xEvent.getAttributes().put(entry.getKey(), attribute);
+            }
+        }
+
+        Map<String, String> eventAttributes = myEvent.getEventAttributes();
+        for (Map.Entry<String, String> entry : eventAttributes.entrySet()) {
+            if (entry.getValue() != null && entry.getValue().trim().length() != 0) {
+                attribute = new XAttributeLiteralImpl(entry.getKey(), entry.getValue());
+                xEvent.getAttributes().put(entry.getKey(), attribute);
+            }
+        }
+
+        return xEvent;
+    }
+}
--- Apromore-Custom-Plugins/CSVImporter-Logic/src/main/java/org/apromore/service/csvimporter/impl/LogModelImpl.java
+++ Apromore-Custom-Plugins/CSVImporter-Logic/src/main/java/org/apromore/service/csvimporter/impl/LogModelImpl.java
@@ -22,37 +22,21 @@
 
 package org.apromore.service.csvimporter.impl;
 
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
 import org.apromore.service.csvimporter.LogErrorReport;
 import org.apromore.service.csvimporter.LogModel;
-import org.deckfour.xes.extension.std.XConceptExtension;
-import org.deckfour.xes.extension.std.XLifecycleExtension;
-import org.deckfour.xes.extension.std.XOrganizationalExtension;
-import org.deckfour.xes.extension.std.XTimeExtension;
-import org.deckfour.xes.factory.XFactory;
-import org.deckfour.xes.factory.XFactoryNaiveImpl;
-import org.deckfour.xes.model.*;
-import org.deckfour.xes.model.impl.XAttributeLiteralImpl;
-import org.deckfour.xes.model.impl.XAttributeTimestampImpl;
+import org.deckfour.xes.model.XLog;
+
+import java.util.List;
 
 class LogModelImpl implements LogModel {
 
     private List<LogEventModel> rows;
     private List<LogErrorReport> logErrorReport;
 
-
     LogModelImpl(List<LogEventModel> rows, List<LogErrorReport> logErrorReportImpl) {
         this.rows        = rows;
         this.logErrorReport = logErrorReportImpl;
     }
-
 
     @Override
     public List<LogErrorReport> getLogErrorReport() { return logErrorReport; }
@@ -63,151 +47,5 @@ class LogModelImpl implements LogModel {
     }
 
     @Override
-    public XLog getXLog() {
-        return createXLog(rows);
-    }
-
-    private static XLog createXLog(List<LogEventModel> traces) {
-        if (traces == null) return null;
-
-        XFactory xFactory = new XFactoryNaiveImpl();
-        XLog xLog = xFactory.createLog();
-        XTrace xTrace = null;
-        XEvent xEvent = null;
-        List<XEvent> allEvents = new ArrayList<XEvent>();
-
-        XConceptExtension concept = XConceptExtension.instance();
-        XLifecycleExtension lifecycle = XLifecycleExtension.instance();
-        XTimeExtension timestamp = XTimeExtension.instance();
-        XOrganizationalExtension resource = XOrganizationalExtension.instance();
-
-        xLog.getExtensions().add(concept);
-        xLog.getExtensions().add(lifecycle);
-        xLog.getExtensions().add(timestamp);
-        xLog.getExtensions().add(resource);
-
-        lifecycle.assignModel(xLog, XLifecycleExtension.VALUE_MODEL_STANDARD);
-
-        String newTraceID = null;    // to keep track of traces, when a new trace is created we assign its value and add the respective events for the trace.
-
-        Comparator<XEvent> compareTimestamp = (XEvent o1, XEvent o2) -> {
-            Date o1Date;
-            Date o2Date;
-            if (o1.getAttributes().get("time:timestamp") != null) {
-                XAttribute o1da = o1.getAttributes().get("time:timestamp");
-                if (((XAttributeTimestamp) o1da).getValue() != null) {
-                    o1Date = ((XAttributeTimestamp) o1da).getValue();
-                } else {
-                    return -1;
-                }
-            } else {
-                return -1;
-            }
-
-            if (o2.getAttributes().get("time:timestamp") != null) {
-                XAttribute o2da = o2.getAttributes().get("time:timestamp");
-                if (((XAttributeTimestamp) o2da).getValue() != null) {
-                    o2Date = ((XAttributeTimestamp) o2da).getValue();
-                } else {
-                    return 1;
-                }
-            } else {
-                return 1;
-            }
-
-            if (o1Date == null || o1Date.toString().isEmpty()) {
-                return 1;
-            } else if (o2Date == null || o2Date.toString().isEmpty()) {
-                return -1;
-            } else {
-                return o1Date.compareTo(o2Date);
-            }
-        };
-
-        for (LogEventModel trace : traces) {
-            String caseID = trace.getCaseID();
-
-            if (newTraceID == null || !newTraceID.equals(caseID)) {    // This could be new trace
-
-                if (!allEvents.isEmpty()) {
-                    Collections.sort(allEvents, compareTimestamp);
-                    xTrace.addAll(allEvents);
-                    allEvents = new ArrayList<>();
-                }
-
-                xTrace = xFactory.createTrace();
-                concept.assignName(xTrace, caseID);
-
-                XAttribute attribute;
-                Map<String, String> myCaseAttributes = trace.getCaseAttributes();
-                for (Map.Entry<String, String> entry : myCaseAttributes.entrySet()) {
-                    if (entry.getValue() != null && entry.getValue().trim().length() != 0) {
-                        attribute = new XAttributeLiteralImpl(entry.getKey(), entry.getValue());
-                        xTrace.getAttributes().put(entry.getKey(), attribute);
-                    }
-                }
-                xLog.add(xTrace);
-                newTraceID = caseID;
-            }
-
-            if (trace.getStartTimestamp() != null) {
-                xEvent = createEvent(trace, xFactory, concept, lifecycle, timestamp, resource, false);
-                allEvents.add(xEvent);
-            }
-            if (timestamp != null) {
-                xEvent = createEvent(trace, xFactory, concept, lifecycle, timestamp, resource, true);
-            }
-            allEvents.add(xEvent);
-        }
-
-        if (!allEvents.isEmpty()) {
-            Collections.sort(allEvents, compareTimestamp);
-            xTrace.addAll(allEvents);
-        }
-
-
-        if (xEvent == null) {
-            return null;
-        } else {
-            return xLog;
-        }
-    }
-
-    private static XEvent createEvent(LogEventModel theTrace, XFactory xFactory, XConceptExtension concept, XLifecycleExtension lifecycle, XTimeExtension timestamp, XOrganizationalExtension resource, Boolean isEndTimestamp) {
-
-        XEvent xEvent = xFactory.createEvent();
-        concept.assignName(xEvent, theTrace.getActivity());
-
-        if (theTrace.getResource() != null) {
-            resource.assignResource(xEvent, theTrace.getResource());
-        }
-
-        XAttribute attribute;
-        if (theTrace.getOtherTimestamps() != null) {
-            Map<String, Timestamp> otherTimestamps = theTrace.getOtherTimestamps();
-            for (Map.Entry<String, Timestamp> entry : otherTimestamps.entrySet()) {
-                attribute = new XAttributeTimestampImpl(entry.getKey(), entry.getValue());
-                xEvent.getAttributes().put(entry.getKey(), attribute);
-            }
-        }
-        Map<String, String> others = theTrace.getEventAttributes();
-        for (Map.Entry<String, String> entry : others.entrySet()) {
-            if (entry.getValue() != null && entry.getValue().trim().length() != 0) {
-                attribute = new XAttributeLiteralImpl(entry.getKey(), entry.getValue());
-                xEvent.getAttributes().put(entry.getKey(), attribute);
-            }
-        }
-        if (theTrace.getEndTimestamp() != null) {
-            if (!isEndTimestamp) {
-                lifecycle.assignStandardTransition(xEvent, XLifecycleExtension.StandardModel.START);
-                timestamp.assignTimestamp(xEvent, theTrace.getStartTimestamp());
-
-            } else {
-                lifecycle.assignStandardTransition(xEvent, XLifecycleExtension.StandardModel.COMPLETE);
-                timestamp.assignTimestamp(xEvent, theTrace.getEndTimestamp());
-            }
-        }
-
-        return xEvent;
-    }
+    public XLog getXLog() { return new CreateXLog().generateXLog(rows); }
 }
