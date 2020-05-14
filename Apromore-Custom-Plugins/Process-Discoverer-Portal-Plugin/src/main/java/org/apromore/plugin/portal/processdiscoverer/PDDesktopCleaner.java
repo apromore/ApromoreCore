c2608279a7c305ece925adb849894a80f796eb3b
Bruce Nguyen <hoangnguyen7x@yahoo.com>
2020-05-15 07:23:07 +1000

    Must leave some leak in the UserSessionManager object to allow page refresh after timeout.

--- Apromore-Custom-Plugins/Process-Discoverer-Portal-Plugin/src/main/java/org/apromore/plugin/portal/processdiscoverer/PDDesktopCleaner.java
+++ Apromore-Custom-Plugins/Process-Discoverer-Portal-Plugin/src/main/java/org/apromore/plugin/portal/processdiscoverer/PDDesktopCleaner.java
@@ -1,7 +1,6 @@
 package org.apromore.plugin.portal.processdiscoverer;
 
 import org.apromore.plugin.portal.processdiscoverer.vis.ProcessVisualizer;
-import org.apromore.portal.common.UserSessionManager;
 import org.apromore.processdiscoverer.ProcessDiscoverer;
 import org.zkoss.zk.ui.Desktop;
 import org.zkoss.zk.ui.util.DesktopCleanup;
@@ -24,7 +23,8 @@ public class PDDesktopCleaner implements DesktopCleanup {
         ((ProcessVisualizer)desktop.getAttribute("processVisualizer")).cleanUp();
 
         // Clean up the Portal session as it doesn't control plugin session
-        UserSessionManager.removeEditSession(desktop.getAttribute("pluginSessionId").toString());
+        // TEMPORARY: remove this step to keep PD keeps working from refreshing the page after session timeout  
+        //UserSessionManager.removeEditSession(desktop.getAttribute("pluginSessionId").toString());
 
         System.out.println("PD cleanup is done for desktopID = " + desktop.getId());
     }
