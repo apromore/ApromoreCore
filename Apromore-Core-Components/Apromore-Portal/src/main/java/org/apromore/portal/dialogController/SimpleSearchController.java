4fb44319f4bdf94e1df5f0539f88dd0027306167
raboczi <raboczi@icloud.com>
2020-05-20 16:35:28 +1000

    Rewrote SimpleSearchController.addSearchHistory (AP-1593)

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/SimpleSearchController.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/SimpleSearchController.java
@@ -28,6 +28,7 @@ package org.apromore.portal.dialogController;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import javax.xml.bind.JAXBException;
 
@@ -131,46 +132,47 @@ public class SimpleSearchController extends BaseController {
         int nbAnswers = summaries.getSummary().size();
         mainC.displayMessage("Search returned " + nbAnswers + ((nbAnswers == 1) ? " result." : " results."));
         mainC.displaySearchResult(summaries);
-        addSearchHistory(query);
+        mainC.updateSearchHistory(addSearchHistory(mainC.getSearchHistory(), query));
     }
 
     /* Add a search History for this user for later use. */
-    private void addSearchHistory(String query) throws Exception {
-        List<SearchHistoriesType> searchHist = this.mainC.getSearchHistory();
-        if (searchHist != null) {
-            if (searchHist.size() == Constants.maxSearches) {
-                int min = -1, indMin = 0;
-                for (int i = 0; i < searchHist.size(); i++){
-                    SearchHistoriesType hist = searchHist.get(i);
-                    if (min == -1 || hist.getNum() != -1 && hist.getNum() < min) {
-                        min = hist.getNum();
-                        indMin = i;
-                    }
-                }
-                mainC.getSearchHistory().remove(indMin);
-            }
+    static List<SearchHistoriesType> addSearchHistory(List<SearchHistoriesType> searchHist, String query) throws Exception {
 
-            int i = 0;
-            while (i < searchHist.size() && searchHist.get(i).getSearch().compareTo(query) < 0) {
-                i++;
-            }
-            if (i == searchHist.size()) {
-                SearchHistoriesType h = new SearchHistoriesType();
-                h.setSearch(query);
-                h.setNum(-1);
-                searchHist.add(h);
-            } else {
-                if (searchHist.get(i).getSearch().compareTo(query) > 0) {
-                    SearchHistoriesType h = new SearchHistoriesType();
-                    h.setSearch(query);
-                    h.setNum(-1);
-                    searchHist.add(i, h);
-                }
+        // If the new query is already present, remove it
+        for (SearchHistoriesType sh: searchHist) {
+            if (sh.getSearch().equals(query)) {
+                searchHist.remove(sh);
+                break;
             }
         }
 
+        // Sort the elements by recency (i.e. higher number = more recent)
+        searchHist.sort(new Comparator<SearchHistoriesType>() {
+            public int compare(SearchHistoriesType lhs, SearchHistoriesType rhs) {
+                return lhs.getNum() - rhs.getNum();
+            }
+        });
+
+        // Append the new query
+        {
+            SearchHistoriesType sh = new SearchHistoriesType();
+            sh.setSearch(query);
+            searchHist.add(sh);
+        }
+
+        // Expire the oldest queries if there are too many
+        while (searchHist.size() > Constants.maxSearches) {
+            searchHist.remove(0);
+        }
+
+        // Update the recencies
+        int i = 0;
+        for (SearchHistoriesType sh: searchHist) {
+            sh.setNum(++i);
+        }
+
         // Send to the portal to store it.
-        mainC.updateSearchHistory(searchHist);
+        return searchHist;
     }
 
 }
---
+++ Apromore-Core-Components/Apromore-Portal/src/test/java/org/apromore/portal/dialogController/SimpleSearchControllerUnitTest.java
@@ -0,0 +1,60 @@
+/*-
+ * #%L
+ * This file is part of "Apromore Core".
+ * %%
+ * Copyright (C) 2020, Apromore Pty Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+package org.apromore.portal.dialogController;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apromore.model.SearchHistoriesType;
+import static org.junit.Assert.assertEquals;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/** Test suite for {@link ImportController}. */
+public class SimpleSearchControllerUnitTest {
+
+    // Test cases.
+
+    /** Test the {@link ImportController#importFile} method with no file importers and a CSV log. */
+    @Ignore
+    @Test
+    public void testAddSearchHistory() throws Exception {
+	
+        List<SearchHistoriesType> result = SimpleSearchController.addSearchHistory(new ArrayList<>(), "one");
+        result = SimpleSearchController.addSearchHistory(result, "two");
+        result = SimpleSearchController.addSearchHistory(result, "three");
+        result = SimpleSearchController.addSearchHistory(result, "four");
+        result = SimpleSearchController.addSearchHistory(result, "five");
+        result = SimpleSearchController.addSearchHistory(result, "six");
+        result = SimpleSearchController.addSearchHistory(result, "seven");
+        result = SimpleSearchController.addSearchHistory(result, "eight");
+        result = SimpleSearchController.addSearchHistory(result, "nine");
+        result = SimpleSearchController.addSearchHistory(result, "ten");
+        result = SimpleSearchController.addSearchHistory(result, "eleven");
+
+        assertEquals(10, result.size());
+        assertEquals("two", result.get(0).getSearch());
+        assertEquals(new Integer(1), result.get(0).getNum());
+        assertEquals("eleven", result.get(9).getSearch());
+        assertEquals(new Integer(10), result.get(9).getNum());
+    }
+}
