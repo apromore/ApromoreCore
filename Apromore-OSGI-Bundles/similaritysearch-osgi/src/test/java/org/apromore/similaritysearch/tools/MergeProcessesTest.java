ae582c55bc8bbf5daf79936b896d0b3525493ba9
Bruce Nguyen <hoangnguyen7x@yahoo.com>
2020-06-26 06:52:42 +1000

    Update

--- Apromore-OSGI-Bundles/similaritysearch-osgi/src/test/java/org/apromore/similaritysearch/tools/MergeProcessesTest.java
+++ Apromore-OSGI-Bundles/similaritysearch-osgi/src/test/java/org/apromore/similaritysearch/tools/MergeProcessesTest.java
@@ -23,7 +23,6 @@ package org.apromore.similaritysearch.tools;
 
 import static org.junit.Assert.fail;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -31,7 +30,6 @@ import java.util.List;
 import org.apromore.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
 import org.apromore.processmining.models.graphbased.directed.bpmn.BPMNDiagramSupport;
 import org.apromore.processmining.models.graphbased.directed.bpmn.BPMNNode;
-import org.apromore.processmining.plugins.bpmn.plugins.BpmnExportPlugin;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -119,8 +117,8 @@ public class MergeProcessesTest extends TestDataSetup {
             List<BPMNDiagram> diagrams = Arrays.asList(new BPMNDiagram[] {read_one_task_B(), read_two_tasks_sequence_AB()});
             BPMNDiagram merge = MergeProcesses.mergeProcesses(diagrams, false, "Greedy", 0.6, 0.6, 0.75, 1.0, 1.0, 1.0);
             
-            BpmnExportPlugin exporter = new BpmnExportPlugin();
-            exporter.export(merge, new File("testMergeProcesses_xor_branches.bpmn"));
+            //BpmnExportPlugin exporter = new BpmnExportPlugin();
+            //exporter.export(merge, new File("testMergeProcesses_xor_branches.bpmn"));
             
             Assert.assertEquals(6, merge.getNodes().size());
             Assert.assertEquals(6, merge.getEdges().size());
