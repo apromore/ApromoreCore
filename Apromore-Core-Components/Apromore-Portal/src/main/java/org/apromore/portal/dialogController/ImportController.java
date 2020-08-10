b1b9d3dae58de3d230a7614c75bbe6340e6d0cbf
frank-ma-leonardo <f.ma@leonardo.com.au>
2020-08-10 14:20:55 +1000

    AP-2006 Log cannot be imported

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/ImportController.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/ImportController.java
@@ -317,7 +317,7 @@ public class ImportController extends BaseController {
             fileName = StringUtil.contentDispositionFileName(contentDisposition);
         }
         if (StringUtil.isEmpty(fileName) && !StringUtil.isEmpty(contentDisposition)) {
-            fileName = contentDisposition.substring(contentDisposition.indexOf("filename=\"") + 10, contentDisposition.length() - 1);
+            fileName = contentDisposition.substring(contentDisposition.indexOf("filename=") + 9);
         }
         if (StringUtil.isEmpty(fileName)) {
             fileName = url.substring(url.lastIndexOf('/') + 1);
