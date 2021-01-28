40a7db0348e64deb551fbbcecf603c5f65616834
Mayank <mayanktomar20@gmail.com>
2021-01-29 03:46:26 +1100

    chore: added the impala properties and configurations in pom.

--- Apromore-Core-Components/Apromore-Manager/src/test/java/org/apromore/service/impl/AuthorizationServiceImplTest.java
+++ Apromore-Core-Components/Apromore-Manager/src/test/java/org/apromore/service/impl/AuthorizationServiceImplTest.java
@@ -50,7 +50,6 @@ public class AuthorizationServiceImplTest extends AbstractTest {
     private UsermetadataRepository usermetadataRepository;
     private LogRepository logRepository;
 
-
     private Group group1;
     private Group group2;
     private Group group3;
