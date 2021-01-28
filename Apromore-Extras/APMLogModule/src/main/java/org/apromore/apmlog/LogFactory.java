2d41bc8e6987361722efd9358eb9ff820ae13eae
Chii Chang <chiichang@users.noreply.github.com>
2021-01-28 17:18:52 +1100

    fixed the way to identifying the activity

--- Apromore-Extras/APMLogModule/src/main/java/org/apromore/apmlog/LogFactory.java
+++ Apromore-Extras/APMLogModule/src/main/java/org/apromore/apmlog/LogFactory.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -197,30 +197,35 @@ public class LogFactory {
                                 nEvent.getAttributes().get("lifecycle:transition").toString().toLowerCase() :
                                 "complete";
 
-                        long nT = getTimestamp(nEvent);
+                        if (foundStart && lifecycle.equals("start")) {
+                            // SKIP this event; it is the start of another activity with the same concept:name
+                        } else {
 
-                        if (lifecycle.equals("start")) {
-                            startTime = nT;
-                            startEvent = nEvent;
-                        }
+                            long nT = getTimestamp(nEvent);
 
-                        if (lifecycle.equals("complete") ||
-                                lifecycle.equals("manualskip") ||
-                                lifecycle.equals("autoskip")) {
+                            if (lifecycle.equals("start") && !foundStart) {
+                                startTime = nT;
+                                startEvent = nEvent;
+                            }
 
-                            eventIndexList.add(i);
                             if (nT > endTime) endTime = nT;
 
-                            break;
+                            if (lifecycle.equals("complete") ||
+                                    lifecycle.equals("manualskip") ||
+                                    lifecycle.equals("autoskip")) {
 
-                        } else {
-                            if (lifecycle.equals("start")){
-                                if (!foundStart) {
-                                    eventIndexList.add(i);
-                                    foundStart = true;
-                                }
-                            } else {
                                 eventIndexList.add(i);
+                                break;
+
+                            } else {
+                                if (lifecycle.equals("start")) {
+                                    if (!foundStart) {
+                                        eventIndexList.add(i);
+                                        foundStart = true;
+                                    }
+                                } else {
+                                    eventIndexList.add(i);
+                                }
                             }
                         }
                     }
