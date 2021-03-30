93d355a8a6d159255fd96c37b5ae61f290999301
Mayank <mayanktomar20@gmail.com>
2021-03-30 17:51:16 +1100

    chore: fixed comments from PR

--- Apromore-Commons/src/test/java/org/apromore/commons/utils/DelimiterUnitTest.java
+++ Apromore-Commons/src/test/java/org/apromore/commons/utils/DelimiterUnitTest.java
@@ -21,6 +21,7 @@
  */
 package org.apromore.commons.utils;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -31,18 +32,27 @@ import static org.junit.Assert.assertNotEquals;
 
 public class DelimiterUnitTest {
 
+    private List<String> rows;
+
+    /**
+     * Setup for tests.
+     */
+    @Before
+
+    public void setup() {
+        rows = new ArrayList<>();
+    }
+
     /**
      * This test driver tests the CleanDelimiter.
      */
     @Test
     public void testCleanDelimiter() {
         // Create a 1D list Strings [header, r1, r2, r3]
-        List<String> rows = new ArrayList<String>() {{
-            add("col1,col2,col3");
-            add("3,fgdfd,asdasdasasd");
-            add(",,jh");
-            add("546,kujgh,3");
-        }};
+        rows.add("col1,col2,col3");
+        rows.add("3,fgdfd,asdasdasasd");
+        rows.add(",,jh");
+        rows.add("546,kujgh,3");
         assertEquals(",", Delimiter.findDelimiter(rows));
         rows.add("dffd,fd,");
         assertEquals(",", Delimiter.findDelimiter(rows));
@@ -60,12 +70,10 @@ public class DelimiterUnitTest {
     @Test
     public void testCommaDelimiter() {
         // Create a 1D list Strings [header, r1, r2, r3]
-        List<String> rows = new ArrayList<String>() {{
-            add("col1,col2,col3");
-            add("3,fgdfd,asdasda;sasd");
-            add(",,j:h");
-            add("546,kujgh,3");
-        }};
+        rows.add("col1,col2,col3");
+        rows.add("3,fgdfd,asdasda;sasd");
+        rows.add(",,j:h");
+        rows.add("546,kujgh,3");
         assertEquals(",", Delimiter.findDelimiter(rows));
         rows.add("dffd,fd,");
         assertEquals(",", Delimiter.findDelimiter(rows));
@@ -83,12 +91,10 @@ public class DelimiterUnitTest {
     @Test
     public void testSemiColonDelimiter() {
         // Create a 1D list Strings [header, r1, r2, r3]
-        List<String> rows = new ArrayList<String>() {{
-            add("col1;col2;col3");
-            add("3;fgdfd,asdasda;sasd");
-            add(";;j:h");
-            add("546;kujgh;ss");
-        }};
+        rows.add("col1;col2;col3");
+        rows.add("3;fgdfd,asdasda;sasd");
+        rows.add(";;j:h");
+        rows.add("546;kujgh;ss");
         assertEquals(";", Delimiter.findDelimiter(rows));
         rows.add("dffd;fd;");
         assertEquals(";", Delimiter.findDelimiter(rows));
@@ -106,12 +112,10 @@ public class DelimiterUnitTest {
     @Test
     public void testColonDelimiter() {
         // Create a 1D list Strings [header, r1, r2, r3]
-        List<String> rows = new ArrayList<String>() {{
-            add("col1:col2:col3");
-            add("3:fgdfd,asdasda:sasd");
-            add("::j;h");
-            add("546:kujgh:ss");
-        }};
+        rows.add("col1:col2:col3");
+        rows.add("3:fgdfd,asdasda:sasd");
+        rows.add("::j;h");
+        rows.add("546:kujgh:ss");
         assertEquals(":", Delimiter.findDelimiter(rows));
         rows.add("dffd:fd:");
         assertEquals(":", Delimiter.findDelimiter(rows));
@@ -129,12 +133,10 @@ public class DelimiterUnitTest {
     @Test
     public void testTabDelimiter() {
         // Create a 1D list Strings [header, r1, r2, r3]
-        List<String> rows = new ArrayList<String>() {{
-            add("col1\tcol2\tcol3");
-            add("3\tfgdfd,asdasda\tsasd");
-            add("\t\tj;h");
-            add("546\tkujgh\tss");
-        }};
+        rows.add("col1\tcol2\tcol3");
+        rows.add("3\tfgdfd,asdasda\tsasd");
+        rows.add("\t\tj;h");
+        rows.add("546\tkujgh\tss");
         assertEquals("\t", Delimiter.findDelimiter(rows));
         rows.add("dffd\tfd\t");
         assertEquals("\t", Delimiter.findDelimiter(rows));
@@ -152,12 +154,10 @@ public class DelimiterUnitTest {
     @Test
     public void testSpaceDelimiter() {
         // Create a 1D list Strings [header, r1, r2, r3]
-        List<String> rows = new ArrayList<String>() {{
-            add("col1 col2 col3");
-            add("3 fgdfd,asdasda sasd");
-            add("  j;h");
-            add("546 kujgh ss");
-        }};
+        rows.add("col1 col2 col3");
+        rows.add("3 fgdfd,asdasda sasd");
+        rows.add("  j;h");
+        rows.add("546 kujgh ss");
         assertEquals(" ", Delimiter.findDelimiter(rows));
         rows.add("dffd fd ");
         assertEquals(" ", Delimiter.findDelimiter(rows));
@@ -175,12 +175,10 @@ public class DelimiterUnitTest {
     @Test
     public void testPipeDelimiter() {
         // Create a 1D list Strings [header, r1, r2, r3]
-        List<String> rows = new ArrayList<String>() {{
-            add("col1|col2|col3");
-            add("3|fgdfd,asdasda|sasd");
-            add("|j|h");
-            add("546|kujgh|ss");
-        }};
+        rows.add("col1|col2|col3");
+        rows.add("3|fgdfd,asdasda|sasd");
+        rows.add("|j|h");
+        rows.add("546|kujgh|ss");
         assertEquals("\\|", Delimiter.findDelimiter(rows));
         rows.add("dffd|fd|");
         assertEquals("\\|", Delimiter.findDelimiter(rows));
@@ -209,18 +207,16 @@ public class DelimiterUnitTest {
      */
     @Test
     public void testRealInput() {
-        List<String> rows = new ArrayList<String>() {{
-            add("OID|PID|userID|time_stamp|quantity|payment_type|delivery_mode");
-            add("1|316717|5276|07/08/2010 10:33|10|paypal|normal");
-            add("2|359728|2144|07/08/2010 10:33|74|cash|pickup");
-            add("3|404571|4531|07/08/2010 10:33|12|mastercard|pickup");
-            add("4|34122|1598|07/08/2010 10:33|43|mastercard|first_class");
-            add("5|577352|2614|07/08/2010 10:33|58|visa|second_class");
-            add("6|457481|3030|07/08/2010 10:33|34|mastercard|pickup");
-            add("7|618694|6635|07/08/2010 10:33|70|paypal|pickup");
-            add("8|896132|2828|07/08/2010 10:33|59|mastercard|second_class");
-            add("9|732581|7228|07/08/2010 10:33|21|paypal|second_class");
-        }};
+        rows.add("OID|PID|userID|time_stamp|quantity|payment_type|delivery_mode");
+        rows.add("1|316717|5276|07/08/2010 10:33|10|paypal|normal");
+        rows.add("2|359728|2144|07/08/2010 10:33|74|cash|pickup");
+        rows.add("3|404571|4531|07/08/2010 10:33|12|mastercard|pickup");
+        rows.add("4|34122|1598|07/08/2010 10:33|43|mastercard|first_class");
+        rows.add("5|577352|2614|07/08/2010 10:33|58|visa|second_class");
+        rows.add("6|457481|3030|07/08/2010 10:33|34|mastercard|pickup");
+        rows.add("7|618694|6635|07/08/2010 10:33|70|paypal|pickup");
+        rows.add("8|896132|2828|07/08/2010 10:33|59|mastercard|second_class");
+        rows.add("9|732581|7228|07/08/2010 10:33|21|paypal|second_class");
         assertEquals("\\|", Delimiter.findDelimiter(rows));
     }
 }
