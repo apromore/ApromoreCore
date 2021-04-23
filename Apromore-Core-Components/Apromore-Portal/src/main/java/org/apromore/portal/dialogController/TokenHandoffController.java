01431e369c2b09d468ce268a59b1c5ddfc487314
Mic G <fs.micdev@gmail.com>
2021-04-23 19:00:44 +1000

    WIP (significant changes) for JWT hand-off - session setting authenticated user.

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/TokenHandoffController.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/TokenHandoffController.java
@@ -21,28 +21,63 @@
  */
 package org.apromore.portal.dialogController;
 
+import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
 import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apromore.dao.model.Membership;
+import org.apromore.dao.model.Role;
+import org.apromore.dao.model.User;
 import org.apromore.manager.client.ManagerService;
+import org.apromore.mapper.SearchHistoryMapper;
 import org.apromore.portal.common.Constants;
 import org.apromore.portal.common.UserSessionManager;
+import org.apromore.portal.model.RoleType;
 import org.apromore.portal.model.UserType;
 import org.apromore.portal.security.helper.SecuritySsoHelper;
-import org.apromore.security.JwtUtils;
+import org.apromore.security.util.SecurityUtil;
+import org.apromore.service.SecurityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.zkoss.spring.SpringUtil;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zul.Window;
 
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
 
 public class TokenHandoffController extends SelectorComposer<Window> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TokenHandoffController.class);
 
+    private static final Duration WEBAPP_SSO_SESSION_TIMEOUT = Duration.ofMinutes(30); // Duration.ofHours(2L);
+
     private ManagerService managerService;
+    private SecurityService securityService;
+
+    private static final String JWT_KEY_SUBJECT_USERNAME = "sub";
+    private static final String JWT_KEY_ISSUED_AT = "iat";
+    private static final String JWT_EXPIRY_TIME = "exp";
+    private static final String STR_JWT_KEY_ISSUED_AT = "striat";
+    private static final String STR_JWT_EXPIRY_TIME = "strexp";
+    private static final String JWT_KC_USER_ID = "kcuserid";
+
+    private static final String JWT_KEY_SUBJECT_EMAIL = "email";
+    private static final String JWT_KEY_GIVEN_NAME = "givenName";
+    private static final String JWT_KEY_FAMILY_NAME = "familyName";
 
     @Override
     public void doAfterCompose(final Window window) {
@@ -50,16 +85,46 @@ public class TokenHandoffController extends SelectorComposer<Window> {
         LOGGER.info(">>>>> JWT token handoff <<<<<");
 
         managerService = (ManagerService) SpringUtil.getBean(Constants.MANAGER_SERVICE);
+        securityService = (SecurityService) SpringUtil.getBean(Constants.SECURITY_SERVICE);
         LOGGER.info("Manager service: " + managerService);
+        LOGGER.info("Security service: " + securityService);
 
         try {
             final UserType userType = userFromJwt();
 
             if (userType != null) {
                 UserSessionManager.setCurrentUser(userType);
-                LOGGER.info("Current user: " + UserSessionManager.getCurrentUser());
 
-                Executions.sendRedirect("/index.zul");
+                final UserType currentUserType = UserSessionManager.getCurrentUser();
+                LOGGER.info("Current session logged-in currentUserType: " + currentUserType);
+                LOGGER.info("Current session logged-in username: " + currentUserType.getUsername());
+
+                // UserSessionManager.initializeUser(managerService, configBean, userType, user);
+
+                final SecurityContext springSecurityContext = SecurityContextHolder.getContext();
+                LOGGER.info("springSecurityContext {}", springSecurityContext);
+
+                if (springSecurityContext != null) {
+                    final Authentication springAuthObj = springSecurityContext.getAuthentication();
+                    LOGGER.info("springAuthObj {}", springAuthObj);
+
+                    final User user = convertFromUserType(currentUserType, securityService);
+                    LOGGER.info("user {}", user);
+
+                    final List<GrantedAuthority> authorities = getAuthorities(user.getRoles());
+
+                    final PreAuthenticatedAuthenticationToken preAuthenticatedAuthenticationToken =
+                            new PreAuthenticatedAuthenticationToken(user.getUsername(),
+                                    "", authorities);
+                    LOGGER.info("preAuthenticatedAuthenticationToken {}", preAuthenticatedAuthenticationToken);
+
+                    springSecurityContext.setAuthentication(preAuthenticatedAuthenticationToken);
+                } else {
+                    LOGGER.info("Spring SecurityContext is null!!");
+                }
+
+
+                Executions.sendRedirect("http://localhost:8181/index.zul");
             } else {
                 Executions.sendRedirect("/login.zul?error=2");
             }
@@ -71,6 +136,16 @@ public class TokenHandoffController extends SelectorComposer<Window> {
         }
     }
 
+    public List<GrantedAuthority> getAuthorities(Set<Role> access) {
+        final List<GrantedAuthority> authList = new ArrayList<>();
+
+        for (Role role : access) {
+            authList.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return authList;
+    }
+
     /**
      * Return authenticated user if JWT token is not expired, otherwise <code>null</code>.
      *
@@ -80,16 +155,32 @@ public class TokenHandoffController extends SelectorComposer<Window> {
         final String appAuthHeader = SecuritySsoHelper.getAppAuthHeader();
         final String signedAppAuthHeader = SecuritySsoHelper.getSignedAppAuthHeader();
 
+        final SecurityContextHolderAwareRequestWrapper securityContextHolderAwareRequestWrapper =
+                SecuritySsoHelper.getSecurityContextHolderAwareRequestWrapper();
+        final Enumeration<String> headerNames =
+                securityContextHolderAwareRequestWrapper.getHeaderNames();
+
+        LOGGER.info("\n\nALL HEADERS via: " +
+                "SecuritySsoHelper.getSecurityContextHolderAwareRequestWrapper().getHeaderNames(): {}",
+            headerNames);
+        final String strJwtKeyIssuedAt =
+                securityContextHolderAwareRequestWrapper.getHeader(STR_JWT_KEY_ISSUED_AT);
+        final String strJwtKeyExpiryAt =
+                securityContextHolderAwareRequestWrapper.getHeader(STR_JWT_EXPIRY_TIME);
+        LOGGER.info("\nstrJwtKeyIssuedAt {}", strJwtKeyIssuedAt);
+        LOGGER.info("\nstrJwtKeyExpiryAt {}", strJwtKeyExpiryAt);
+
         try {
-            final JWTClaimsSet jwtClaimsSet = JwtUtils.getClaimsSetFromJWT(appAuthHeader);
+            final JWTClaimsSet jwtClaimsSet = getClaimsSetFromJWT(appAuthHeader);
+            LOGGER.info("jwtClaimsSet {}", jwtClaimsSet);
                     // JWTClaimsSet.parse(appAuthHeader);
 
-            final Object issuedAtObj = jwtClaimsSet.getClaim(JwtUtils.JWT_KEY_ISSUED_AT);
-            LOGGER.info("issuedAtObj {}", issuedAtObj);
-            final Object expiryAtObj = jwtClaimsSet.getClaim(JwtUtils.JWT_EXPIRY_TIME);
-            LOGGER.info("expiryAtObj {}", expiryAtObj);
+            final String issuedAtStr = (String)jwtClaimsSet.getStringClaim(STR_JWT_KEY_ISSUED_AT);
+            LOGGER.info("issuedAtStr {}", issuedAtStr);
+            final String expiryAtStr = (String)jwtClaimsSet.getStringClaim(STR_JWT_EXPIRY_TIME);
+            LOGGER.info("expiryAtStr {}", expiryAtStr);
 
-            final boolean jwtExpired = JwtUtils.isJwtExpired(jwtClaimsSet);
+            final boolean jwtExpired = isJwtExpired(jwtClaimsSet, issuedAtStr, expiryAtStr);
 
             if (!jwtExpired) {
                 final UserType authUserType = buildAuthenticatedUserFromJwt(jwtClaimsSet);
@@ -106,10 +197,98 @@ public class TokenHandoffController extends SelectorComposer<Window> {
     }
 
     private UserType buildAuthenticatedUserFromJwt(final JWTClaimsSet jwtClaimsSet) {
-        final String username = (String)jwtClaimsSet.getClaim(JwtUtils.JWT_KEY_SUBJECT_USERNAME);
+        final String username = (String)jwtClaimsSet.getClaim(JWT_KEY_SUBJECT_USERNAME);
 
         final UserType userType = managerService.readUserByUsername(username);
 
         return userType;
+    }
+
+
+    private JWTClaimsSet getClaimsSetFromJWT(final String jwtStr) throws Exception {
+        final JWT jwtParsed = JWTParser.parse(jwtStr);
+        final JWTClaimsSet jwtClaimsSet = jwtParsed.getJWTClaimsSet();
+
+        return jwtClaimsSet;
+    }
+
+    private boolean isJwtExpired(final JWTClaimsSet jwtClaimsSet,
+                                 final String issuedAtStr,
+                                 final String expiredAtStr) {
+        long jwtExpiryEpoch;
+        if (expiredAtStr != null) {
+            jwtExpiryEpoch = Long.valueOf(expiredAtStr.substring(3));
+            LOGGER.info("jwtExpiryEpoch {}", jwtExpiryEpoch);
+        } else { // Calculate relative
+            final Long jwtIssuedAt = Long.valueOf(issuedAtStr.substring(3));
+
+            jwtExpiryEpoch = calculateExpiryTime(jwtIssuedAt, WEBAPP_SSO_SESSION_TIMEOUT);
+            LOGGER.info("jwtExpiryEpoch {}", jwtExpiryEpoch);
+        }
+
+        final Long nowEpoch = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond();
+
+        final boolean jwtExpired = (nowEpoch > jwtExpiryEpoch);
+        LOGGER.info(">>>>> >>>>> >>>>> jwtExpired? {}", jwtExpired);
+
+        return jwtExpired;
+    }
+
+    private long calculateExpiryTime(final long createdAtEpoch, final Duration sessionDuration) {
+        return createdAtEpoch + sessionDuration.toMillis();
+    }
+
+    public User convertFromUserType(final UserType userType, final SecurityService securityService) {
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = null;
+        if (userType.getLastActivityDate() != null && !userType.getLastActivityDate().equals("")) {
+            try {
+                date = formatter.parse(userType.getLastActivityDate());
+            } catch (ParseException ex) {
+                LOGGER.error("Last Activity Date: " + userType.getLastActivityDate() + " could not be parsed.");
+            }
+        }
+
+        User user = new User();
+        user.setLastName(userType.getLastName());
+        user.setFirstName(userType.getFirstName());
+        user.setUsername(userType.getUsername());
+        user.setOrganization(userType.getOrganization());
+        user.setRole(userType.getRole());
+        user.setCountry(userType.getCountry());
+        user.setPhone(userType.getPhone());
+        user.setSubscription(userType.getSubscription());
+        user.setRowGuid(userType.getId());
+        if (date != null){
+            user.setLastActivityDate(date);
+        }
+        if (user.getSearchHistories() != null) {
+            user.setSearchHistories(SearchHistoryMapper.convertFromSearchHistoriesType(userType.getSearchHistories()));
+        }
+
+        Membership membership = new Membership();
+        if (userType.getMembership() != null) {
+            membership.setSalt("username");
+            membership.setDateCreated(new Date());
+            membership.setEmail(userType.getMembership().getEmail());
+            if (userType.getMembership().getPassword() != null) {
+                membership.setPassword(SecurityUtil.hashPassword(userType.getMembership().getPassword()));
+            } else {
+                membership.setPassword("");
+            }
+            membership.setQuestion(userType.getMembership().getPasswordQuestion());
+            membership.setAnswer(userType.getMembership().getPasswordAnswer());
+            membership.setFailedPasswordAttempts(0);
+            membership.setFailedAnswerAttempts(0);
+            membership.setUser(user);
+
+            user.setMembership(membership);
+        }
+
+        for (RoleType roleType : userType.getRoles()){
+            user.getRoles().add(securityService.findRoleByName(roleType.getName()));
+        }
+
+        return user;
     }
 }
