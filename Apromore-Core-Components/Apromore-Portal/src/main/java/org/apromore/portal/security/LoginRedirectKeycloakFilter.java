f3afee37ac5ed124f60bd0f8e2f004db61bd9630
Mic G <fs.micdev@gmail.com>
2021-04-28 11:18:51 +1000

    Cleanup.

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/security/LoginRedirectKeycloakFilter.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/security/LoginRedirectKeycloakFilter.java
@@ -52,7 +52,6 @@ public class LoginRedirectKeycloakFilter extends GenericFilterBean {
 
     public void setFullProtocolHostPortUrl(final String fullProtocolHostPortUrl) {
         s_fullConfigurableReturnPath = fullProtocolHostPortUrl;
-        s_fullConfigurableReturnPath = "https://sandbox-u21.apromore.org:443/";
 
         LOGGER.info("Set fullProtocolHostPortUrl to {}", s_fullConfigurableReturnPath);
     }
