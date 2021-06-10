559a4f930e7fbd7f5a3dd63c6f05b07cc85f69e4
Mic G <fs.micdev@gmail.com>
2021-06-10 11:09:31 +1000

    Base64 fix (instead of props file).

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/security/LoginRedirectKeycloakFilter.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/security/LoginRedirectKeycloakFilter.java
@@ -48,10 +48,12 @@ public class LoginRedirectKeycloakFilter extends GenericFilterBean {
 
     private static String keycloakLoginFormUrl;
 
-    private String s_fullConfigurableReturnPath = "http://localhost:8181/";
+    private String s_fullConfigurableReturnPath =
+            new String(
+                    Base64.getEncoder().encode("http://localhost:8181/".getBytes()));
     private boolean s_utiliseKeycloakSso = false;
 
-    public void setFullProtocolHostPortUrl(final String fullProtocolHostPortUrl) {
+    public void setFullProtocolHostPortUrql(final String fullProtocolHostPortUrl) {
         s_fullConfigurableReturnPath = new String(
                 Base64.getEncoder().encode(fullProtocolHostPortUrl.getBytes()));
 
