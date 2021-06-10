e6493cda8ce9ea0296211e185ad24764518a3740
Mic G <fs.micdev@gmail.com>
2021-06-10 11:15:36 +1000

    Fix typo.

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/security/LoginRedirectKeycloakFilter.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/security/LoginRedirectKeycloakFilter.java
@@ -53,7 +53,7 @@ public class LoginRedirectKeycloakFilter extends GenericFilterBean {
                     Base64.getEncoder().encode("http://localhost:8181/".getBytes()));
     private boolean s_utiliseKeycloakSso = false;
 
-    public void setFullProtocolHostPortUrql(final String fullProtocolHostPortUrl) {
+    public void setFullProtocolHostPortUrl(final String fullProtocolHostPortUrl) {
         s_fullConfigurableReturnPath = new String(
                 Base64.getEncoder().encode(fullProtocolHostPortUrl.getBytes()));
 
