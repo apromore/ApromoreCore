0896ee6c5723b947fd688d945b7d8b83ab9f2500
Nolan Tellis <nolan.tellis@apromore.com>
2021-06-24 15:37:23 +1000

    Added calender helper methos

--- Apromore-Core-Components/Apromore-Manager/src/main/java/org/apromore/service/EventLogService.java
+++ Apromore-Core-Components/Apromore-Manager/src/main/java/org/apromore/service/EventLogService.java
@@ -24,8 +24,11 @@
 
 package org.apromore.service;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
 import org.apromore.apmlog.APMLog;
-import org.apromore.dao.model.CustomCalendar;
+import org.apromore.calendar.model.CalendarModel;
 import org.apromore.dao.model.Log;
 import org.apromore.dao.model.User;
 import org.apromore.exception.ImportException;
@@ -34,104 +37,99 @@ import org.apromore.portal.model.ExportLogResultType;
 import org.apromore.portal.model.SummariesType;
 import org.deckfour.xes.model.XLog;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.List;
-
 /**
- * Interface for the Process Service. Defines all the methods that will do the majority of the work for
- * the Apromore application.
+ * Interface for the Process Service. Defines all the methods that will do the majority of the work
+ * for the Apromore application.
  *
  * @author <a href="mailto:cam.james@gmail.com">Cameron James</a>
  */
 public interface EventLogService {
 
-    /**
-     * Loads all the process Summaries. It will either get all or use the keywords parameter
-     * to load a subset of the processes.
-     *
-     * @param folderId         the folder we are currently in.
-     * @param searchExpression the search expression to limit the search.
-     * @return The ProcessSummariesType used for Webservices.
-     */
-    SummariesType readLogSummaries(final Integer folderId, final String searchExpression);
+  /**
+   * Loads all the process Summaries. It will either get all or use the keywords parameter to load a
+   * subset of the processes.
+   *
+   * @param folderId the folder we are currently in.
+   * @param searchExpression the search expression to limit the search.
+   * @return The ProcessSummariesType used for Webservices.
+   */
+  SummariesType readLogSummaries(final Integer folderId, final String searchExpression);
 
-    /**
-     * Import a Process.
-     *
-     * @param username    The user doing the importing.
-     * @param folderId    The folder we are saving the process in.
-     * @param logName     the name of the process being imported.
-     * @param domain      the domain of the model
-     * @param created     the time created
-     * @param publicModel is this a public model?
-     * @return the processSummaryType
-     * @throws ImportException if the import process failed for any reason.
-     */
-    Log importLog(String username, Integer folderId, String logName, InputStream log, String extension,
-                  String domain, String created, boolean publicModel)
-            throws Exception;
-
-
-    Log importLog(Integer folderId, String logName, String domain, String created, boolean publicModel, User user,
-                  XLog xLog);
-
-    /**
-     * @param username a username
-     * @param logId    identifier for a log
-     * @return whether the <var>user</var> should be allowed to update the log identified by <var>logId</var>
-     */
-    boolean canUserWriteLog(String username, Integer logId) throws UserNotFoundException;
-
-    ExportLogResultType exportLog(Integer logId)
-            throws Exception;
-
-    void cloneLog(String username, Integer folderId, String logName, Integer sourcelogId,
-                  String domain, String created, boolean publicModel)
-            throws Exception;
-
-    XLog getXLog(Integer logId);
-
-    XLog getXLog(Integer logId, String factoryName);
-
-    void deleteLogs(List<Log> logs, User user) throws Exception;
-
-    void exportToStream(OutputStream outputStream, XLog log) throws Exception;
-
-    void updateLogMetaData(Integer logId, String logName, boolean isPublic);
-
-    boolean isPublicLog(Integer logId);
-
-    /**
-     * Get aggregated log.
-     *
-     * @param logId logId of Log
-     * @return The aggregated log placed into the cache, or generated on the fly if not found or expired
-     */
-    APMLog getAggregatedLog(Integer logId);
+  /**
+   * Import a Process.
+   *
+   * @param username The user doing the importing.
+   * @param folderId The folder we are saving the process in.
+   * @param logName the name of the process being imported.
+   * @param domain the domain of the model
+   * @param created the time created
+   * @param publicModel is this a public model?
+   * @return the processSummaryType
+   * @throws ImportException if the import process failed for any reason.
+   */
+  Log importLog(String username, Integer folderId, String logName, InputStream log,
+      String extension, String domain, String created, boolean publicModel) throws Exception;
 
 
-    /**
-     * Update specified CustomCalendar that linked to specified Log
-     *
-     * @param logId logId of Log
-     * @param calenderId calenderId of CustomCalendar
-     */
-    void updateCalendarForLog(Integer logId, Long calenderId);
+  Log importLog(Integer folderId, String logName, String domain, String created,
+      boolean publicModel, User user, XLog xLog);
 
-    /**
-     * Get specified Log by logId
-     *
-     * @param logId logId of Log
-     * @return CalendarId
-     */
-    Long getCalendarIdFromLog(Integer logId);
+  /**
+   * @param username a username
+   * @param logId identifier for a log
+   * @return whether the <var>user</var> should be allowed to update the log identified by
+   *         <var>logId</var>
+   */
+  boolean canUserWriteLog(String username, Integer logId) throws UserNotFoundException;
 
-    /**
-     * Get the CustomCalendar that linked to specified Log
-     *
-     * @param logId logId of Log
-     * @return CustomCalendar
-     */
-    CustomCalendar getCalendarFromLog(Integer logId);
+  ExportLogResultType exportLog(Integer logId) throws Exception;
+
+  void cloneLog(String username, Integer folderId, String logName, Integer sourcelogId,
+      String domain, String created, boolean publicModel) throws Exception;
+
+  XLog getXLog(Integer logId);
+
+  XLog getXLog(Integer logId, String factoryName);
+
+  void deleteLogs(List<Log> logs, User user) throws Exception;
+
+  void exportToStream(OutputStream outputStream, XLog log) throws Exception;
+
+  void updateLogMetaData(Integer logId, String logName, boolean isPublic);
+
+  boolean isPublicLog(Integer logId);
+
+  /**
+   * Get aggregated log.
+   *
+   * @param logId logId of Log
+   * @return The aggregated log placed into the cache, or generated on the fly if not found or
+   *         expired
+   */
+  APMLog getAggregatedLog(Integer logId);
+
+
+  /**
+   * Update specified CustomCalendar that linked to specified Log
+   *
+   * @param logId logId of Log
+   * @param calenderId calenderId of CustomCalendar
+   */
+  void updateCalendarForLog(Integer logId, Long calenderId);
+
+  /**
+   * Get specified Log by logId
+   *
+   * @param logId logId of Log
+   * @return CalendarId
+   */
+  Long getCalendarIdFromLog(Integer logId);
+
+  /**
+   * Get the CustomCalendar that linked to specified Log
+   *
+   * @param logId logId of Log
+   * @return CustomCalendar
+   */
+  CalendarModel getCalendarFromLog(Integer logId);
 }
--- Apromore-Core-Components/Apromore-Manager/src/test/java/org/apromore/test/service/impl/EventLogServiceImplTest.java
+++ Apromore-Core-Components/Apromore-Manager/src/test/java/org/apromore/test/service/impl/EventLogServiceImplTest.java
@@ -23,10 +23,7 @@
 package org.apromore.test.service.impl;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.powermock.api.easymock.PowerMock.createMock;
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -38,25 +35,19 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Set;
 import java.util.stream.Stream;
-
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
+import org.apromore.calendar.service.CustomCalendarService;
 import org.apromore.common.ConfigBean;
 import org.apromore.dao.CustomCalendarRepository;
 import org.apromore.dao.FolderRepository;
 import org.apromore.dao.GroupLogRepository;
 import org.apromore.dao.GroupRepository;
-import org.apromore.dao.GroupUsermetadataRepository;
 import org.apromore.dao.LogRepository;
 import org.apromore.dao.StorageRepository;
-import org.apromore.dao.UsermetadataLogRepository;
 import org.apromore.dao.UsermetadataRepository;
-import org.apromore.dao.UsermetadataTypeRepository;
 import org.apromore.service.EventLogFileService;
-import org.apromore.service.UserMetadataService;
 import org.apromore.service.UserService;
-import org.apromore.service.helper.UserInterfaceHelper;
 import org.apromore.service.impl.EventLogServiceImpl;
 import org.apromore.service.impl.TemporaryCacheService;
 import org.apromore.storage.StorageClient;
@@ -64,8 +55,6 @@ import org.apromore.storage.factory.StorageManagementFactory;
 import org.deckfour.xes.factory.XFactory;
 import org.deckfour.xes.factory.XFactoryRegistry;
 import org.deckfour.xes.in.XesXmlParser;
-import org.deckfour.xes.info.XLogInfo;
-import org.deckfour.xes.info.XLogInfoFactory;
 import org.deckfour.xes.model.XAttributable;
 import org.deckfour.xes.model.XAttribute;
 import org.deckfour.xes.model.XAttributeMap;
@@ -81,181 +70,183 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.google.common.collect.Sets;
 
 public class EventLogServiceImplTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EventLogServiceImplTest.class);
-    // inject EntityManager for simple test
-    private static EntityManagerFactory emf;
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
-    private LogRepository logRepository;
-    private GroupRepository groupRepository;
-    private GroupLogRepository groupLogRepository;
-    private FolderRepository folderRepo;
-    private UserService userSrv;
-    private EventLogServiceImpl eventLogService;
-    private UsermetadataRepository userMetadataRepo;
-    private TemporaryCacheService temporaryCacheService;
-    private StorageManagementFactory<StorageClient> storageFactory;
-    private EventLogFileService logFileService;
-    private StorageRepository storageRepository;
-    private CustomCalendarRepository calendarRepository;
+  private static final Logger LOGGER = LoggerFactory.getLogger(EventLogServiceImplTest.class);
+  // inject EntityManager for simple test
+  private static EntityManagerFactory emf;
+  @Rule
+  public ExpectedException exception = ExpectedException.none();
+  private LogRepository logRepository;
+  private GroupRepository groupRepository;
+  private GroupLogRepository groupLogRepository;
+  private FolderRepository folderRepo;
+  private UserService userSrv;
+  private EventLogServiceImpl eventLogService;
+  private UsermetadataRepository userMetadataRepo;
+  private TemporaryCacheService temporaryCacheService;
+  private StorageManagementFactory<StorageClient> storageFactory;
+  private EventLogFileService logFileService;
+  private StorageRepository storageRepository;
+  private CustomCalendarRepository calendarRepository;
+  private CustomCalendarService calendarService;
 
 
-    private static void walkLog(XLog log) {
-        walkAttributes(log);
-        for (XTrace trace : log) {
-            walkTrace(trace);
-        }
+  private static void walkLog(XLog log) {
+    walkAttributes(log);
+    for (XTrace trace : log) {
+      walkTrace(trace);
+    }
+  }
+
+  private static void walkTrace(XTrace trace) {
+    walkAttributes(trace);
+    for (XEvent event : trace) {
+      walkAttributes(event);
+    }
+  }
+
+  private static void walkAttributes(XAttributable attributable) {
+    XAttributeMap attributeMap = attributable.getAttributes();
+    for (XAttribute attribute : attributeMap.values()) {
+      String key = attribute.getKey();
+      String value = attribute.toString();
+      key.trim();
+      value.trim();
+      walkAttributes(attribute);
+    }
+  }
+
+  public EntityManagerFactory getEntityManagerFactory() {
+    if (emf == null) {
+      emf = Persistence.createEntityManagerFactory("TESTApromore");
+    }
+    return emf;
+  }
+
+  @Before
+  public final void setUp() {
+    logRepository = createMock(LogRepository.class);
+    groupRepository = createMock(GroupRepository.class);
+    groupLogRepository = createMock(GroupLogRepository.class);
+    folderRepo = createMock(FolderRepository.class);
+    userSrv = createMock(UserService.class);
+    userMetadataRepo = createMock(UsermetadataRepository.class);
+    temporaryCacheService = createMock(TemporaryCacheService.class);
+    storageFactory = createMock(StorageManagementFactory.class);
+    logFileService = createMock(EventLogFileService.class);
+    storageRepository = createMock(StorageRepository.class);
+    calendarRepository = createMock(CustomCalendarRepository.class);
+    calendarService = createMock(CustomCalendarService.class);
+    ConfigBean config = new ConfigBean();
+
+    eventLogService = new EventLogServiceImpl(logRepository, groupRepository, groupLogRepository,
+        folderRepo, userSrv, config, userMetadataRepo, temporaryCacheService, storageFactory,
+        logFileService, storageRepository, calendarRepository, calendarService);
+  }
+
+  @Test
+  @Ignore
+  public void getOpenXesVersion() {
+    System.out.println("OPENXES_VERSION: " + XRuntimeUtils.OPENXES_VERSION);
+    // XFactoryNaiveImpl xFactoryNaive = new XFactoryNaiveImpl();
+    // System.out.println(xFactoryNaive.isUseInterner());
+  }
+
+  @Test
+  public void validateXLogImport() {
+
+    try (Stream<Path> paths =
+        Files.walk(Paths.get(ClassLoader.getSystemResource("XES_logs/").getPath()))) {
+      paths.filter(Files::isRegularFile).forEach(this::getXlog);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void getXlog(Path path) {
+    XTimer timer = new XTimer();
+    List<XLog> parsedLog;
+    XFactory factory = XFactoryRegistry.instance().currentDefault();
+    XesXmlParser parser = new XesXmlParser(factory);
+    try {
+      parsedLog = parser.parse(new FileInputStream(path.toFile()));
+      walkLog(parsedLog.iterator().next());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    timer.stop();
+
+    System.out.println("Imported and walked log: " + path.getFileName());
+    System.out.println("Duration: " + timer.getDurationString());
+  }
+
+  @Test
+  public void eventLogValidationTest() {
+    Path name = Paths
+        .get(ClassLoader.getSystemResource("XES_logs/A1_overlap_no_waiting_time.xes").getPath());
+    XLog xLog = fetchXlog(name);
+
+    assert xLog != null;
+    EventLogServiceImpl.validateLog(xLog);
+    assertEquals(4, xLog.size());
+    int totalEventSize = 0;
+    Set<String> uniqueActs = new HashSet<>();
+    for (XTrace trace : xLog) {
+      totalEventSize += trace.size();
+      for (XEvent event : trace) {
+        uniqueActs.add(event.getAttributes().get("concept:name").toString());
+      }
+    }
+    assertEquals(17, totalEventSize);
+    assertEquals(5, uniqueActs.size());
+  }
+
+  private XLog fetchXlog(Path path) {
+    List<XLog> parsedLog;
+    XFactory factory = XFactoryRegistry.instance().currentDefault();
+    XesXmlParser parser = new XesXmlParser(factory);
+    try {
+      parsedLog = parser.parse(new FileInputStream(path.toFile()));
+      return parsedLog.iterator().next();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    private static void walkTrace(XTrace trace) {
-        walkAttributes(trace);
-        for (XEvent event : trace) {
-            walkAttributes(event);
-        }
+    return null;
+  }
+
+  @Test
+  public void intersectionTest() {
+    Set set1 = new HashSet(Arrays.asList(1, 3, 5));
+    Set set2 = new HashSet(Arrays.asList(1, 6, 7, 9, 3));
+    Set set3 = new HashSet(Arrays.asList(1, 3, 10, 11));
+
+    List<Set<Integer>> lists = new ArrayList<>();
+    lists.add(set1);
+    lists.add(set2);
+    lists.add(set3);
+
+    List<Integer> commons = new ArrayList<Integer>();
+    commons.addAll(lists.get(1));
+    for (ListIterator<Set<Integer>> iterator = lists.listIterator(1); iterator.hasNext();) {
+      commons.retainAll(iterator.next());
     }
 
-    private static void walkAttributes(XAttributable attributable) {
-        XAttributeMap attributeMap = attributable.getAttributes();
-        for (XAttribute attribute : attributeMap.values()) {
-            String key = attribute.getKey();
-            String value = attribute.toString();
-            key.trim();
-            value.trim();
-            walkAttributes(attribute);
-        }
+    System.out.println(commons);
+
+    System.out.println(intersection(lists));
+  }
+
+
+  public <T> Set<T> intersection(List<T>... list) {
+    Set<T> result = Sets.newHashSet(list[0]);
+    for (List<T> numbers : list) {
+      result = Sets.intersection(result, Sets.newHashSet(numbers));
     }
-
-    public EntityManagerFactory getEntityManagerFactory() {
-        if (emf == null) {
-            emf = Persistence.createEntityManagerFactory("TESTApromore");
-        }
-        return emf;
-    }
-
-    @Before
-    public final void setUp() {
-        logRepository = createMock(LogRepository.class);
-        groupRepository = createMock(GroupRepository.class);
-        groupLogRepository = createMock(GroupLogRepository.class);
-        folderRepo = createMock(FolderRepository.class);
-        userSrv = createMock(UserService.class);
-        userMetadataRepo = createMock(UsermetadataRepository.class);
-        temporaryCacheService=createMock(TemporaryCacheService.class);
-        storageFactory=createMock(StorageManagementFactory.class);
-        logFileService=createMock(EventLogFileService.class);
-        storageRepository=createMock(StorageRepository.class);
-	calendarRepository = createMock(CustomCalendarRepository.class);
-        ConfigBean config = new ConfigBean();
-
-        eventLogService = new EventLogServiceImpl(logRepository, groupRepository, groupLogRepository, folderRepo,
-                userSrv, config,
-                userMetadataRepo,temporaryCacheService,storageFactory,logFileService,storageRepository, calendarRepository);
-    }
-
-    @Test
-    @Ignore
-    public void getOpenXesVersion() {
-        System.out.println("OPENXES_VERSION: " + XRuntimeUtils.OPENXES_VERSION);
-//        XFactoryNaiveImpl xFactoryNaive = new XFactoryNaiveImpl();
-//        System.out.println(xFactoryNaive.isUseInterner());
-    }
-
-    @Test
-    public void validateXLogImport() {
-
-        try (Stream<Path> paths = Files.walk(Paths.get(ClassLoader.getSystemResource("XES_logs/").getPath()))) {
-            paths.filter(Files::isRegularFile)
-                    .forEach(this::getXlog);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void getXlog(Path path) {
-        XTimer timer = new XTimer();
-        List<XLog> parsedLog;
-        XFactory factory = XFactoryRegistry.instance().currentDefault();
-        XesXmlParser parser = new XesXmlParser(factory);
-        try {
-            parsedLog = parser.parse(new FileInputStream(path.toFile()));
-            walkLog(parsedLog.iterator().next());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        timer.stop();
-
-        System.out.println("Imported and walked log: " + path.getFileName());
-        System.out.println("Duration: " + timer.getDurationString());
-    }
-
-    @Test
-    public void eventLogValidationTest() {
-        Path name = Paths.get(ClassLoader.getSystemResource("XES_logs/A1_overlap_no_waiting_time.xes").getPath());
-        XLog xLog = fetchXlog(name);
-
-        assert xLog != null;
-        EventLogServiceImpl.validateLog(xLog);
-        assertEquals(4, xLog.size());
-        int totalEventSize = 0;
-        Set<String> uniqueActs = new HashSet<>();
-        for (XTrace trace : xLog) {
-            totalEventSize += trace.size();
-            for (XEvent event : trace) {
-                uniqueActs.add(event.getAttributes().get("concept:name").toString());
-            }
-        }
-        assertEquals(17, totalEventSize);
-        assertEquals(5, uniqueActs.size());
-    }
-
-    private XLog fetchXlog(Path path) {
-        List<XLog> parsedLog;
-        XFactory factory = XFactoryRegistry.instance().currentDefault();
-        XesXmlParser parser = new XesXmlParser(factory);
-        try {
-            parsedLog = parser.parse(new FileInputStream(path.toFile()));
-            return parsedLog.iterator().next();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    @Test
-    public void intersectionTest() {
-        Set set1 = new HashSet(Arrays.asList(1, 3, 5));
-        Set set2 = new HashSet(Arrays.asList(1, 6, 7, 9, 3));
-        Set set3 = new HashSet(Arrays.asList(1, 3, 10, 11));
-
-        List<Set<Integer>> lists = new ArrayList<>();
-        lists.add(set1);
-        lists.add(set2);
-        lists.add(set3);
-
-        List<Integer> commons = new ArrayList<Integer>();
-        commons.addAll(lists.get(1));
-        for (ListIterator<Set<Integer>> iterator = lists.listIterator(1); iterator.hasNext(); ) {
-            commons.retainAll(iterator.next());
-        }
-
-        System.out.println(commons);
-
-        System.out.println(intersection(lists));
-    }
-
-
-    public <T> Set<T> intersection(List<T>... list) {
-        Set<T> result = Sets.newHashSet(list[0]);
-        for (List<T> numbers : list) {
-            result = Sets.intersection(result, Sets.newHashSet(numbers));
-        }
-        return result;
-    }
+    return result;
+  }
 
 }
