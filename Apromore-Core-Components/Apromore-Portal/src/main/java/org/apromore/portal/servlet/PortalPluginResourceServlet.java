204b1886d5592ac45184f3dda833d99cecdfc941
Simon Raboczi <simon.raboczi@apromore.com>
2021-08-02 14:18:09 +1000

    Fix pointed out by SonarCloud.

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/servlet/PortalPluginResourceServlet.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/servlet/PortalPluginResourceServlet.java
@@ -36,19 +36,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apromore.plugin.portal.PortalPlugin;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.zkoss.util.Locales;
 
 /**
  * Serves resources with <code>.png</code> or <code>.svg</code> extensions
- * from {@link PortalPlugin} bundles.
+ * from the classloader.
  *
  * The required path format is <code>portalPluginResource/<var>path</var>.<var>extension</var></code>
  * where:
  * <ul>
- * <li><var>path</var>.<var>extension</var> names a resource within the {@link PortalPlugin} bundle</li>
+ * <li><var>path</var>.<var>extension</var> names a resource within the classloader</li>
  * <li><var>extension</var> is either <code>png</code> or <code>svg</code></li>
  * </ul>
  *
@@ -80,39 +77,31 @@ public class PortalPluginResourceServlet extends HttpServlet {
     public void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
         Pattern p = Pattern.compile("/(?<resource>[^\\.]*\\.(?<extension>[^/\\.]*))");
         Matcher m = p.matcher(req.getPathInfo());
-        if (m.find()) {
-            String resource = URLDecoder.decode(m.group("resource"), "utf-8");
-            String extension = URLDecoder.decode(m.group("extension"), "utf-8");
+        if (!m.find()) {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Unable to parse path " + req.getPathInfo());
+            return;
+        }
 
-            AutowireCapableBeanFactory beanFactory = WebApplicationContextUtils.getWebApplicationContext(getServletContext()).getAutowireCapableBeanFactory();
+        String resource = URLDecoder.decode(m.group("resource"), "utf-8");
+        String extension = URLDecoder.decode(m.group("extension"), "utf-8");
 
-            for (PortalPlugin portalPlugin: (List<PortalPlugin>) beanFactory.getBean("portalPlugins")){
-                try (InputStream in = portalPlugin.getResourceAsStream(resource)) {
-                    if (in == null) {
-                        resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Unable to find resource for " + req.getPathInfo());
-                        return;
-                    }
-
-                    String contentType = contentTypeMap.get(extension.toLowerCase());
-                    if (contentType == null) {
-                        resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Unsupported resource extension \"" + extension + "\" for " + req.getPathInfo());
-                        return;
-                    }
-
-                    resp.setStatus(HttpServletResponse.SC_OK);
-                    resp.setContentType(contentType);
-                    try (OutputStream out = resp.getOutputStream()) {
-                        ByteStreams.copy(in, out);
-                    }
-                }
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream(resource)) {
+            if (in == null) {
+                resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Unable to find resource for " + req.getPathInfo());
                 return;
             }
 
-            // Fall through
-            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Unable to find portal plugin for " + req.getPathInfo());
+            String contentType = contentTypeMap.get(extension.toLowerCase());
+            if (contentType == null) {
+                resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Unsupported resource extension \"" + extension + "\" for " + req.getPathInfo());
+                return;
+            }
 
-        } else {
-            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Unable to parse path " + req.getPathInfo());
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setContentType(contentType);
+            try (OutputStream out = resp.getOutputStream()) {
+                ByteStreams.copy(in, out);
+            }
         }
     }
 }
