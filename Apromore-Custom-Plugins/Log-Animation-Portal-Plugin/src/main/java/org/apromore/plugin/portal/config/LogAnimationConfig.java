e74ac3a35228529740dc0b6f8d08110191fe454e
Nolan Tellis <nolan.tellis@apromore.com>
2021-08-03 15:04:32 +1000

    Minor chnage in bean name

--- Apromore-Custom-Plugins/Log-Animation-Portal-Plugin/src/main/java/org/apromore/plugin/portal/config/LogAnimationConfig.java
+++ Apromore-Custom-Plugins/Log-Animation-Portal-Plugin/src/main/java/org/apromore/plugin/portal/config/LogAnimationConfig.java
@@ -22,21 +22,20 @@
 package org.apromore.plugin.portal.config;
 
 import org.apromore.plugin.portal.loganimation.LogAnimationPlugin;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogAnimationConfig {
 
-	@Bean
-	public LogAnimationPlugin logAnimationPluginCe() {
-		LogAnimationPlugin lgPlugin = new LogAnimationPlugin();
-		lgPlugin.setGroupLabel("Analyze");
-		lgPlugin.setLabel("Animate logs CE");
-		return lgPlugin;
-	}
-
-
+    @Bean
+    @ConditionalOnMissingBean(name = "logAnimationPlugin")
+    public LogAnimationPlugin logAnimationPlugin() {
+	LogAnimationPlugin lgPlugin = new LogAnimationPlugin();
+	lgPlugin.setGroupLabel("Analyze");
+	lgPlugin.setLabel("Animate logs CE");
+	return lgPlugin;
+    }
 
 }
