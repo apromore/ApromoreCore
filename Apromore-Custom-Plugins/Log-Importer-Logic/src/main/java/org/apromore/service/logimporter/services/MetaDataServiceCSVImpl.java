fdf23aa5d11f8971ace2bcb67610dffb45fca036
frankma <49012776+frank-ma-leonardo@users.noreply.github.com>
2021-08-17 14:32:15 +1000

    AP-2774 Rename CSVImporter plugin to LogImporter (#1203)
    * AP-2774 Rename CSVImporter plugin to LogImporter
    
    * Fix bugs SonarCloud reported
    
    * Bugfix
    
    * Fix code smell
    
    * temp fix of sec-hotspots
    
    * Fix failing unit tests on Windows
    
    * Revert change on temp file creation since SonarCloud could not run it correctly

---
+++ Apromore-Custom-Plugins/Log-Importer-Logic/src/main/java/org/apromore/service/logimporter/services/MetaDataServiceCSVImpl.java
@@ -0,0 +1,174 @@
+/*-
+ * #%L
+ * This file is part of "Apromore Core".
+ * %%
+ * Copyright (C) 2018 - 2021 Apromore Pty Ltd.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+package org.apromore.service.logimporter.services;
+
+import com.google.common.base.Splitter;
+import com.opencsv.CSVReader;
+import org.apache.commons.io.input.ReaderInputStream;
+import org.apromore.commons.utils.Delimiter;
+import org.apromore.service.logimporter.io.CSVFileReader;
+import org.apromore.service.logimporter.model.LogMetaData;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+class MetaDataServiceCSVImpl implements MetaDataService {
+
+    /**
+     * Sample rows that are used to determine Delimiter
+     */
+    private static final int SAMPLE_ROW_COUNT = 2;
+
+    private Reader reader;
+    private BufferedReader brReader;
+    private InputStream in2;
+    private CSVReader csvReader;
+    private List<String> headers = new ArrayList<String>();
+    private String separator = "";
+
+    @Override
+    public void validateLog(InputStream in, String charset) throws Exception {
+
+        try {
+            reader = new InputStreamReader(in, Charset.forName(charset));
+            brReader = new BufferedReader(reader);
+
+            List<String> sampleRows = getSampleRows(brReader);
+
+            String firstLine = sampleRows.get(0);
+            if (firstLine == null || firstLine.isEmpty()) {
+                throw new Exception("header must have non-empty value!");
+            }
+
+            this.separator = Delimiter.findDelimiter(sampleRows);
+            this.headers = Splitter.on(separator).splitToList(firstLine);
+
+        } catch (IOException e) {
+            throw new Exception("Unable to import file", e);
+        } finally {
+            closeQuietly(in);
+        }
+    }
+
+    @Override
+    public LogMetaData extractMetadata(InputStream in, String charset) throws Exception {
+
+        if (!headers.isEmpty()) {
+            return new LogMetaData(this.headers);
+        }
+        try {
+            reader = new InputStreamReader(in, Charset.forName(charset));
+            brReader = new BufferedReader(reader);
+
+            List<String> sampleRows = getSampleRows(brReader);
+
+            String firstLine = sampleRows.get(0);
+            this.separator = Delimiter.findDelimiter(sampleRows);
+            this.headers = Splitter.on(separator).splitToList(firstLine);
+
+            return new LogMetaData(headers);
+
+        } finally {
+            closeQuietly(in);
+        }
+    }
+
+    @Override
+    public List<List<String>> generateSampleLog(InputStream in, int sampleSize, String charset) throws Exception {
+
+        try {
+            reader = new InputStreamReader(in, Charset.forName(charset));
+            brReader = new BufferedReader(reader);
+
+            List<String> sampleRows = getSampleRows(brReader);
+
+            String firstLine = sampleRows.get(0);
+
+            String separator = !Objects.equals(this.separator, "") ? this.separator :
+                    Delimiter.findDelimiter(sampleRows);
+
+            List<String> header = !headers.isEmpty() ? headers
+                    : Splitter.on(separator).splitToList(firstLine);
+
+            in2 = new ReaderInputStream(brReader, charset);
+            csvReader = new CSVFileReader().newCSVReader(in2, charset, !Objects.equals(separator, "") ?
+                    separator.charAt(0) : '\u0000');
+
+            if (csvReader == null)
+                return null;
+
+            List<List<String>> lines = new ArrayList<>();
+            String[] myLine;
+
+            // Add content lines to sample log to avoid resetting of stream
+            for (int i = 1; i < SAMPLE_ROW_COUNT; i++) {
+                lines.add(Splitter.on(separator).splitToList(sampleRows.get(i).replaceAll("\"", "")));
+            }
+
+            while ((myLine = csvReader.readNext()) != null && lines.size() < sampleSize) {
+                if (myLine.length != header.size())
+                    continue;
+                lines.add(Arrays.asList(myLine));
+            }
+
+            return lines;
+
+        } finally {
+            closeQuietly(in);
+        }
+    }
+
+    private List<String> getSampleRows(BufferedReader brReader) throws IOException {
+
+        List<String> rows = new ArrayList<>();
+
+        for (int i = 0; i < SAMPLE_ROW_COUNT; i++) {
+            String rowLine = brReader.readLine();
+            if (!"".equals(rowLine)) {
+                rows.add(rowLine.replaceAll("\"", ""));
+            }
+        }
+
+        return rows;
+    }
+
+    private void closeQuietly(InputStream in) throws IOException {
+        if (in != null)
+            in.close();
+        if (this.csvReader != null)
+            this.csvReader.close();
+        if (this.brReader != null)
+            this.brReader.close();
+        if (this.reader != null)
+            this.reader.close();
+        if (this.in2 != null)
+            this.in2.close();
+    }
+}
---
+++ Apromore-Custom-Plugins/Log-Importer-Portal/src/main/java/org/apromore/plugin/portal/logimporter/LogImporterFileImporterPlugin.java
@@ -0,0 +1,294 @@
+/*-
+ * #%L
+ * This file is part of "Apromore Core".
+ * %%
+ * Copyright (C) 2018 - 2021 Apromore Pty Ltd.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+package org.apromore.plugin.portal.logimporter;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import org.apromore.dao.model.Log;
+import org.apromore.dao.model.Usermetadata;
+import org.apromore.exception.UserNotFoundException;
+import org.apromore.plugin.portal.FileImporterPlugin;
+import org.apromore.plugin.portal.PortalContext;
+import org.apromore.plugin.portal.PortalLoggerFactory;
+import org.apromore.plugin.portal.logimporter.listener.LogImportListener;
+import org.apromore.service.UserMetadataService;
+import org.apromore.service.logimporter.exception.EmptyHeaderException;
+import org.apromore.service.logimporter.exception.UnsupportedSeparatorException;
+import org.apromore.service.logimporter.services.ParquetFactoryProvider;
+import org.apromore.service.logimporter.services.legacy.LogImporterProvider;
+import org.apromore.util.UserMetadataTypeEnum;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.zkoss.json.JSONObject;
+import org.zkoss.json.JSONValue;
+import org.zkoss.util.media.Media;
+import org.zkoss.web.Attributes;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Window;
+import com.opencsv.CSVReader;
+
+@Component
+public class LogImporterFileImporterPlugin implements FileImporterPlugin {
+
+  private static Logger LOGGER = PortalLoggerFactory.getLogger(LogImporterFileImporterPlugin.class);
+
+  @Autowired
+  private ParquetFactoryProvider parquetFactoryProvider;
+  @Autowired
+  private LogImporterProvider logImporterProvider;
+  @Autowired
+  private UserMetadataService userMetadataService;
+
+  private static String getMediaFormat(Media media) throws Exception {
+    if (media.getName().lastIndexOf('.') < 0)
+      throw new Exception("Can't read file format");
+    return media.getName().substring(media.getName().lastIndexOf('.') + 1);
+  }
+
+  public ParquetFactoryProvider getParquetFactoryProvider() {
+    return parquetFactoryProvider;
+  }
+
+  public void setParquetFactoryProvider(ParquetFactoryProvider parquetFactoryProvider) {
+    this.parquetFactoryProvider = parquetFactoryProvider;
+  }
+
+  public LogImporterProvider getLogImporterProvider() {
+    return logImporterProvider;
+  }
+
+  public void setLogImporterProvider(LogImporterProvider logImporterProvider) {
+    this.logImporterProvider = logImporterProvider;
+  }
+
+  public ResourceBundle getLabels() {
+    Locale locale = (Locale) Sessions.getCurrent().getAttribute(Attributes.PREFERRED_LOCALE);
+    return ResourceBundle.getBundle(PluginMeta.PLUGIN_ID, locale,
+            LogImporterController.class.getClassLoader());
+  }
+
+  // Implementation of FileImporterPlugin
+
+  public void setUserMetadataService(UserMetadataService newUserMetadataService) {
+    LOGGER.info("Injected CSV importer logic {}", newUserMetadataService);
+    this.userMetadataService = newUserMetadataService;
+  }
+
+  @Override
+  public Set<String> getFileExtensions() {
+    return new HashSet<>(Arrays.asList("csv", "parquet", "xlsx"));
+  }
+
+  @Override
+  @Transactional(rollbackFor = Exception.class)
+  public void importFile(Media media, boolean isLogPublic) {
+
+    // Configure the arguments to pass to the CSV importer view
+    Map arg = new HashMap<>();
+    arg.put("parquetFactoryProvider", parquetFactoryProvider);
+    arg.put("logImporterProvider", logImporterProvider);
+    arg.put("media", media);
+    Sessions.getCurrent().setAttribute(LogImporterController.SESSION_ATTRIBUTE_KEY, arg);
+    PortalContext portalContext =
+        (PortalContext) Sessions.getCurrent().getAttribute("portalContext");
+    LogImportListener logImportListener = new LogImportListener(arg,
+        (String) Sessions.getCurrent().getAttribute("fileimportertarget"), null, null);
+    // Only works for CSV
+    try {
+      if ("csv".equals(getMediaFormat(media))) {
+        String username = portalContext.getCurrentUser().getUsername();
+        // Get header from imported CSV
+        List<String> header = new ArrayList<>();
+        String fileEncoding = "UTF-8";
+        CSVFileReader csvFileReader = new CSVFileReader();
+        try (CSVReader csvReader = csvFileReader.newCSVReader(media, fileEncoding)) {
+          header = Arrays.asList(csvReader.readNext());
+        } catch (EmptyHeaderException e) {
+          Messagebox.show(getLabels().getString("failedImportHeader"));
+          return;
+        } catch (UnsupportedSeparatorException e) {
+          Messagebox.show(getLabels().getString("unsupportedSeparator"));
+          return;
+        } catch (IOException e) {
+          Messagebox.show(getLabels().getString("failedImport"));
+          LOGGER.error("Unable to read CSV", e);
+          return;
+        }
+        // Get saved schema mapping from DB
+        List<Usermetadata> mappingJSONList;
+        Set<Usermetadata> usermetadataSet = null;
+
+        try {
+          usermetadataSet = userMetadataService
+              .getUserMetadataWithoutLog(UserMetadataTypeEnum.CSV_IMPORTER, username);
+        } catch (UserNotFoundException e) {
+          LOGGER.error("Unable to find user " + username, e);
+          return;
+        }
+
+        if (usermetadataSet != null) {
+          mappingJSONList = new ArrayList<>(usermetadataSet);
+        } else
+          mappingJSONList = new ArrayList<>();
+
+        // Sort by Usermetadata object Id, since a new csv schema mapping is created each time.
+        mappingJSONList.sort(Comparator.comparing(Usermetadata::getId));
+
+        if (mappingJSONList.size() != 0) {
+
+          // Matching from the latest record
+          for (int i = mappingJSONList.size() - 1; i >= 0; i--) {
+
+            Usermetadata usermetadata = mappingJSONList.get(i);
+
+            JSONObject jsonObject = (JSONObject) JSONValue.parse(usermetadata.getContent());
+
+            assert jsonObject != null;
+            LOGGER.debug("Trying to match with stored schema: {}",
+                JSONValue.parse(jsonObject.get("header").toString()));
+
+            List<String> sampleHeader = (List<String>) jsonObject.get("header");
+
+            // Create a popup window on top of Importer window to prompt matched schema
+            if (sampleHeader != null && sampleHeader.equals(header))
+              try {
+
+                LOGGER.debug("Found matched schema: {} : {}", sampleHeader, header);
+                Map<String, Object> arg2 = new HashMap<>();
+                arg2.put("labels", getLabels());
+                Window matchedMappingPopUp =
+                    (Window) portalContext.getUI().createComponent(getClass().getClassLoader(),
+                        "zul/matchedMapping.zul", null, arg2);
+                matchedMappingPopUp.doModal();
+
+                Date date = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
+                    .parse(usermetadata.getCreatedTime());
+                String formattedDate = new SimpleDateFormat("dd/MM/yyyy").format(date);
+                String formattedTime = new SimpleDateFormat("HH:mm:ss").format(date);
+
+                Label fileNameLabel = (Label) matchedMappingPopUp.getFellow("fileNameLabel");
+                Label fileTimeLabel = (Label) matchedMappingPopUp.getFellow("fileTimeLabel");
+                Set<Log> logs = usermetadata.getLogs();
+
+                for (Log log : logs) {
+                  fileNameLabel.setValue(log.getName());
+                  fileTimeLabel
+                      .setValue(" , uploaded at " + formattedTime + " on " + formattedDate);
+                }
+
+                Button uploadWithMatchedMappingBtn =
+                    (Button) matchedMappingPopUp.getFellow("uploadWithMatchedMapping");
+                Button uploadAsNewBtn = (Button) matchedMappingPopUp.getFellow("uploadAsNew");
+                uploadWithMatchedMappingBtn.addEventListener("onClick", event -> {
+                  arg.put("mappingJSON", jsonObject);
+                  matchedMappingPopUp.detach();
+
+                  // Create a CSV importer view
+                  switch ((String) Sessions.getCurrent().getAttribute("fileimportertarget")) {
+                    case "page": // create the view in its own page
+                      Executions.getCurrent().sendRedirect("import-csv/csvimporter.zul", "_blank");
+                      break;
+
+                    case "modal":
+                    default: // create the view in a modal popup within the current page
+                      try {
+                        Window window = (Window) portalContext.getUI().createComponent(
+                            getClass().getClassLoader(), "import-csv/csvimporter.zul", null, arg);
+                        window.doModal();
+
+                      } catch (IOException e) {
+                        LOGGER.error("Unable to create window", e);
+                      }
+                      break;
+                  }
+                });
+                uploadAsNewBtn.addEventListener("onClick", event -> {
+                  arg.put("mappingJSON", null);
+                  matchedMappingPopUp.detach();
+
+                  // Create a CSV importer view
+                  switch ((String) Sessions.getCurrent().getAttribute("fileimportertarget")) {
+                    case "page": // create the view in its own page
+                      Executions.getCurrent().sendRedirect("import-csv/csvimporter.zul", "_blank");
+                      break;
+
+                    case "modal":
+                    default: // create the view in a modal popup within the current page
+                      try {
+                        Window window = (Window) portalContext.getUI().createComponent(
+                            getClass().getClassLoader(), "import-csv/csvimporter.zul", null, arg);
+                        window.doModal();
+
+                      } catch (IOException e) {
+                        LOGGER.error("Unable to create window", e);
+                      }
+                      break;
+                  }
+                });
+                // only match the last schema mapping if there are multiple
+                return;
+
+              } catch (IOException | ParseException e) {
+                LOGGER.error("Unable to import CSV", e);
+              }
+
+          }
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("Can't read file format ", e);
+    }
+    // can't find match in JSONList or no mapping record
+    arg.put("mappingJSON", null);
+
+    // Create a CSV importer view
+    switch ((String) Sessions.getCurrent().getAttribute("fileimportertarget")) {
+      case "page": // create the view in its own page
+        Executions.getCurrent().sendRedirect("import-csv/csvimporter.zul", "_blank");
+        break;
+
+      case "modal":
+      default: // create the view in a modal popup within the current page
+
+        try {
+          Window window =
+              (Window) portalContext.getUI().createComponent(getClass().getClassLoader(),
+                  "import-csv/csvimporter.zul", null, arg);
+          window.doModal();
+
+        } catch (IOException e) {
+          LOGGER.error("Unable to create window", e);
+        }
+        break;
+    }
+  }
+}
