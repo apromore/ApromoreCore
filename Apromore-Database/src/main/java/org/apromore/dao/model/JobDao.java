0e26f6d0b4dbf173e4f9cf31ccc70d052d9578b3
Mayank Tomar <mayank.tomar@apromore.com>
2021-09-03 15:54:21 +1000

    test: updated tests for job db dao

--- Apromore-Database/src/main/java/org/apromore/dao/model/JobDao.java
+++ Apromore-Database/src/main/java/org/apromore/dao/model/JobDao.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -44,87 +44,87 @@ import lombok.Setter;
 @Setter
 public class JobDao implements Serializable {
 
-  private Long id;
-  private String dagId;
-  private List<DagConnection> connections = new ArrayList<>();
-  private List<StaticLog> staticLogs = new ArrayList<>();
-  private OutputLogInfo logInfo;
-  private String finalTransformQuery;
-  private String schedule;
-  private String username;
-  private String pipelineName;
-  private boolean error;
-  private int scheduleVersionNumber;
-  private boolean paused;
+    private Long id;
+    private String dagId;
+    private List<DagConnection> connections = new ArrayList<>();
+    private List<StaticLog> staticLogs = new ArrayList<>();
+    private OutputLogInfo logInfo;
+    private String finalTransformQuery;
+    private String schedule;
+    private String username;
+    private String pipelineName;
+    private boolean error;
+    private int scheduleVersionNumber;
+    private boolean paused;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id", unique = true, nullable = false)
-  public Long getId() {
-    return id;
-  }
-
-  @OneToMany(mappedBy = "job", cascade = CascadeType.ALL, orphanRemoval = true)
-  public List<DagConnection> getConnections() {
-    return connections;
-  }
-
-  @OneToMany(mappedBy = "job", cascade = CascadeType.ALL, orphanRemoval = true)
-  public List<StaticLog> getStaticLogs() {
-    return staticLogs;
-  }
-
-  @Column(name = "dag_id")
-  public String getDagId() {
-    return dagId;
-  }
-
-  @Column(name = "final_transform_query")
-  public String getFinalTransformQuery() {
-    return finalTransformQuery;
-  }
-
-  @Column(name = "schedule")
-  public String getSchedule() {
-    return schedule;
-  }
-
-  @Column(name = "username")
-  public String getUsername() {
-    return username;
-  }
-
-  @Column(name = "pipeline_name")
-  public String getPipelineName() {
-    return pipelineName;
-  }
-
-  @Column(name = "error")
-  public boolean getError() {
-    return error;
-  }
-
-  @Column(name = "schedule_version_number")
-  public int getScheduleVersionNumber() {
-    return scheduleVersionNumber;
-  }
-
-  @Column(name = "paused")
-  public boolean getPaused() {
-    return paused;
-  }
-
-  @Embedded
-  public OutputLogInfo getLogInfo() {
-    return logInfo;
-  }
-
-  public void synchronizedMetaData() {
-    for (DagConnection connection : connections) {
-      connection.setJob(this);
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    public Long getId() {
+        return id;
     }
-    for (StaticLog staticLog : staticLogs) {
-      staticLog.setJob(this);
+
+    @OneToMany(mappedBy = "job", cascade = CascadeType.ALL, orphanRemoval = true)
+    public List<DagConnection> getConnections() {
+        return connections;
     }
-  }
+
+    @OneToMany(mappedBy = "job", cascade = CascadeType.ALL, orphanRemoval = true)
+    public List<StaticLog> getStaticLogs() {
+        return staticLogs;
+    }
+
+    @Column(name = "dag_id")
+    public String getDagId() {
+        return dagId;
+    }
+
+    @Column(name = "final_transform_query")
+    public String getFinalTransformQuery() {
+        return finalTransformQuery;
+    }
+
+    @Column(name = "schedule")
+    public String getSchedule() {
+        return schedule;
+    }
+
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    @Column(name = "pipeline_name")
+    public String getPipelineName() {
+        return pipelineName;
+    }
+
+    @Column(name = "error")
+    public boolean getError() {
+        return error;
+    }
+
+    @Column(name = "schedule_version_number")
+    public int getScheduleVersionNumber() {
+        return scheduleVersionNumber;
+    }
+
+    @Column(name = "paused")
+    public boolean getPaused() {
+        return paused;
+    }
+
+    @Embedded
+    public OutputLogInfo getLogInfo() {
+        return logInfo;
+    }
+
+    public void synchronizedMetaData() {
+        for (DagConnection connection : connections) {
+            connection.setJob(this);
+        }
+        for (StaticLog staticLog : staticLogs) {
+            staticLog.setJob(this);
+        }
+    }
 }
--- Apromore-Database/src/test/java/org/apromore/dao/jpa/etl/DbConnectorDaoUnitTest.java
+++ Apromore-Database/src/test/java/org/apromore/dao/jpa/etl/DbConnectorDaoUnitTest.java
@@ -7,28 +7,79 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 public class DbConnectorDaoUnitTest extends BaseTestClass {
+    public static final String CONNECTION_ID = "connection_id";
+
     @Autowired
     DbConnectorRepository dbConnectorRepository;
 
     @Test
-    public void test() {
+    public void createDbConnectorDao() {
         // Given
-        DbConnectorDao dbConnectorDao = new DbConnectorDao(true, "connection_id", "username", "password", "url", "database_schema", "port");
+        DbConnectorDao dbConnectorDao = new DbConnectorDao(true, CONNECTION_ID, "username", "password", "url", "database_schema", "port");
+
         // When
         dbConnectorDao = dbConnectorRepository.saveAndFlush(dbConnectorDao);
+
         // Then
         assertThat(dbConnectorDao.getId()).isNotNull();
+        assertThat(dbConnectorDao.getConnectionKey()).isEqualTo(CONNECTION_ID);
+    }
 
-        // Find
-        DbConnectorDao dbConnectorDaoExpected = dbConnectorRepository.findById(dbConnectorDao.getId()).get();
-        assertThat(dbConnectorDaoExpected).usingRecursiveComparison().isEqualTo(dbConnectorDao);
+    @Test
+    public void getDbConnectorDao() {
+        // Given
+        DbConnectorDao dbConnectorDaoToSave = new DbConnectorDao(true, CONNECTION_ID, "username", "password", "url", "database_schema", "port");
+        dbConnectorDaoToSave = dbConnectorRepository.saveAndFlush(dbConnectorDaoToSave);
+        Long id = dbConnectorDaoToSave.getId();
 
-        // Delete
-        dbConnectorRepository.deleteById(dbConnectorDao.getId());
-        assertThat(dbConnectorRepository.existsById(dbConnectorDao.getId())).isFalse();
+        // When
+        DbConnectorDao dbConnectorDaoExpected = dbConnectorRepository.findById(id).get();
+
+        // Then
+        assertThat(dbConnectorDaoExpected.getConnectionKey()).isEqualTo(dbConnectorDaoToSave.getConnectionKey());
+        assertThat(dbConnectorDaoExpected.getDatabaseSchema()).isEqualTo(dbConnectorDaoToSave.getDatabaseSchema());
+        assertThat(dbConnectorDaoExpected.getPassword()).isEqualTo(dbConnectorDaoToSave.getPassword());
+        assertThat(dbConnectorDaoExpected.getPort()).isEqualTo(dbConnectorDaoToSave.getPort());
+        assertThat(dbConnectorDaoExpected.getId()).isEqualTo(dbConnectorDaoToSave.getId());
+        assertThat(dbConnectorDaoExpected.isCreated()).isEqualTo(dbConnectorDaoToSave.isCreated());
+        assertThat(dbConnectorDaoExpected.getUrl()).isEqualTo(dbConnectorDaoToSave.getUrl());
+        assertThat(dbConnectorDaoExpected.getUsername()).isEqualTo(dbConnectorDaoToSave.getUsername());
+    }
+
+    @Test
+    public void updateDbConnectorDao() {
+        // Given
+        DbConnectorDao dbConnectorDaoToSave = new DbConnectorDao(true, CONNECTION_ID, "username", "password", "url", "database_schema", "port");
+        dbConnectorDaoToSave = dbConnectorRepository.saveAndFlush(dbConnectorDaoToSave);
+        Long id = dbConnectorDaoToSave.getId();
+        dbConnectorDaoToSave.setPassword("updatedPassword");
+        dbConnectorRepository.saveAndFlush(dbConnectorDaoToSave);
+
+        // When
+        DbConnectorDao dbConnectorDaoExpected = dbConnectorRepository.findById(id).get();
+
+        // Then
+        assertThat(dbConnectorDaoExpected.getPassword()).isEqualTo("updatedPassword");
+    }
+
+    @Test(expected= NoSuchElementException.class)
+    public void deleteDbConnectorDao() {
+        // Given
+        DbConnectorDao dbConnectorDaoToSave = new DbConnectorDao(true, CONNECTION_ID, "username", "password", "url", "database_schema", "port");
+        dbConnectorDaoToSave = dbConnectorRepository.saveAndFlush(dbConnectorDaoToSave);
+        Long id = dbConnectorDaoToSave.getId();
+        dbConnectorRepository.deleteById(id);
+
+        // When
+        dbConnectorRepository.findById(id).get();
+
+        // Then
+        // NoSuchElementException
     }
  }
