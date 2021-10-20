efd23d4ab91c1896aa3186e98cdcd99269d3f236
Bruce Nguyen <hoangnguyen7x@yahoo.com>
2021-10-20 18:11:22 +1000

    Add copyright header.

--- Apromore-ProcessMining-Collection/prom-bpmn/src/test/java/org/apromore/processmining/plugins/bpmn/UnmarshallTest.java
+++ Apromore-ProcessMining-Collection/prom-bpmn/src/test/java/org/apromore/processmining/plugins/bpmn/UnmarshallTest.java
@@ -1,3 +1,20 @@
+/**
+ * #%L
+ * This file is part of "Apromore Enterprise Edition".
+ * %%
+ * Copyright (C) 2019 - 2021 Apromore Pty Ltd. All Rights Reserved.
+ * %%
+ * NOTICE:  All information contained herein is, and remains the
+ * property of Apromore Pty Ltd and its suppliers, if any.
+ * The intellectual and technical concepts contained herein are
+ * proprietary to Apromore Pty Ltd and its suppliers and may
+ * be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this
+ * material is strictly forbidden unless prior written permission
+ * is obtained from Apromore Pty Ltd.
+ * #L%
+ */
 package org.apromore.processmining.plugins.bpmn;
 
 import org.apromore.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
