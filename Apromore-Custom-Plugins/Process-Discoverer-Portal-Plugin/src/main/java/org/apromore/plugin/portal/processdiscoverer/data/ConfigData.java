57edfbac3158746ba329bb95c1678ff440a2dcb7
hoangnguyen7x <hoangnguyen7x@yahoo.com>
2021-11-11 12:23:47 +1000

    Externalize two properties maxNumberOfNodes and maxNumberOfArcs to a property file.

--- Apromore-Custom-Plugins/Process-Discoverer-Portal-Plugin/src/main/java/org/apromore/plugin/portal/processdiscoverer/data/ConfigData.java
+++ Apromore-Custom-Plugins/Process-Discoverer-Portal-Plugin/src/main/java/org/apromore/plugin/portal/processdiscoverer/data/ConfigData.java
@@ -22,14 +22,29 @@
 
 package org.apromore.plugin.portal.processdiscoverer.data;
 
+import org.apromore.commons.config.ConfigBean;
+import org.apromore.logman.Constants;
 import org.apromore.logman.attribute.graph.MeasureAggregation;
 import org.apromore.logman.attribute.graph.MeasureType;
 import org.eclipse.collections.impl.bimap.mutable.HashBiMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+@Configuration
+@PropertySource("classpath:pd.application.properties")
 public class ConfigData {
     private final String DEFAULT_SELECTOR;
+
     private final int MAX_NUMBER_OF_NODES;
+
     private final int MAX_NUMBER_OF_ARCS;
+
     private final HashBiMap<String,String> displayToOriginKeyMapping = new HashBiMap<>();
     {
         displayToOriginKeyMapping.put("concept:name", "Activity");
@@ -42,10 +57,12 @@ public class ConfigData {
     public static MeasureType DEFAULT_MEASURE_TYPE = MeasureType.FREQUENCY;
     public static MeasureAggregation DEFAULT_MEASURE_AGGREGATE = MeasureAggregation.CASES;
 
-    public static ConfigData DEFAULT = new ConfigData("concept:name", 5000, 15000);
-    
-    public ConfigData(String selector, int maxNumberOfNodes, int maxNumberOfArcs) {
-        DEFAULT_SELECTOR = selector;
+    public static ConfigData DEFAULT = new ConfigData("concept:name", 500, 500);
+
+    public ConfigData(@Value(Constants.ATT_KEY_CONCEPT_NAME) String attributeName,
+                      @Value("${pd.maxNodes}") int maxNumberOfNodes,
+                      @Value("${pd.maxArcs}") int maxNumberOfArcs) {
+        DEFAULT_SELECTOR = attributeName;
         MAX_NUMBER_OF_NODES = maxNumberOfNodes;
         MAX_NUMBER_OF_ARCS = maxNumberOfArcs;
     }
