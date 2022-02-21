d841f5d61a01d17c978a0288dcff9752943ba2c4
Jane Hoh <jane.hoh@apromore.com>
2022-02-21 17:26:41 +1100

    AP-5948 Add checkstyle to User-Admin-Portal-Plugin (#1751)

--- Apromore-Custom-Plugins/User-Admin-Portal-Plugin/src/main/java/org/apromore/plugin/portal/useradmin/listbox/AssignedUserListbox.java
+++ Apromore-Custom-Plugins/User-Admin-Portal-Plugin/src/main/java/org/apromore/plugin/portal/useradmin/listbox/AssignedUserListbox.java
@@ -8,25 +8,25 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
+
 package org.apromore.plugin.portal.useradmin.listbox;
 
-import org.apromore.plugin.portal.useradmin.common.SearchableListbox;
-import org.apromore.dao.model.User;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.ListModelList;
-
 import java.util.Set;
+import org.apromore.dao.model.User;
+import org.apromore.plugin.portal.useradmin.common.SearchableListbox;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
 
 public class AssignedUserListbox extends SearchableListbox {
 
@@ -36,11 +36,11 @@ public class AssignedUserListbox extends SearchableListbox {
 
     @Override
     public String getValue(int index) {
-        return ((User)this.getSourceListModel().get(index)).getUsername().toLowerCase();
+        return ((User) this.getSourceListModel().get(index)).getUsername().toLowerCase();
     }
 
     @Override
     public Set<User> getSelection() {
-        return (Set<User>)getListModel().getSelection();
+        return (Set<User>) getListModel().getSelection();
     }
 }
--- Apromore-Custom-Plugins/User-Admin-Portal-Plugin/src/main/java/org/apromore/plugin/portal/useradmin/listbox/TristateModel.java
+++ Apromore-Custom-Plugins/User-Admin-Portal-Plugin/src/main/java/org/apromore/plugin/portal/useradmin/listbox/TristateModel.java
@@ -8,24 +8,25 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
+
 package org.apromore.plugin.portal.useradmin.listbox;
 
 public class TristateModel {
 
-    public final static Integer UNCHECKED = 0;
-    public final static Integer CHECKED = 1;
-    public final static Integer INDETERMINATE = 2;
+    public static final Integer UNCHECKED = 0;
+    public static final Integer CHECKED = 1;
+    public static final Integer INDETERMINATE = 2;
 
     private String label;
     private String key;
