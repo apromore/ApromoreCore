c15e514fb3c26ce0c6254241f463b60801b9210b
hoangnguyen7x <hoangnguyen7x@yahoo.com>
2022-03-08 14:34:17 +1000

    Remove unused import.

--- Apromore-Core-Components/Apromore-Portal/src/test/java/org/apromore/portal/servlet/PortalPluginResourceServletUnitTest.java
+++ Apromore-Core-Components/Apromore-Portal/src/test/java/org/apromore/portal/servlet/PortalPluginResourceServletUnitTest.java
@@ -34,7 +34,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.api.Disabled;
 
 /**
  * Test suite for {@link PortalPluginResourceServlet}.
