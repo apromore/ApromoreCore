d27b5b4f99d7b7a50b1086bd6f4591748c10d6c3
Your Name <you@example.com>
2022-03-09 16:35:58 +1100

    fix: added headers

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/context/PluginPortalSelection.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/context/PluginPortalSelection.java
@@ -1,3 +1,21 @@
+/**
+ * #%L
+ * This file is part of "Apromore Enterprise Edition".
+ * %%
+ * Copyright (C) 2019 - 2021 Apromore Pty Ltd. All Rights Reserved.
+ * %%
+ * NOTICE:  All information contained herein is, and remains the
+ * property of Apromore Pty Ltd and its suppliers, if any.
+ * The intellectual and technical concepts contained herein are
+ * proprietary to Apromore Pty Ltd and its suppliers and may
+ * be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this
+ * material is strictly forbidden unless prior written permission
+ * is obtained from Apromore Pty Ltd.
+ * #L%
+ */
+
 package org.apromore.portal.context;
 
 import java.util.ArrayList;
@@ -10,8 +28,6 @@ import org.apromore.portal.model.LogSummaryType;
 import org.apromore.portal.model.ProcessSummaryType;
 import org.apromore.portal.model.SummaryType;
 import org.apromore.portal.model.VersionSummaryType;
-import org.zkoss.zhtml.Li;
-import org.zkoss.zul.Messagebox;
 
 public class PluginPortalSelection implements PortalSelection {
     private MainController mainController;
