5c6ff6e379c8dd3478507ec14bf2394f7bbda1e1
shapon2004 <shapon2004@hotmail.com>
2022-03-18 12:20:18 +0600

    AP-6162 Ordering of folders and files in tree and tile/list view.

--- Apromore-Core-Components/Apromore-Portal/src/test/java/org/apromore/portal/util/AlphaNumericComparatorUnitTest.java
+++ Apromore-Core-Components/Apromore-Portal/src/test/java/org/apromore/portal/util/AlphaNumericComparatorUnitTest.java
@@ -1,80 +1,35 @@
 package org.apromore.portal.util;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class AlphaNumericComparatorUnitTest {
 
-
-    @Test
-    public void samePrefixStringCompare(){
-        String name1="procure";
-        String name2="procurePay";
-        Assert.assertTrue((name1.compareTo(name2))<0);
-        Assert.assertTrue((AlphaNumericComparator.compareTo(name1,name2))<0);
+    @ParameterizedTest
+    @CsvSource({
+        "procure, procurePay",
+        "procureMent, procurePay",
+        "1procure, 2procure",
+        "1.1procure, 2procure",
+        "1.10procure, 2procure"
+    })
+    public void similarResultWithRegularComparison(String name1, String name2) {
+        Assert.assertTrue((name1.compareTo(name2)) < 0);
+        Assert.assertTrue((AlphaNumericComparator.compareTo(name1, name2)) < 0);
     }
 
-    @Test
-    public void samePrefixWithDifferentSuffixStringCompare(){
-        String name1="procureMent";
-        String name2="procurePay";
-        Assert.assertTrue((name1.compareTo(name2))<0);
-        Assert.assertTrue((AlphaNumericComparator.compareTo(name1,name2))<0);
-    }
+    @ParameterizedTest
+    @CsvSource({
+        "1.3procure, 1.10procure",
+        "procure1.3, procure1.10",
+        "procure1.3payment, procure1.10payment"
+    })
+    public void differentResultWithRegularComparison(String name1, String name2) {
+        Assert.assertFalse((name1.compareTo(name2)) < 0);
 
-    @Test
-    public void prefixNumberWithStringCompare(){
-        String name1="1procure";
-        String name2="2procure";
-        Assert.assertTrue((name1.compareTo(name2))<0);
-        Assert.assertTrue((AlphaNumericComparator.compareTo(name1,name2))<0);
-    }
-    @Test
-    public void prefixDecimalNumberAndStringCompare(){
-
-        String name1="1.1procure";
-        String name2="2procure";
-        Assert.assertTrue((name1.compareTo(name2))<0);
-        Assert.assertTrue((AlphaNumericComparator.compareTo(name1,name2))<0);
-
-        name1="1.10procure";
-        name2="2procure";
-        Assert.assertTrue((name1.compareTo(name2))<0);
-        Assert.assertTrue((AlphaNumericComparator.compareTo(name1,name2))<0);
-
-    }
-    @Test
-    public void prefixDecimalWithDifferentOrderAndStringCompare(){
-        String name1="1.3procure";
-        String name2="1.10procure";
-
-        //Regular compare not works here
-        Assert.assertFalse((name1.compareTo(name2))<0);
-
-        Assert.assertTrue((AlphaNumericComparator.compareTo(name1,name2))<0);
-
+        Assert.assertTrue((AlphaNumericComparator.compareTo(name1, name2)) < 0);
     }
 
 
-    @Test
-    public void suffixDecimalWithDifferentOrderAndStringCompare(){
-        String name1="procure1.3";
-        String name2="procure1.10";
-        //Regular compare not works here
-        Assert.assertFalse((name1.compareTo(name2))<0);
-
-        Assert.assertTrue((AlphaNumericComparator.compareTo(name1 ,name2))<0);
-
-    }
-
-    @Test
-    public void middlePartDecimalWithDifferentOrderAndStringCompare(){
-        String name1="procure1.3payment";
-        String name2="procure1.10payment";
-        //Regular compare not works here
-        Assert.assertFalse((name1.compareTo(name2))<0);
-
-        Assert.assertTrue((AlphaNumericComparator.compareTo(name1,name2))<0);
-
-    }
 }
