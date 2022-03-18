e1e30a13d7aa80639753d42e1b57ca6f0eca58e3
shapon2004 <shapon2004@hotmail.com>
2022-03-18 12:34:08 +0600

    AP-6162 Ordering of folders and files in tree and tile/list view.

--- Apromore-Core-Components/Apromore-Portal/src/test/java/org/apromore/portal/util/AlphaNumericComparatorUnitTest.java
+++ Apromore-Core-Components/Apromore-Portal/src/test/java/org/apromore/portal/util/AlphaNumericComparatorUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class AlphaNumericComparatorUnitTest {
+class AlphaNumericComparatorUnitTest {
 
     @ParameterizedTest
     @CsvSource({
@@ -14,7 +14,7 @@ public class AlphaNumericComparatorUnitTest {
         "1.1procure, 2procure",
         "1.10procure, 2procure"
     })
-    public void similarResultWithRegularComparison(String name1, String name2) {
+    void similarResultWithRegularComparison(String name1, String name2) {
         Assert.assertTrue((name1.compareTo(name2)) < 0);
         Assert.assertTrue((AlphaNumericComparator.compareTo(name1, name2)) < 0);
     }
@@ -25,7 +25,7 @@ public class AlphaNumericComparatorUnitTest {
         "procure1.3, procure1.10",
         "procure1.3payment, procure1.10payment"
     })
-    public void differentResultWithRegularComparison(String name1, String name2) {
+    void differentResultWithRegularComparison(String name1, String name2) {
         Assert.assertFalse((name1.compareTo(name2)) < 0);
 
         Assert.assertTrue((AlphaNumericComparator.compareTo(name1, name2)) < 0);
