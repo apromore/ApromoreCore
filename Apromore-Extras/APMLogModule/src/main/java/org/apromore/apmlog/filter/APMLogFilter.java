f895291a41b2a83980fb9c5b25bc1f1a6c0eb863
Chii Chang <chiichang@users.noreply.github.com>
2022-03-23 13:24:37 +1100

    fix/code_cleanup

--- Apromore-Extras/APMLogModule/src/main/java/org/apromore/apmlog/filter/APMLogFilter.java
+++ Apromore-Extras/APMLogModule/src/main/java/org/apromore/apmlog/filter/APMLogFilter.java
@@ -70,7 +70,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.BitSet;
 import java.util.Comparator;
 import java.util.HashMap;
