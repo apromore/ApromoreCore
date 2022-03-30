870e6863ce766890048f33e3257abcc19120bd56
Samuel Tumewa <samuel.tumewa@apromore.com>
2022-03-30 11:46:15 +1100

    fix: Removed previous commit files

--- Apromore-Database/src/main/java/org/apromore/dao/PredictionRepository.java
+++
@@ -1,9 +0,0 @@
-package org.apromore.dao;
-
-import org.apromore.dao.model.PredictionDao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PredictionRepository extends JpaRepository<PredictionDao, Long> {
-}
