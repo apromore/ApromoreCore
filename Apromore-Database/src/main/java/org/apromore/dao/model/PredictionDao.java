285f5e3421493aa135cc1e9a69ebb05d778eb495
Mayank Tomar <mayank.tomar@apromore.com>
2022-03-31 18:54:00 +1100

    fix: the update accross the sessions

--- Apromore-Database/src/main/java/org/apromore/dao/model/PredictionDao.java
+++ Apromore-Database/src/main/java/org/apromore/dao/model/PredictionDao.java
@@ -7,8 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
