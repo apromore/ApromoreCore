14083e8750217d8ad699b6ee9f33b28f62134df7
shapon2004 <shapon2004@hotmail.com>
2022-04-05 13:41:21 +0600

    AP-6309 message is displayed when the item is dragged&dropped but didn't change the location.

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/renderer/SummaryItemRenderer.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/renderer/SummaryItemRenderer.java
@@ -353,7 +353,7 @@ public class SummaryItemRenderer implements ListitemRenderer {
 
                     if (droppedToItem.getValue() != null && droppedToItem.getValue() instanceof FolderType
                             && droppedObjects.size() > 0) {
-                        mainController.getBaseListboxController().drop(droppedToItem.getValue(), droppedObjects);
+                        mainController.getBaseListboxController().drop(droppedToItem.getValue(), droppedObjects,false);
                     }
                 } catch (Exception e) {
                     LOGGER.error("Error Occured in Drag and Drop", e);
