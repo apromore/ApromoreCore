9964a38d73aec968a308b4b74dc32719aadd579a
Samuel Tumewa <samuel.tumewa@apromore.com>
2022-04-06 18:02:44 +1000

    fix: Removed mention of logId

--- Apromore-Database/src/main/java/org/apromore/dao/PredictorRepository.java
+++ Apromore-Database/src/main/java/org/apromore/dao/PredictorRepository.java
@@ -18,12 +18,10 @@
 
 package org.apromore.dao;
 
-import java.util.List;
 import org.apromore.dao.model.PredictorDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PredictorRepository extends JpaRepository<PredictorDao, Integer> {
-    List<PredictorDao> findAllByLogId(int logId);
 }
