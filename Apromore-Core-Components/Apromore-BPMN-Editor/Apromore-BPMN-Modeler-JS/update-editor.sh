ffdd951d90d694a39f6f6ec0d6fd2fa149b5bc16
shapon2004 <shapon2004@hotmail.com>
2022-05-10 11:29:28 +0600

    AP-5905 BPMN Editor font size

--- Apromore-Core-Components/Apromore-BPMN-Editor/Apromore-BPMN-Modeler-JS/update-editor.sh
+++ Apromore-Core-Components/Apromore-BPMN-Editor/Apromore-BPMN-Modeler-JS/update-editor.sh
@@ -5,4 +5,5 @@ cp dist/bpmn-modeler.development.js ../../../Apromore-Frontend/src/bpmneditor/ed
 cd ../../../Apromore-Frontend
 npm run build
 cp dist/bpmneditor.js ../Apromore-Core-Components/Apromore-BPMN-Editor/Apromore-BPMN-Editor-Portal/src/main/resources/static/bpmneditor/editor/
+cp dist/bpmneditor.js ../Apromore-Boot/build/resources/main/web/bpmneditor/editor/bpmneditor.js
 cd ../Apromore-Core-Components/Apromore-BPMN-Editor/Apromore-BPMN-Modeler-JS
