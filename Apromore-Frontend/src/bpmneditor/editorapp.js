2ece34c4fee84d2e2daf13a435ae8a8728245e56
shapon2004 <shapon2004@hotmail.com>
2022-05-30 15:52:54 +0600

    AP-6677 Editor: avoid zooming out when opening side panel

--- Apromore-Frontend/src/bpmneditor/editorapp.js
+++ Apromore-Frontend/src/bpmneditor/editorapp.js
@@ -268,8 +268,19 @@ export default class EditorApp {
         let me = this;
 
         if (this.useSimulationPanel) {
-            this.layout_regions.center.addListener('resize', function () {
-                me.zoomFitToModel();
+            this.layout_regions.center.addListener('resize', function (event) {
+            try{
+                const listerTypes=me.editorCommandStackListeners;
+                if(listerTypes){
+                for (const listerType of listerTypes) {
+                          if(listerType && listerType.type ==='Toolbar'){
+                            return;
+                        }
+                    }
+                }
+                }catch(e){}
+
+            me.zoomFitToModel();
             });
         }
 
