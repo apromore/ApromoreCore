67e5a077115e871f008223fa6f4ed3f9c649514f
lahiru mahanama <lahiru.mahanama@apromore.com>
2022-06-02 14:45:07 +1000

    Code cleanup

--- Apromore-Database/src/main/java/org/apromore/dao/model/PhysicalLog.java
+++ Apromore-Database/src/main/java/org/apromore/dao/model/PhysicalLog.java
@@ -20,8 +20,6 @@ package org.apromore.dao.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -29,7 +27,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Configurable;
 
 
