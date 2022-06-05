509fad4c7f5d0be946b639233b02dbc8a5949b7a
lahiru mahanama <lahiru.mahanama@apromore.com>
2022-06-06 09:59:44 +1000

    fixing sonar cloud smells

--- Apromore-Database/src/main/java/org/apromore/dao/model/PhysicalLogAttribute.java
+++ Apromore-Database/src/main/java/org/apromore/dao/model/PhysicalLogAttribute.java
@@ -28,7 +28,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Configurable;
 
 @Entity
