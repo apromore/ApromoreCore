26b6b35597264b7514ca14890b4e32e4aa4b5818
Chii Chang <chiichang@users.noreply.github.com>
2022-06-21 19:37:01 +1000

    fix/code cleanup

--- Apromore-Extras/APMLogModule/src/main/java/org/apromore/apmlog/filter/typefilters/costfilters/NodeCostFilter.java
+++ Apromore-Extras/APMLogModule/src/main/java/org/apromore/apmlog/filter/typefilters/costfilters/NodeCostFilter.java
@@ -33,7 +33,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @UtilityClass
 public class NodeCostFilter {
