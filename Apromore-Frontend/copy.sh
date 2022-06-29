39b91a54d1eb69281a661988ffc33b41510e1ae7
hoangnguyen7x <hoangnguyen7x@yahoo.com>
2022-06-29 12:46:38 +1000

    Remove the line to update new PD's frontend. It must stay with the old version until we finish all the migration for the new PD.

--- Apromore-Frontend/copy.sh
+++ Apromore-Frontend/copy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 cp dist/processdiscoverer.js ../Apromore-Custom-Plugins/Process-Discoverer-Portal-Plugin/src/main/resources/static/processdiscoverer/js/ap
-cp dist/processdiscoverer.js ../../Process-Discoverer-Portal-Plugin2/src/main/resources/static/pd/js/ap
 cp dist/loganimationbpmn.js ../../Log-Animation-Portal-Plugin/src/main/resources/static/loganimation2/js/ap/
 cp dist/bpmneditor.js ../Apromore-Core-Components/Apromore-BPMN-Editor/Apromore-BPMN-Editor-Portal/src/main/resources/static/bpmneditor/editor/
