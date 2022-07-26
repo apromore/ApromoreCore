5c898d9ecf2f2ffe78fd167b53485bd3a7b9782a
shapon2004 <shapon2004@hotmail.com>
2022-07-26 07:18:42 +0600

    AP-7127 extend numerical attributes ( Release - 8.4.1)

--- Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/BPMNEditorController.java
+++ Apromore-Core-Components/Apromore-Portal/src/main/java/org/apromore/portal/dialogController/BPMNEditorController.java
@@ -691,6 +691,8 @@ public class BPMNEditorController extends BaseController implements Composer<Com
     param.put("doAutoLayout", "false");
     param.put("username", currentUserType.getUsername());
     param.put("isNewProcess", false);
+    //View mode doesn't use the simulator so this is a dummy value
+    param.put("qbpProcessMaxLimit", 25000);
     Executions.getCurrent().pushArg(param);
   }
 
